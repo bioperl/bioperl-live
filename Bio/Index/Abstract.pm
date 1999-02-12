da826c33be57c9bd95e2625696f6360fd2a00207
birney <birney@bioperl.org>
1999-02-12 17:40:42 +0000

    better lisp
    added index/abstract.pm
    
    svn path=/bioperl-live/trunk/; revision=854

---
+++ Bio/Index/Abstract.pm
@@ -0,0 +1,189 @@
+
+#
+# BioPerl module for Bio::Index::Abstract
+#
+# Cared for by Ewan Birney <birney@sanger.ac.uk>
+#
+# Copyright Ewan Birney
+#
+# You may distribute this module under the same terms as perl itself
+
+# POD documentation - main docs before the code
+
+=head1 NAME
+
+Bio::Index::Abstract - Abstract interface for indexing a flat file
+
+=head1 SYNOPSIS
+
+You should not be using this module directly
+
+=head1 DESCRIPTION
+
+This object provides the basic mechanism to associate positions
+in files with names. The position and filenames are stored in DBM
+which can then be accessed later on. It is the equivalent of flat
+file indexing (eg, SRS or efetch).
+
+This object is the guts to the mechanism, which will be used by the
+specific objects inherieting from it.
+
+
+=head1 FEEDBACK
+
+=head2 Mailing Lists
+
+User feedback is an integral part of the evolution of this and other
+Bioperl modules. Send your comments and suggestions preferably to one
+of the Bioperl mailing lists.  Your participation is much appreciated.
+
+  vsns-bcd-perl@lists.uni-bielefeld.de          - General discussion
+  vsns-bcd-perl-guts@lists.uni-bielefeld.de     - Technically-oriented discussion
+  http://bio.perl.org/MailList.html             - About the mailing lists
+
+=head2 Reporting Bugs
+
+Report bugs to the Bioperl bug tracking system to help us keep track
+the bugs and their resolution.  Bug reports can be submitted via
+email or the web:
+
+  bioperl-bugs@bio.perl.org
+  http://bio.perl.org/bioperl-bugs/
+
+=head1 AUTHOR - Ewan Birney
+
+Email - birney@sanger.ac.uk
+
+=head1 APPENDIX
+
+The rest of the documentation details each of the object methods. Internal methods are usually preceded with a _
+
+=cut
+
+
+# Let the code begin...
+
+
+package Bio::Index::Abstract;
+use vars qw($AUTOLOAD @ISA);
+use strict;
+
+# Object preamble - inheriets from Bio::Root::Object
+
+use Bio::Root::Object;
+
+
+use AutoLoader;
+@ISA = qw(Bio::Root::Object Exporter);
+@EXPORT_OK = qw();
+# new() is inherited from Bio::Root::Object
+
+# _initialize is where the heavy stuff will happen when new is called
+
+sub _initialize {
+  my($self,@args) = @_;
+
+  my $make = $self->SUPER::_initialize;
+
+# set stuff in self from @args
+ return $make; # success - we hope!
+}
+
+
+=head2 start_record
+
+ Title   : start_record
+ Usage   : Abstract - should be overriden by the subclass
+ Function: This function is called for every line in the 
+           indexing function. It should return 1 when the
+           start of a record occurs
+ Example :
+ Returns : 1 or 0
+ Args    :
+
+
+=cut
+
+sub start_record{
+   my ($self,@args) = @_;
+
+   $self->throw("In Bio::Index::Abstract, defaulting to start_record - the sub class should be providing this");
+}
+
+=head2 end_record
+
+ Title   : end_record
+ Usage   : Internal - should be overriden in subclass
+ Function: This function is called for every line in the indexing function
+           It should return 1 at the end of a record and 0 otherwise
+ Example :
+ Returns : 
+ Args    :
+
+
+=cut
+
+sub end_record{
+   my ($self,@args) = @_;
+   
+   $self->throw("In Bio::Index::Abstract - defaulted for end_record. Sub class should provide this");
+}
+
+=head2 record_id
+
+ Title   : record_id
+ Usage   : Internal - should be overridden in subclass
+ Function: should return the id (if wanted) for this entry
+           to be indexed under
+ Example :
+ Returns : scalar or undef
+ Args    :
+
+
+=cut
+
+sub record_id{
+   my ($self,@args) = @_;
+
+   $self->throw("In Bio::Index::Abstract - defaulted for record_id. Sub class should provide this");
+}
+
+
+=head2 _index_hash
+
+ Title   : _index_hash
+ Usage   : %hash = $obj->_index_hash();
+ Function: returns the hash from the dbm index
+ Returns : a hash
+ Args    : none
+
+
+=cut
+
+sub _index_hash{
+   my ($self,@args) = @_;
+
+}
+
+=head2 _dbm_file
+
+ Title   : _dbm_file
+ Usage   : $obj->_dbm_file($newval)
+ Function: _dbm_file should be a fully qualified 
+           filename for the dbm file
+ Returns : value of _dbm_file
+ Args    : newvalue (optional)
+
+
+=cut
+
+sub _dbm_file{
+   my ($obj,$value) = @_;
+   if( defined $value) {
+      $obj->{'_dbm_file'} = $value;
+    }
+    return $obj->{'_dbm_file'};
+}
+
+
+
--- bioperl.lisp
+++ bioperl.lisp
@@ -1,4 +1,46 @@
 
+;; Perl mode set up
+
+(assoc "\\.pl$" auto-mode-alist)
+(setq auto-mode-alist (cons '("\\.pl$" . perl-mode) auto-mode-alist))
+
+(assoc "\\.pm$" auto-mode-alist)
+(setq auto-mode-alist (cons '("\\.pm$" . perl-mode) auto-mode-alist))
+
+(defun perl-insert-start ()
+  "Places #!..perl at the start of the script"
+  (interactive)
+  (goto-char (point-min))
+  (insert "#!/usr/local/bin/perl\n"))
+
+
+
+(defun bioperl-object-start (perl-object-name perl-caretaker-name caretaker-email)
+  "Places standard bioperl object notation headers and footers"
+  (interactive "sName of Object: \nsName of caretaker: \nsEmail: ")
+  (insert "\n#\n# BioPerl module for " perl-object-name "\n#\n# Cared for by " perl-caretaker-name " <" caretaker-email ">\n#\n# Copyright " perl-caretaker-name "\n#\n# You may distribute this module under the same terms as perl itself\n\n")
+  (insert "# POD documentation - main docs before the code\n\n")
+  (insert "=head1 NAME\n\n" perl-object-name " - DESCRIPTION of Object\n\n")
+  (insert "=head1 SYNOPSIS\n\nGive standard usage here\n\n")
+  (insert "=head1 DESCRIPTION\n\nDescribe the object here\n\n")
+  (insert "=head1 FEEDBACK\n\n=head2 Mailing Lists\n\n")
+  (insert "User feedback is an integral part of the evolution of this\nand other Bioperl modules. Send your comments and suggestions preferably\n to one of the Bioperl mailing lists.\nYour participation is much appreciated.\n\n")
+  (insert "  vsns-bcd-perl@lists.uni-bielefeld.de          - General discussion\n  vsns-bcd-perl-guts@lists.uni-bielefeld.de     - Technically-oriented discussion\n  http://bio.perl.org/MailList.html             - About the mailing lists\n\n")
+  (insert "=head2 Reporting Bugs\n\nReport bugs to the Bioperl bug tracking system to help us keep track\n the bugs and their resolution.\n Bug reports can be submitted via email or the web:\n\n")
+  (insert "  bioperl-bugs@bio.perl.org\n  http://bio.perl.org/bioperl-bugs/\n\n")
+  (insert "=head1 AUTHOR - " perl-caretaker-name "\n\nEmail " caretaker-email "\n\nDescribe contact details here\n\n")
+  (insert "=head1 APPENDIX\n\nThe rest of the documentation details each of the object methods. Internal methods are usually preceded with a _\n\n=cut\n\n")
+  (insert "\n# Let the code begin...\n\n")
+  (insert "\npackage " perl-object-name ";\n")
+  (insert "use vars qw($AUTOLOAD @ISA);\n")
+  (insert "use strict;\n")
+  (insert "\n# Object preamble - inheriets from Bio::Root::Object\n")
+  (insert "\nuse Bio::Root::Object;\n\n")
+  (insert "\nuse AutoLoader;\n@ISA = qw(Bio::Root::Object Exporter);\n@EXPORT_OK = qw();\n")
+  (insert "# new() is inherited from Bio::Root::Object\n\n")
+  (insert "# _initialize is where the heavy stuff will happen when new is called\n\n")
+  (insert "sub _initialize {\n  my($self,@args) = @_;\n\n  my $make = $self->SUPER::_initialize;\n\n# set stuff in self from @args\n return $make; # success - we hope!\n}\n")
+  )
 
 (defun bioperl-method (method-name)
   "puts in a bioperl method complete with pod bioler-plate"
@@ -9,27 +51,33 @@
     (insert "\n\n}\n"))
   )
 
-(defun bioperl-object-start (perl-object-name perl-caretaker-name caretaker-email)
-  "Places standard bioperl object notation headers and footers"
-  (interactive "sName of Object: \nsName of caretaker: \nsEmail: ")
-  (insert "\n#\n# BioPerl module for " perl-object-name "\n#\n# Cared for by " perl-caretaker-name " <" caretaker-email ">\n#\n# Copyright " perl-caretaker-name "\n#\n# You may distribute this module under the same terms as perl itself\n\n")
-  (insert "# POD documentation - main docs before the code\n\n")
-  (insert "=head1 NAME\n\n" perl-object-name " - DESCRIPTION of Object\n\n")
-  (insert "=head1 SYNOPSIS\n\nGive standard usage here\n\n")
-  (insert "=head1 DESCRIPTION\n\nDescribe the object here\n\n")
-  (insert "=head1 CONTACT\n\nDescribe contact details here\n\n")
-  (insert "=head1 APPENDIX\n\nThe rest of the documentation details each of the object methods. Internal methods are usually preceded with a _\n\n=cut\n\n")
-  (insert "\n# Let the code begin...\n\n")
-  (insert "\npackage " perl-object-name ";\n")
-  (insert "use vars qw(@ISA);\n")
-  (insert "use strict;\n")
-  (insert "\n# Object preamble - inheriets from Bio::Root::Object\n")
-  (insert "\nuse Bio::Root::Object;\n\n")
-  (insert "\n@ISA = qw(Bio::Root::Object Exporter);\n")
-  (insert "# new() is inherited from Bio::Root::Object\n\n")
-  (insert "# _initialize is where the heavy stuff will happen when new is called\n\n")
-  (insert "sub _initialize {\n  my($self,@args) = @_;\n\n  my $make = $self->SUPER::_initialize;\n\n# set stuff in self from @args\n return $make; # success - we hope!\n}\n")
-  )
+
+(defun bioperl-getset (field-name)
+  "puts in a bioperl method for a get/set method complete with pod bioler-plate"
+  (interactive "sfield name:")
+  (insert "=head2 " field-name "\n\n Title   : " field-name "\n Usage   : $obj->" field-name "($newval)\n Function: \n Example : \n Returns : value of " field-name "\n Args    : newvalue (optional)\n\n\n=cut\n\n")
+  (insert "sub " field-name "{\n   my ($obj,$value) = @_;\n   if( defined $value) {\n      $obj->{'" field-name "'} = $value;\n    }\n    return $obj->{'" field-name "'};\n")
+  (insert "\n}\n"))
+
+(setq perl-mode-hook 
+      '(lambda ()
+	 (define-key perl-mode-map "\C-c\C-h" 'perl-insert-start)
+	 (define-key perl-mode-map "\C-c\C-b" 'bioperl-object-start)
+	 (define-key perl-mode-map "\C-c\C-v" 'bioperl-getset)
+	 (define-key perl-mode-map "\C-c\C-b" 'bioperl-method)
+	 (define-key perl-mode-map "\C-c\C-z" 'compile)
+	 (define-key perl-mode-map [menu-bar] (make-sparse-keymap))
+	 (define-key perl-mode-map [menu-bar p]
+	   (cons "BioPerl" (make-sparse-keymap "BioPerl")))
+	 (define-key perl-mode-map [menu-bar p perl-script-start]
+	   '("Insert script template" . perl-script-start))
+	 (define-key perl-mode-map [menu-bar p bioperl-object-start]
+	   '("bioperl object template" . bioperl-object-start))
+	 (define-key perl-mode-map [menu-bar p bioperl-getset]
+	   '("bioperl field func" . bioperl-getset))
+	 (define-key perl-mode-map [menu-bar p bioperl-method]
+	   '("bioperl method" . bioperl-method))
+	 ))
 
 
 
