69482b17ae52de4f1a20fef903462f3d494c0ce7
amackey <amackey@bioperl.org>
1999-02-16 18:01:38 +0000

    Overhauled a bit to use IO::Socket instead of LWP and Bio::SeqIO instead of Seq->parse_genbank().  Probably not functional yet.
    svn path=/bioperl-live/trunk/; revision=867

--- Bio/DB/GenBank.pm
+++ Bio/DB/GenBank.pm
@@ -5,9 +5,8 @@ use strict;
 
 use Bio::DB::Abstract;     # to inherit from
 
-use Bio::Seq;              # to return a Seq object
-use Bio::Tools::WWW qw(:obj);       # to get url information
-use LWP::Simple qw(get);           # to access the internet
+use Bio::SeqIO;                     # to parse and return Seq object
+use IO::Socket;                     # to connect with GenBank
 
 @ISA = qw(Bio::DB::Abstract Exporter);
 @EXPORT_OK = qw();
@@ -18,17 +17,17 @@ sub get_Seq_by_id {
   my($uid, $alpha) = $self->_rearrange( [qw(UID ALPHA)], @params);
 
   $alpha ||= 'n'; # default of nucleotide
-  my $entrez = ($alpha =~ m/^p$/i ?
-		$BioWWW->search_url('gb_p') :
-		$BioWWW->search_url('gb_n')
-	       ) . $uid . '&html=no&title=no';
+  my $entrez = ($alpha =~ m/^p/i ? 'db=p&' : 'db=n&') .
+    "form=6&dopt=g&html=no&title=no&uid=$uid" ;
 
-  my $seq = new Bio::Seq;
-  my $data = get($entrez);
-  print STDOUT $data; # debugging only AJM
-  $seq->parse($data, 'genbank');
+  my $sock = IO::Socket::INET->new(PeerAddr => 'www3.ncbi.nlm.nih.gov',
+				   PeerPort => 'http(80)',
+				   Proto    => 'tcp' );
+  select $sock;
+  $sock->autoflush(1);
+  print 'GET ' . "htbin-post/Entrez/query?$entrez";
 
-  return $seq;
+  return Bio::SeqIO->new(-file => $sock, -format => 'EMBL')->next_seq()
 }
 
 1;
