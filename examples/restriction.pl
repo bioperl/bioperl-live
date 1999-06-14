aeaf00133553bf9df0cb513d7a99aac3f6c9996e
birney <birney@bioperl.org>
1999-06-14 14:36:07 +0000

    *** empty log message ***
    svn path=/bioperl-live/branches/stable-0-05-new/; revision=1010

--- examples/restriction.pl
+++ examples/restriction.pl
@@ -30,7 +30,7 @@ eval{
     $seq = new Bio::Seq(-ID  =>'test_seq', 
 			-SEQ =>$sequence); 
     
-    $re  = new Bio::Tools::RestrictionEnzyme(-NAME=>'EcoRI');
+    $re  = new Bio::Tools::RestrictionEnzyme(-name=>'EcoRI');
 };
 $@ && die "\n*** TROUBLE BUILDING OBJECTS:\n$@\n"; 
 
