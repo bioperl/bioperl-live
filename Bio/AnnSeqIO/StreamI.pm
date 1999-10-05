b3cb697890faf2277fd01b897d6a23a8a1da0d48
birney <birney@bioperl.org>
1999-10-05 18:58:12 +0000

    added StreamI.pm
    svn path=/bioperl-live/trunk/; revision=1082

---
+++ Bio/AnnSeqIO/StreamI.pm
@@ -0,0 +1,89 @@
+
+#
+# BioPerl module for Bio::AnnSeqIO::StreamI
+#
+# Cared for by Ewan Birney <birney@sanger.ac.uk>
+#
+# Copyright Ewan Birney
+#
+# You may distribute this module under the same terms as perl itself
+
+# POD documentation - main docs before the code
+
+=head1 NAME
+
+Bio::AnnSeqIO::StreamI - DESCRIPTION of Object
+
+=head1 SYNOPSIS
+
+Give standard usage here
+
+=head1 DESCRIPTION
+
+Describe the object here
+
+=head1 FEEDBACK
+
+=head2 Mailing Lists
+
+User feedback is an integral part of the evolution of this
+and other Bioperl modules. Send your comments and suggestions preferably
+ to one of the Bioperl mailing lists.
+Your participation is much appreciated.
+
+  vsns-bcd-perl@lists.uni-bielefeld.de          - General discussion
+  vsns-bcd-perl-guts@lists.uni-bielefeld.de     - Technically-oriented discussion
+  http://bio.perl.org/MailList.html             - About the mailing lists
+
+=head2 Reporting Bugs
+
+Report bugs to the Bioperl bug tracking system to help us keep track
+ the bugs and their resolution.
+ Bug reports can be submitted via email or the web:
+
+  bioperl-bugs@bio.perl.org
+  http://bio.perl.org/bioperl-bugs/
+
+=head1 AUTHOR - Ewan Birney
+
+Email birney@sanger.ac.uk
+
+Describe contact details here
+
+=head1 APPENDIX
+
+The rest of the documentation details each of the object methods. Internal methods are usually preceded with a _
+
+=cut
+
+
+# Let the code begin...
+
+
+package Bio::AnnSeqIO::StreamI;
+use vars qw($AUTOLOAD @ISA);
+use strict;
+
+# Object preamble - inherits from Bio::Root::Object
+
+
+=head2 next_annseq
+
+ Title   : next_annseq
+ Usage   :
+ Function:
+ Example :
+ Returns : 
+ Args    :
+
+
+=cut
+
+sub next_annseq{
+   my ($self) = @_;
+
+   $self->throw("Base class has not implemented this yet");
+
+}
+
+1;
