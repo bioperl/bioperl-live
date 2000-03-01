bdab639d759152faacff18e87f889faa797caed8
heikki <heikki@bioperl.org>
2000-03-01 10:09:09 +0000

    *** empty log message ***
    svn path=/bioperl-live/trunk/; revision=1165

---
+++ Bio/Tools/CodonTable.pm
@@ -0,0 +1,469 @@
+
+#
+# bioperl module for Bio::Tools::CodonTable
+#
+# Cared for by Heikki Lehvaslaiho <heikki@ebi.ac.uk>
+#
+# Copyright Heikki Lehvaslaiho
+#
+# You may distribute this module under the same terms as perl itself
+
+# POD documentation - main docs before the code
+
+=head1 NAME
+
+Bio::Tools::CodonTable - Bioperl codon table object
+
+=head1 SYNOPSIS
+
+  This is a read-only class for all known codon tables.
+  The IDs are the ones used by nucleotide sequence databases.
+
+  # to use
+  use Bio::Tools:CodonTable;
+
+  # defaults to ID 1 "Standard"
+  $myCodonTable   = Bio::Tools::CodonTable->new();
+  $myCodonTable2  = Bio::Tools::CodonTable -> new ( -id => 3 );
+
+  # change codon table
+  $myCodonTable->id(5);
+
+  # examine codon table
+  print  join (' ', "The name of the codon table no.", $myCodonTable->id(4), 
+	       "is:", $myCodonTable->name(), "\n");
+
+  # translate a codon
+  $aa = $myCodonTable->translate('ACU');
+  $aa = $myCodonTable->translate('act');
+
+  # reverse translate an amino acid 
+  @codons = $myCodonTable->revtranslate('A');
+  @codons = $myCodonTable->revtranslate('Ser');
+  @codons = $myCodonTable->revtranslate('cYS', 'rna');
+
+  #boolean tests
+   print "Is a start\n"       if $myCodonTable->is_start_codon('ATG');
+   print "Is a termianator\n" if $myCodonTable->is_ter_codon('ATG');
+
+=head1 DESCRIPTION
+
+Codon tables are also called translation tables or genetics codes
+since that is what they try to represent. A bit more complete picture
+of the full complexity of codon usage in various taxonomic groups
+presented at the NCBI Genetic Codes Home page.
+
+
+CodonTable is a BioPerl class that knows all current translation
+tables that are used by primary nucleotide sequence databases
+(GenBank, EMBL and DDBJ). It provides methods to output information
+about tables and relationships between codons and amino acids.
+
+It is a nuisance to separate RNA and cDNA representations of nucleic
+acid transcripts. The CodonTable object accepts codons of both type as
+input and allows the user to set the mode for output when reverse
+translating. It default for output is DNA. 
+
+Note: This class deals with individual codons and amino acids, only.
+      Call it from your own objects to translate and reverse translate
+      longer sequences.
+
+
+NCBI Genetic Codes home page:
+     http://www.ncbi.nlm.nih.gov/htbin-post/Taxonomy/wprintgc?mode=c
+
+EBI Translation Table Viewer:
+     http://www.ebi.ac.uk/cgi-bin/mutations/trtables.cgi
+
+Amended ASN.1 version with ids 16 and 21 is at:
+     ftp://ftp.ebi.ac.uk/pub/databases/geneticcode/
+
+Thank your for Matteo diTomasso for the original Perl implementation
+of these tables.
+
+=head1 FEEDBACK
+
+=head2 Mailing Lists
+
+User feedback is an integral part of the evolution of this and other
+Bioperl modules. Send your comments and suggestions preferably to one
+of the Bioperl mailing lists.  Your participation is much appreciated.
+
+  vsns-bcd-perl@lists.uni-bielefeld.de          - General discussion
+  vsns-bcd-perl-guts@lists.uni-bielefeld.de     - Technically-oriented discussion
+  http://bio.perl.org/MailList.html             - About the mailing lists
+
+=head2 Reporting Bugs
+
+report bugs to the Bioperl bug tracking system to help us keep track
+ the bugs and their resolution.  Bug reports can be submitted via
+ email or the web:
+
+  bioperl-bugs@bio.perl.org
+  http://bio.perl.org/bioperl-bugs/
+
+=head1 AUTHOR - Heikki Lehvaslaiho
+
+Email:  heikki@ebi.ac.uk
+Address: 
+
+     EMBL Outstation, European Bioinformatics Institute
+     Wellcome Trust Genome Campus, Hinxton
+     Cambs. CB10 1SD, United Kingdom 
+
+=head1 APPENDIX
+
+The rest of the documentation details each of the object
+methods. Internal methods are usually preceded with a _
+
+=cut
+
+
+# Let the code begin...
+
+
+package Bio::Tools::CodonTable;
+use vars qw(@ISA);
+use strict;
+
+# Object preamble - inherits from Bio::Root::Object
+
+use Bio::Root::Object;
+
+@ISA = qw(Bio::Root::Object);
+
+# new() is inherited from Bio::Root::Object
+
+# _initialize is where the heavy stuff will happen when new is called
+
+
+{
+# first set internal values for all translation tables
+
+
+my @names =  #id
+    (
+     'Standard', #1 
+     'Vertebrate Mitochondrial',#2 
+     'Yeast Mitochondrial',# 3 
+     'Mold, Protozoan, and CoelenterateMitochondrial and Mycoplasma/Spiroplasma',#4
+     'Invertebrate Mitochondrial',#5 
+     'Ciliate, Dasycladacean and Hexamita Nuclear',# 6 
+       '', '',
+     'Echinoderm Mitochondrial',#9 
+     'Euplotid Nuclear',#10
+     '"Bacterial"',# 11
+     'Alternative Yeast Nuclear',# 12
+     'Ascidian Mitochondrial',# 13
+     'Flatworm Mitochondrial',# 14
+     'Blepharisma Nuclear',# 15
+     'Chlorophycean Mitochondrial',# 16
+       '', '',  '', '',
+     'Trematode Mitochondrial'# 21
+     );
+
+my @tables = 
+    qw(
+       FFLLSSSSYY**CC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG
+       FFLLSSSSYY**CCWWLLLLPPPPHHQQRRRRIIMMTTTTNNKKSS**VVVVAAAADDEEGGGG
+       FFLLSSSSYY**CCWWTTTTPPPPHHQQRRRRIIMMTTTTNNKKSSRRVVVVAAAADDEEGGGG
+       FFLLSSSSYY**CCWWLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG
+       FFLLSSSSYY**CCWWLLLLPPPPHHQQRRRRIIMMTTTTNNKKSSSSVVVVAAAADDEEGGGG
+       FFLLSSSSYYQQCC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG
+       '' ''
+       FFLLSSSSYY**CCWWLLLLPPPPHHQQRRRRIIIMTTTTNNNKSSSSVVVVAAAADDEEGGGG
+       FFLLSSSSYY**CCCWLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG
+       FFLLSSSSYY**CC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG
+       FFLLSSSSYY**CC*WLLLSPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG
+       FFLLSSSSYY**CCWWLLLLPPPPHHQQRRRRIIMMTTTTNNKKSSGGVVVVAAAADDEEGGGG
+       FFLLSSSSYYY*CCWWLLLLPPPPHHQQRRRRIIIMTTTTNNNKSSSSVVVVAAAADDEEGGGG
+       FFLLSSSSYY*QCC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG
+       FFLLSSSSYY*LCC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG
+       '' '' '' ''
+       FFLLSSSSYY**CCWWLLLLPPPPHHQQRRRRIIMMTTTTNNNKSSSSVVVVAAAADDEEGGGG   
+       );
+
+
+my @starts = 
+    qw(
+       ---M---------------M---------------M----------------------------
+       --------------------------------MMMM---------------M------------
+       ----------------------------------MM----------------------------
+       --MM---------------M------------MMMM---------------M------------
+       ---M----------------------------MMMM---------------M------------
+       -----------------------------------M----------------------------
+       '' ''
+       -----------------------------------M----------------------------
+       -----------------------------------M----------------------------
+       ---M---------------M------------MMMM---------------M------------
+       -------------------M---------------M----------------------------
+       -----------------------------------M----------------------------
+       -----------------------------------M----------------------------
+       -----------------------------------M----------------------------
+       -----------------------------------M----------------------------
+       '' ''  '' ''
+       -----------------------------------M---------------M------------  
+       );
+
+
+my @nucs = ('t','c','a','g');
+my ($x) = 0;
+my ($i, $j, $k);
+my ($codons, $trCol);
+
+for ($i=0;$i<4;$i++) {
+    for ($j=0;$j<4;$j++) {
+	for ($k=0;$k<4;$k++) {
+	    $codons->{$nucs[$i]. $nucs[$j]. $nucs[$k]} = $x;
+	    $trCol->{$x} = $nucs[$i]. $nucs[$j]. $nucs[$k];
+	    $x++;
+	}   
+    }   
+}
+
+my  %onecode =   (Ala => 'A',
+		  Cys => 'C',
+		  Asp => 'D',
+		  Glu => 'E',
+		  Phe => 'F',
+		  Gly => 'G',
+		  His => 'H',
+		  Ile => 'I',
+		  Lys => 'K',
+		  Leu => 'L',
+		  Met => 'M',
+		  Asn => 'N',
+		  Pro => 'P',
+		  Gln => 'Q',
+		  Arg => 'R',
+		  Ser => 'S',
+		  Thr => 'T',
+		  Val => 'V',
+		  Trp => 'W',
+		  Tyr => 'Y',
+		  Ter => 'X'
+		  );
+    
+
+sub _initialize {
+    my($obj,@args) = @_;
+    
+    my($id) =
+	$obj->_rearrange([qw(ID
+			     )],
+			 @args);
+    
+    $id = 1 if ( ! $id );
+    
+    my $make = $obj->SUPER::_initialize(@args);
+    
+    $id  && $obj->id($id);
+    
+    return $make; # success - we hope!
+}
+
+
+=head2 id
+
+ Title   : id
+ Usage   : $obj->id(3); $id_integer = $obj->id();
+ Function: 
+
+           Sets or returns the id of the translation table.  IDs are
+           integers from 1 to 15, excluding 7 and 8 which have been
+           removed as redundant. If an invalid ID is given the method
+           returns 0, false.
+
+
+ Example : 
+ Returns : value of id, a scalar, 0 if not a valid 
+ Args    : newvalue (optional)
+
+=cut
+
+sub id{
+   my ($obj,$value) = @_;
+   if( defined $value) {
+       if (  !(defined $tables[$value-1]) or $tables[$value-1] eq '') {
+	   $obj->warn("Not a valid codon table ID [$value] ");
+	   $value = 0;
+       }
+       $obj->{'id'} = $value;
+   }
+   return $obj->{'id'};
+}
+
+=head2 name
+
+ Title   : name
+ Usage   : $obj->name()
+ Function: returns the descriptive name of the translation table
+ Example : 
+ Returns : A string
+ Args    : None
+
+
+=cut
+
+sub name{
+   my ($obj) = @_;
+
+   my ($id) = $obj->{'id'};
+   return $names[$id-1];
+
+}
+
+
+=head2 translate
+
+ Title   : translate
+ Usage   : $obj->translate('ACT')
+ Function: returns one letter amino acid code for a codon input
+
+           User is responsible to resolve ambiguous nucleotide codes
+           before calling this method. Returns 'X' for unknown codons
+           and an empty string for input strings that are not three
+           characters long.
+
+ Example : 
+ Returns : A string
+ Args    : a codon = a three nucleotide character string
+
+
+=cut
+
+sub translate{
+   my ($obj, $value) = @_;
+   my ($id) = $obj->{'id'};
+
+   $value  =~ tr/GACTUu/gacttt/;
+
+   if (length $value != 3 ) {
+       return '';
+   }
+   elsif (!(defined $codons->{$value}))  {
+       return 'X';
+   }
+   else {
+       return substr($tables[$id-1],$codons->{$value},1);
+   }
+}
+
+
+=head2 revtranslate
+
+ Title   : revtranslate
+ Usage   : $obj->revtranslate('G')
+ Function: returns codons for an amino acid
+
+           User is responsible to resolve ambiguous amino acid codes
+           codes before calling this method. Returns an empty string
+           for unknown amino acid codes. Both single and three letter
+           amino acid codes are accepted. 
+
+           By default the output codons are shown in DNA.  If the output is
+           needed in RNA (tr/t/u/), add a second argument 'RNA'.
+
+ Example : $obj->revtranslate('Gly', 'RNA')
+ Returns : An array of three lower case letter strings i.e. codons
+ Args    : amino acid, 'RNA'
+
+=cut
+
+sub revtranslate {
+    my ($obj, $value, $coding) = @_;
+    my ($id) = $obj->{'id'};
+    my (@codons, $p);
+
+    if (length($value) == 3 ) {
+	$value = lc $value;
+	$value = ucfirst $value;
+	$value = $onecode{$value}; 
+    }
+    if (defined $value and length($value) == 1 ) {
+	$value = uc $value;
+	while ($tables[$id-1] =~ m/$value/g) {
+	    $p = pos $tables[$id-1];
+	    push (@codons, $trCol->{--$p});
+	} 
+    }
+
+    if ($coding and uc ($coding) eq 'RNA') {
+	for my $i (0..$#codons)  {
+	    $codons[$i] =~ tr/t/u/;
+	}
+    }
+
+    return @codons; 
+}
+
+=head2 is_start_codon
+
+ Title   : is_start_codon
+ Usage   : $obj->is_start_codon('ATG')
+ Function: returns true (1) for all codons that can be used as a
+           translation start, false (0) for others.
+ Example : $myCodonTable->is_start_codon('ATG')
+ Returns : boolean
+ Args    : codon
+
+
+=cut
+
+sub is_start_codon{
+   my ($obj, $value) = @_;
+   my ($id) = $obj->{'id'};
+
+   $value  =~ tr/GACTUu/gacttt/;
+
+   if (length $value != 3  or !(defined $codons->{$value}))  {
+       return 0;
+   }
+   else {
+       if ( substr($starts[$id-1],$codons->{$value},1) eq "M" ) {
+	   return 1 ;
+       }
+       else {
+	   return 0;
+       }
+   }
+}
+
+
+
+=head2 is_ter_codon
+
+ Title   : is_ter_codon
+ Usage   : $obj->is_ter_codon('GAA')
+ Function: returns true (1) for all codons that can be used as a
+           translation tarminator, false (0) for others.
+ Example : myCodonTable->is_ter_codon('ATG')
+ Returns : boolean
+ Args    : codon
+
+
+=cut
+
+sub is_ter_codon{
+   my ($obj, $value) = @_;
+   my ($id) = $obj->{'id'};
+
+   $value  =~ tr/GACTUu/gacttt/;
+
+   if (length $value != 3  or !(defined $codons->{$value}))  {
+       return 0;
+   }
+   else {
+       if ( substr($tables[$id-1],$codons->{$value},1) eq '*' ) {
+	   return 1 ;
+       }
+       else {
+	   return 0;
+       }
+   }
+}
+
+}
+
+1;
+
---
+++ t/CodonTable.t
@@ -0,0 +1,128 @@
+## Bioperl Test Harness Script for Modules
+## $Id$
+
+# Before `make install' is performed this script should be runnable with
+# `make test'. After `make install' it should work as `perl test.t'
+
+#-----------------------------------------------------------------------
+## perl test harness expects the following output syntax only!
+## 1..3
+## ok 1  [not ok 1 (if test fails)]
+## 2..3
+## ok 2  [not ok 2 (if test fails)]
+## 3..3
+## ok 3  [not ok 3 (if test fails)]
+##
+## etc. etc. etc. (continue on for each tested function in the .t file)
+#-----------------------------------------------------------------------
+
+
+## We start with some black magic to print on failure.
+BEGIN { $| = 1; print "1..10\n"; 
+	use vars qw($loaded); }
+END {print "not ok 1\n" unless $loaded;}
+
+#use lib '../';
+use Bio::Tools::CodonTable;
+
+$loaded = 1;
+print "ok 1\n";    # 1st test passes.
+
+
+## End of black magic.
+##
+## Insert additional test code below but remember to change
+## the print "1..x\n" in the BEGIN block to reflect the
+## total number of tests that will be run. 
+
+
+# create a table object by giving an ID
+
+$myCodonTable = Bio::Tools::CodonTable -> new ( -id => 16);
+print "ok 2\n";  
+
+
+#($start, $stop) = $range->intersection($range2);
+#if( $start == 15 && $stop == 20 ) {
+#   print "ok 5\n";
+#} else {
+#   print "not ok 5\n";
+#}      
+
+# defaults to ID 1 "Standard"
+$myCodonTable = Bio::Tools::CodonTable->new();
+if ( $myCodonTable->id() == 1 ) {
+   print "ok 3\n";
+} else {
+   print "not ok 3\n";
+}
+
+#check if a codon table ID is valid before setting it:
+if ( $myCodonTable->id(55) ) {
+   print "not ok 4\n";
+}
+else {
+   print "ok 4\n";
+}
+
+# change codon table
+$myCodonTable->id(10);
+print "ok 5\n";
+
+if( $myCodonTable->name() ==  'Euplotid Nuclear' ) {
+   print "ok 6\n";
+} else {
+   print "not ok 6\n";
+}
+
+# translate codons
+@ii = ('ACT', 'acu', 'AC', 'ACN');
+@res = ('T', 'T', '', 'X' );
+foreach $i (@ii) {
+    $aa = $myCodonTable->translate($i);
+    push @aa, $aa;
+}
+if (@aa == @res) {
+    print "ok 7\n";
+}
+else {
+    print "not ok 7\n";
+}
+
+
+# reverse translate amino acids 
+@aa=();
+@ii = ('A',  'g',  'ACN', 'Thr', 'sER' );
+@res = qw(gct gcc gca gcg ggt ggc gga ggg act acc aca acg tct tcc tca tcg agt agc);
+foreach $i (@ii) {
+    @codon1 = $myCodonTable->revtranslate($i);
+    push (@aa, @codon1);
+}
+
+if (@aa == @res) {
+    print "ok 8\n";
+}
+else {
+    print "not ok 8\n";
+}
+
+#  #boolean tests
+  $myCodonTable->id(1);
+
+if ($myCodonTable->is_start_codon('ATG') and 
+    !  ($myCodonTable->is_start_codon('GGH')) and
+    !  ($myCodonTable->is_start_codon('CCC')) ){
+    print "ok 9\n";
+}
+else {
+    print "not ok 9\n";
+}
+
+if ($myCodonTable->is_ter_codon('UAG') and  
+    $myCodonTable->is_ter_codon('TaG') and
+    ! ($myCodonTable->is_ter_codon('ttA'))) {
+    print "ok 10\n";
+}
+else {
+    print "not ok 10\n";
+}
