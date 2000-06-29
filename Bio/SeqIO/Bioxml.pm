38512b49bd34105849643b1d53541ac338a20cb5
bradmars <bradmars@bioperl.org>
2000-06-29 06:17:55 +0000

    *** empty log message ***
    svn path=/bioperl-live/trunk/; revision=1346

--- Bio/SeqIO/Bioxml.pm
+++ Bio/SeqIO/Bioxml.pm
@@ -55,7 +55,7 @@ methods. Internal methods are usually preceded with a _
 
 # Let the code begin...
 
- package Bio::SeqIO::bioxml;
+package Bio::SeqIO::bioxml;
 use vars qw(@ISA);
 use strict;
 # Object preamble - inherits from Bio::Root::Object
