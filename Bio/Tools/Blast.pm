aa27d23fe9029d05506ad7974972d9ccac418cdb
lapp <lapp@bioperl.org>
2000-07-31 23:20:17 +0000

    Fixed failure on empty (single) reports. Fixed ambiguous [] range in regexp.
    svn path=/bioperl-live/trunk/; revision=1415

--- Bio/Tools/Blast.pm
+++ Bio/Tools/Blast.pm
@@ -1747,17 +1747,18 @@ sub _get_parse_blast_func {
 		# In some reports, the Database is only listed at end.
 		#$Blast->warn("Can't determine database name from BLAST report.");
 	      }
-	    }
 
-            # Incyte_Fix:   Nasty Invisible Bug.
-            # Records in blast report are delimited by '>', but... when
-            #  there are no hits for a query, there won't be a '>'.  That
-            #  causes several blast reports to run together in the data
-            #  passed to this routine.  Need to get rid of non-hits in data
-            if ($data =~ /.+(No hits? found.+Sequences.+)/so) {
-                $data = $1;
-            }
-            # End Incyte_Fix
+	      # Incyte_Fix:   Nasty Invisible Bug.
+	      # Records in blast report are delimited by '>', but... when
+	      #  there are no hits for a query, there won't be a '>'.  That
+	      #  causes several blast reports to run together in the data
+	      #  passed to this routine.  Need to get rid of non-hits in data
+	      if ($data =~ /.+(No hits? found.+Sequences.+)/so) {
+		  $data = $1;
+	      }
+	      # End Incyte_Fix
+
+	    }
 
 	    # Determine if we need to create a new Blast object 
 	    # or use the $self object for this method.
@@ -2424,7 +2425,7 @@ sub _set_blast1_stats {
 #----------------------
     my ($self, $data) = (@_);
     
-    if(!$self->{'_gapped'} and $self->program_version() =~ /^2[\w-\.]+WashU/) {
+    if(!$self->{'_gapped'} and $self->program_version() =~ /^2[\w\-\.]+WashU/) {
 	$self->_set_gapping_wu($data);
     } else {
 	$self->{'_gapped'} = 0;
