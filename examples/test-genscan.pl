e24e1c906bd31f4c0d121c973a232bacfe79b54f
lapp <lapp@bioperl.org>
2000-08-04 09:22:39 +0000

    Initial revision.
    svn path=/bioperl-live/trunk/; revision=1432

---
+++ examples/test-genscan.pl
@@ -0,0 +1,74 @@
+#!/usr/local/bin/perl
+#
+# Script for checking and demonstrating the Genscan parser module.
+#
+# usage: pipe the Genscan prediction result to this script. Optionally,
+# specify the sequence file (FASTA format) fed to Genscan by --seq myseq.tfa.
+#
+# BTW you can use this script for testing the MZEF module as well with a few
+# modifications: replace every occurrence of Bio::Tools::Genscan with
+# Bio::Tools::MZEF.
+#
+# Hilmar Lapp <hlapp@gmx.net>
+#
+
+use Getopt::Long;
+use Bio::Tools::Genscan;
+use Bio::Tools::Prediction::Gene;
+use Bio::SeqIO;
+
+my $in = \*STDIN;
+my $out = \*STDOUT;
+
+$opt_ok = GetOptions("seq=s", \$seqfile);
+
+my $seq;
+
+if(defined($seqfile)) {
+    my $seqin = Bio::SeqIO->new('-file' => $seqfile, '-format' => "fasta");
+    $seq = $seqin->next_seq();
+    $seqin->close();
+}
+my $seqout = Bio::SeqIO->new('-fh' => $out);
+
+my $genscan = Bio::Tools::Genscan->new('-fh' => $in);
+
+while($gene = $genscan->next_prediction()) {
+    $gene->attach_seq($seq) if $seq;
+    
+    my $fea;
+    
+    print $out "\n======= Next prediction ==========\n\n";
+    foreach $fea ($gene->exons()) {
+	print $out "EXON ", $fea->gff_string(), "\n";
+    }
+    foreach $fea ($gene->promotors()) {
+	print $out "PROM ", $fea->gff_string(), "\n";
+    }
+    foreach $fea ($gene->poly_A_sites()) {
+	print $out "POLA ", $fea->gff_string(), "\n";
+    }
+    # sequences
+    my $cds;
+    my $outseq;
+    if($gene->predicted_protein()) {
+	$outseq = Bio::PrimarySeq->new('-seq' => $gene->predicted_protein(),
+				       '-id' => "pred_pept_" . $gene->primary_tag());
+	$seqout->write_seq($outseq);
+    }
+    if($seq) {
+	$cds = Bio::PrimarySeq->new('-seq' => $gene->cds(1),
+				    '-id' => "cds_" . $gene->primary_tag());
+	$outseq = $cds->translate();
+	$seqout->write_seq($outseq);
+    }
+    if($gene->predicted_cds()) {
+	$outseq = Bio::PrimarySeq->new('-seq' => $gene->predicted_cds(),
+				       '-id' => "cds_pred_" . $gene->primary_tag());
+	$seqout->write_seq($outseq);
+    }
+    if($seq) {
+	$seqout->write_seq($cds);
+    }	
+}
+
