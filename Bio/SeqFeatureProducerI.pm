860ddafba79730ab7886d1919042e06baf0a70e7
lapp <lapp@bioperl.org>
2000-09-05 07:35:13 +0000

    Corrected doc bugs (and spelling of my name :-)
    svn path=/bioperl-live/trunk/; revision=1482

--- Bio/SeqFeatureProducerI.pm
+++ Bio/SeqFeatureProducerI.pm
@@ -33,8 +33,7 @@ and other Bioperl modules. Send your comments and suggestions preferably
  to one of the Bioperl mailing lists.
 Your participation is much appreciated.
 
-  vsns-bcd-perl@lists.uni-bielefeld.de          - General discussion
-  vsns-bcd-perl-guts@lists.uni-bielefeld.de     - Technically-oriented discussion
+  bioperl-l@bioperl.org          - General discussion
   http://bio.perl.org/MailList.html             - About the mailing lists
 
 =head2 Reporting Bugs
@@ -46,10 +45,10 @@ Report bugs to the Bioperl bug tracking system to help us keep track
   bioperl-bugs@bio.perl.org
   http://bio.perl.org/bioperl-bugs/
 
-=head1 AUTHOR - Jason Stajich with Himlar Lapp
+=head1 AUTHOR - Jason Stajich with Hilmar Lapp
 
 Jason Stajich <jason@chg.mc.duke.edu>
-Himlar Lapp <hilmar.lapp@pharma.novartis.com>
+Hilmar Lapp <hilmar.lapp@pharma.novartis.com>
 
 Describe contact details here
 
@@ -69,7 +68,7 @@ use Bio::Root::RootI;
 
 use strict;
 
-# Object preamble - inheriets from Bio::Root::RootI
+# Object preamble - inherits from Bio::Root::RootI
 use Carp;
 
 @ISA = qw(Bio::Root::RootI);
