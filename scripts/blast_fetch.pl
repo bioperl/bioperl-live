fadf35a0d251c81ea5abf4dd1e50aabf5e80d367
jason <jason@bioperl.org>
2000-09-05 13:07:30 +0000

    more versitile blast parse + download seqs example script
    svn path=/bioperl-live/trunk/; revision=1483

--- scripts/blast_fetch.pl
+++ scripts/blast_fetch.pl
@@ -1,21 +1,60 @@
+#!/usr/local/bin/perl -w
 use Bio::Tools::Blast;
 use Bio::DB::GenBank;
+use Bio::DB::GenPept;
 use Bio::SeqIO;
+use Getopt::Long;
 
-die("usage: blast_fetch blastfile outputfile") if( @ARGV != 2 );
-my $db = new Bio::DB::GenBank;
-my $blast = Bio::Tools::Blast->new(-file   =>@ARGV[0],
+use strict;
+use vars qw($usage $moltype $file $db $blastfile );
+$usage = "usage: blast_fetch [ -moltype protein|nuc ] blastfile\n";
+
+$moltype = 'DNA';
+my $result = GetOptions( 'help' => sub { print STDERR "$usage"; exit; },
+			 'moltype=s' => sub { $moltype = &assign_type(pop @_)},
+			 );
+( $blastfile) = @ARGV;
+
+if( $moltype eq 'DNA' ) {
+    $db = new Bio::DB::GenBank;
+} elsif( $moltype eq 'PROT' ) {
+    $db = new Bio::DB::GenPept;
+} else { 
+    die $usage;
+}
+
+my $blast = Bio::Tools::Blast->new(-file   =>$blastfile,
 					-signif => 1e-5,
 					-parse  => 1,
 					-stats  => 1,
 					-check_all_hits => 1,
 					);
-my $seqio = new Bio::SeqIO(-format=>'Fasta', -file=>">$ARGV[1]");
 my ($an,$seq);
 foreach my $hit ( $blast->hits) {
     (undef,$an) = split(/\|/, $hit->name);
-    $seq = $db->get_Seq_by_id($an);
-    if( defined $seq ) {
-	$seqio->write_seq($seq);
-    } 
+    if( !defined $an || $an eq '' ) {
+	print STDERR "could not find accession number in line ", $hit->name(), ", skipping...\n";
+	next;
+    }
+    eval { 
+	$seq = $db->get_Seq_by_id($an);
+	if( defined $seq ) {
+	    my $seqio = new Bio::SeqIO(-format=>'swiss', -file=>">$an.sp");
+	    $seqio->write_seq($seq);	    
+	} 
+    };
+    if( $@ ) {
+	print STDERR $@, "\n", " for name, an $an ", $hit->name, "\n";	
+    }
+}
+
+sub assign_type {
+    my $type = shift @_;
+    if( $type =~ /[dr]na/i ) {
+	return 'DNA';
+    } elsif( $type =~ /prot/i ) {
+	return 'PROT';
+    } else {
+	return '';
+    }
 }
