a714f6e94f276ecd1b8adc9695b0f4d8fa92cf80
jason <jason@bioperl.org>
2000-10-02 16:12:07 +0000

    fixed mailing list address
    svn path=/bioperl-live/trunk/; revision=1587

--- Bio/SeqIO/fasta.pm
+++ Bio/SeqIO/fasta.pm
@@ -34,9 +34,9 @@ and other Bioperl modules. Send your comments and suggestions preferably
  to one of the Bioperl mailing lists.
 Your participation is much appreciated.
 
-  vsns-bcd-perl@lists.uni-bielefeld.de          - General discussion
-  vsns-bcd-perl-guts@lists.uni-bielefeld.de     - Technically-oriented discussion
-  http://bio.perl.org/MailList.html             - About the mailing lists
+  bioperl-l@bioperl.org            - General discussion
+  bioperl-guts-l@bioperl.org       - Technically-oriented discussion
+  http://bioperl.org/MailList.shtml - About the mailing lists
 
 =head2 Reporting Bugs
 
