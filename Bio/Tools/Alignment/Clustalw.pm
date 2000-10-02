e76adc19595c52dbb99012e4b796052484e47592
peter <peter@bioperl.org>
2000-10-02 19:29:03 +0000

    Initial commit of clustalw wrapper files
    svn path=/bioperl-live/trunk/; revision=1593

---
+++ Bio/Tools/Alignment/Clustalw.pm
@@ -0,0 +1,539 @@
+
+#
+# BioPerl module for Bio::Tools::Alignment::Clustalw
+#
+# Cared for by
+#
+# Copyright Peter Schattner
+#
+# You may distribute this module under the same terms as perl itself
+
+# POD documentation - main docs before the code
+
+=head1 NAME
+
+Bio::Tools::Alignment::Clustalw - Object for the calculation of a multiple sequence alignment
+from a set of unaligned sequences or alignments using the Clustalw program
+
+=head1 SYNOPSIS
+
+#  Build a clustalw alignment factory
+	@params = ('ktuple' => 2, 'matrix' => 'BLOSUM');
+	$factory = Bio::Tools::Alignment::Clustalw->new(@params);
+
+#  Pass the factory a list of sequences to be aligned.	
+	$inputfilename = 't/cysprot.fa';
+	$aln = $factory->align($inputfilename); # $aln is a SimpleAlign object.
+or
+	$seq_array_ref = \@seq_array;  # where @seq_array is an array of Bio::Seq objects
+	$aln = $factory->align($seq_array_ref);
+
+#  Or one can pass the factory a pair of (sub)alignments to be aligned against each other, e.g.:	
+	$aln = $factory->profile_align($aln1,$aln2); # where $aln1 and $aln2 are Bio::SimpleAlign objects.
+
+#  Or one can pass the factory an alignment and one or more unaligned
+#  sequences to be added to the alignment. For example: 	
+	$aln = $factory->profile_align($aln1,$seq); # $seq is a Bio::Seq object.
+
+There are various additional options and input formats available.  See the DESCRIPTION section
+that follows for additional details.
+
+=head1 DESCRIPTION
+
+Note: this DESCRIPTION only documents the (Bio)perl interface to Clustalw.  Clustalw, itself,
+is a large & complex program - for more information regarding clustalw, please see the
+clustalw documentation which accompanies the clustalw distribution. Clustalw is available
+from (among others) ftp://ftp.ebi.ac.uk/pub/software/. Clustalw.pm has been tested so far only
+under Linux. I expect that it should also work under other Unix systems.
+However, since the module is currently implemented using (unix) system
+calls, extensive modification may be necessary before Clustalw.pm would work
+under non-Unix operating systems (eg Windows, MacOS).  Clustalw.pm has only been tested
+using version 1.8 of clustalw.  Compatibility with earlier versions of the clustalw program
+is currently unknown. Before running Clustalw.pm successfully
+it will be necessary: to install clustalw on your system, to edit the variable $clustdir in
+Clustalw.pm to point to the clustalw program, and to ensure that users have execute
+privilieges for the clustalw program.
+
+Bio::Tools::Alignment::Clustalw.pm: is an object for performing a multiple sequence alignment
+from a set of unaligned sequences and/or sub-alignments by means of the clustalw program.
+
+Initially, a clustalw "factory object" is created. Optionally, the factory may be
+passed most of the parameters or switches of the clustalw program, e.g.:
+
+	@params = ('ktuple' => 2, 'matrix' => 'BLOSUM');
+	$factory = Bio::Tools::Alignment::Clustalw->new(@params);
+
+Any parameters not explicitly set will remain as the defaults of the clustalw program.
+Additional parameters and switches (not available in clustalw) may also be set.  Currently,
+the only such parameter is "quiet", which when set to a non-zero value, suppresses clustalw's
+terminal output.
+
+Not all clustalw parameters are supported at this stage.  Some parameters
+are not relevant - eg "output" and "outputfile" - since the clustalw output is returned in a
+BioPerl Bio::SimpleAlign object which can then be printed and/or saved in multiple formats using
+the AlignIO.pm module. (Note: as currently implemented, Clustalw.pm also creates .msf and
+.dnd files. These files may be removed explicitly by the calling script.  In the future,
+automatic removal of .dnd and .msf files by Clustalw.pm may be included.)
+
+Other parameters and features (such as those corresponding to tree
+production) have simply not been implemented yet in Perl format.
+
+Alignment parameters can be changed and/or examined at any time after the factory has been created.
+The program checks that any parameter/switch being set/read is valid.  However, currently no
+additional checks are included to check that parameters are of the proper type (eg string or numeric)
+or that their values are within the proper range.
+As an example, to change the value of the clustalw parameter ktuple to 3 and subsequently to check its
+value one would write:
+
+	$ktuple = 3;
+	$factory->ktuple($ktuple);
+ 	$get_ktuple = $factory->ktuple();
+
+
+Once the factory has been created and the appropriate parameters set, one can call the method align()
+to align a set of unaligned sequences, or call profile_align() to add one or more sequences or
+a second alignment to an initial alignment.
+
+Input to align() may consist of a set of unaligned sequences in the form of the name
+of file containing the sequences. For example,
+	
+	$inputfilename = 't/cysprot.fa';
+	$aln = $factory->align($inputfilename);
+
+Alternately one can create an array of Bio::Seq objects somehow
+
+	$str = Bio::SeqIO->new(-file=> 't/cysprot.fa', '-format' => 'Fasta');
+	@seq_array =();
+	while ( my $seq = $str->next_seq() ) {push (@seq_array, $seq) ;}
+
+and pass the factory a reference to that array
+
+	$seq_array_ref = \@seq_array;
+	$aln = $factory->align($seq_array_ref);
+
+In either case, align() returns a reference to a SimpleAlign object which can then be displayed,
+stored, or converted to a UnivAlign object for further manipulation.
+
+Once an initial alignment exists, one can pass the factory additional sequence(s) to be added
+(ie aligned) to the original alignment.  The alignment can be passed as either an alignment file
+or a Bio:SimpleAlign object.  The unaligned sequence(s) can be passed as a filename
+or as an array of BioPerl sequence objects or as a single BioPerl Seq object.
+For example (to add a single sequence to an alignment),	
+
+	$str = Bio::AlignIO->new(-file=> 't/cysprot1a.msf');
+	$aln = $str->next_aln();
+	$str1 = Bio::SeqIO->new(-file=> 't/cysprot1b.fa');
+	$seq = $str1->next_seq();
+	$aln = $factory->profile_align($aln,$seq);
+
+In either case, profile_align() returns a reference to a SimpleAlign object
+containing a new SimpleAlign object of the alignment with the additional sequence(s)
+added in.
+
+Finally one can pass the factory a pair of (sub)alignments to be aligned against each other.
+The alignments can be passed in the form of either a pair of alignment files or a pair of
+Bio:SimpleAlign objects. For example,	
+
+	$profile1 = 't/cysprot1a.msf';
+	$profile2 = 't/cysprot1b.msf';
+	$aln = $factory->profile_align($profile1,$profile2);
+or
+	$str1 = Bio::AlignIO->new(-file=> 't/cysprot1a.msf');
+	$aln1 = $str1->next_aln();
+	$str2 = Bio::AlignIO->new(-file=> 't/cysprot1b.msf');
+	$aln2 = $str2->next_aln();
+	$aln = $factory->profile_align($aln1,$aln2);
+
+In either case, profile_align() returns a reference to a SimpleAlign object
+containing an (super)alignment of the two input alignments.
+
+For more examples of syntax and use of Clustalw.pm, the user is encouraged to run the script
+Clustalw.t is the bioperl/t directory.
+
+Note: Clustalw.pm is still under development. Various features of the clustalw program have not
+yet been implemented.  If you would like that a specific clustalw feature be added to this perl
+interface, let me know.
+
+=head1 DEVELOPERS NOTES
+
+
+=head1 STILL TO WRITE
+
+
+=head1 FEEDBACK
+
+=head2 Mailing Lists
+
+User feedback is an integral part of the evolution of this
+and other Bioperl modules. Send your comments and suggestions preferably
+ to one of the Bioperl mailing lists.
+Your participation is much appreciated.
+
+  vsns-bcd-perl@lists.uni-bielefeld.de          - General discussion
+  vsns-bcd-perl-guts@lists.uni-bielefeld.de     - Technically-oriented discussion
+  http://bio.perl.org/MailList.html             - About the mailing lists
+
+=head2 Reporting Bugs
+
+Report bugs to the Bioperl bug tracking system to help us keep track
+ the bugs and their resolution.
+ Bug reports can be submitted via email or the web:
+
+  bioperl-bugs@bio.perl.org
+  http://bio.perl.org/bioperl-bugs/
+
+=head1 AUTHOR -  Peter Schattner
+
+Email schattner@alum.mit.edu
+
+=head1 APPENDIX
+
+The rest of the documentation details each of the object methods. Internal methods are usually preceded with a _
+
+=cut
+
+
+
+package Bio::Tools::Alignment::Clustalw;
+
+use vars qw($AUTOLOAD @ISA);
+use strict;
+use Bio::Seq;
+use Bio::SeqIO;
+use Bio::SimpleAlign;
+use Bio::AlignIO;
+use Bio::Root::Object;
+
+@ISA = qw(Bio::Root::Object);
+
+# You will need to edit following line to point to your location of the clustalw executable
+my $clustdir =  '/home/peter/clustalw1.8/';
+my $program =   $clustdir.'clustalw' ;
+
+unless (-x $program) {
+	warn "Clustalw program not found or not executable. \n  Clustalw can be obtained from ftp://ftp.ebi.ac.uk/pub/software/ \n";
+}
+# Object preamble - inherits from Bio::Root::Object
+
+
+
+#***Pairwise alignments:***
+#	KTUPLE      	#:(=n) word size
+#	TOPDIAGS  	#:(=n) number of best diagonals
+#	WINDOW   	#:(=n) window around best diagonals
+#	PAIRGAP   	#:(=n)gap penalty
+
+#**Multiple alignments:***
+#	FIXEDGAP  	#:(=n)fixed length gap pen.
+#	FLOATGAP  	#:(=n)variable length gap pen.
+#	MATRIX     	#:= PAM100 or ID or file name. The default weight matrix
+			#	for proteins is PAM 250.	
+#	TYPE	 	#:(=p or d)type is protein or DNA.   This allows you to 	
+			#	explicitly overide the programs attempt at guessing
+			#	the type of the sequence.  It is only useful if you
+			#	are using sequences with a VERY strange composition.
+#	OUTPUT     	#:= GCG or PHYLIP or PIR.  The default output is 	
+			#	Clustal format.
+			## NB OUTFILE option not currently supported.  All output
+			## currently is in the form of SimpleAlign objects
+#	TRANSIT     	#:transitions not weighted.  The default is to weight
+			#	transitions as more favourable than other mismatches
+			#	in DNA alignments.  This switch makes all nucleotide
+			#	mismatches equally weighted.
+
+my @clustal_params = qw(KTUPLE TOPDIAGS WINDOW PAIRGAP FIXEDGAP
+                   FLOATGAP MATRIX TYPE	TRANSIT DNAMATRIX
+                   GAPOPEN GAPEXT MAXDIV GAPDIST HGAPRESIDUES PWMATRIX
+                   PWDNAMATRIX PWGAPOPEN PWGAPEXT SCORE TRANSWEIGHT
+                   SEED HELIXGAP OUTORDER STRANDGAP LOOPGAP TERMINALGAP
+                   HELIXENDIN HELIXENDOUT STRANDENDIN STRANDENDOUT);
+
+my @clustalw_switches = qw(HELP CHECK OPTIONS NEGATIVE NOWEIGHTS ENDGAPS
+                        NOPGAP NOHGAP NOVGAP KIMURA TOSSGAPS);
+
+my @other_switches = qw(QUIET);
+my %ok_field;
+
+
+# Authorize attribute fields
+foreach my $attr ( @clustal_params, @clustalw_switches, @other_switches ) { $ok_field{$attr}++; }
+
+return 1;     # Needed to keep compiler happy
+
+# new() is inherited from Bio::Root::Object
+
+# _initialize is where the heavy stuff will happen when new is called
+
+sub _initialize {
+    my($self,@args) = @_;
+    my ($attr, $value);
+    my $make = $self->SUPER::_initialize;
+    while (@args)  {
+	$attr =  shift @args;
+	$value =  shift @args;
+	$self->$attr($value);
+    }
+
+    return $make; # success - we hope!
+}
+
+
+sub AUTOLOAD {
+    my $self = shift;
+    my $attr = $AUTOLOAD;
+    $attr =~ s/.*:://;
+    $attr = uc $attr;
+    $self->throw("Unallowed parameter: $attr !") unless $ok_field{uc $attr};
+
+    $self->{uc $attr} = shift if @_;
+    return $self->{uc $attr};
+}
+
+
+
+
+
+=head2  exists_clustal()
+
+ Title   : exists_clustal
+ Usage   : $clustalfound = Bio::Tools::Alignment::Clustalw->exists_clustal()
+Function: Determine whether clustalw program can be found on current host
+ Example :
+ Returns : 1 if clustalw program found at expected location, 0 otherwise.
+ Args    :  none
+
+
+=cut
+
+# purpose of this method is to enable a script to determine whether the clustalw
+# is properly installed on the system.
+sub exists_clustal {
+if (-x $program) {return 1;}
+return 0;
+}
+
+=head2  align
+
+ Title   : align
+ Usage   :
+	$inputfilename = 't/cysprot.fa';
+	$aln = $factory->align($inputfilename);
+or
+	$seq_array_ref = \@seq_array; @seq_array is array of Seq objs
+	$aln = $factory->align($seq_array_ref);
+Function: Perform a multiple sequence alignment
+ Example :
+ Returns : Reference to a SimpleAlign object containing the sequence alignment.
+ Args    : Name of a file containing a set of unaligned fasta sequences
+         or else an array of references to Bio::Seq objects.
+Throws an exception if argument is not either a string (eg a filename) or a reference
+to an array of Bio::Seq objects.  If argument is string, throws
+exception if file corresponding to string name can not be found.
+
+
+=cut
+
+sub align {
+
+my $self = shift;
+my $input = shift;
+my ($temp,$infilename, $seq);
+my ($attr, $value, $switch);
+
+
+# Create input file pointer
+$infilename = &_setinput($input);
+if (!$infilename) {$self->throw("Bad input data: $input !");}
+
+# Create parameter string to pass to clustalw program
+my $param_string = &_setparams($self);
+
+# run clustalw
+my $aln = &_runclustalw('align', $infilename, $param_string);
+}
+#################################################
+
+=head2  profile_align
+
+ Title   : profile_align
+ Usage   :
+
+
+Function: Perform an alignment of 2 (sub)alignments
+ Example :
+ Returns : Reference to a SimpleAlign object containing the (super)alignment.
+ Args    : Names of 2 files containing the subalignments
+         or references to 2 Bio::SimpleAlign objects.
+
+Throws an exception if arguments are not either strings (eg filenames) or references
+to SimpleAlign objects.
+
+
+=cut
+
+sub profile_align {
+
+my $self = shift;
+my $input1 = shift;
+my $input2 = shift;
+my ($temp,$infilename1,$infilename2,$input,$seq);
+
+
+
+# Create input file pointers
+$infilename1 = &_setinput($input1,1);
+$infilename2 = &_setinput($input2,2);
+if (!$infilename1 || !$infilename2) {$self->throw("Bad input data: $input1 or $input2 !");}
+
+
+# Create parameter string to pass to clustalw program
+my $param_string = &_setparams($self);
+
+# run clustalw
+my $aln = &_runclustalw('profile-aln', $infilename1, $infilename2, $param_string);
+
+}
+#################################################
+
+=head2  _runclustalw
+
+ Title   :  _runclustalw
+ Usage   :  Internal function, not to be called directly	
+Function:   makes actual system call to clustalw program
+ Example :
+ Returns : nothing; clustalw output is written to a temporary file ./clustalw.tmp
+ Args    : Name of a file containing a set of unaligned fasta sequences
+         and hash of parameters to be passed to clustalw
+
+
+=cut
+
+sub _runclustalw {
+
+my ($instring, $infilename, $infile1, $infile2);
+my $command = shift;
+if ($command =~ /align/) {
+	$infilename = shift;
+	$instring =  "-infile=$infilename";
+}
+if ($command =~ /profile/) {
+	$infile1 = shift;
+	$infile2 = shift;
+	$instring =  "-profile1=$infile1  -profile2=$infile2";
+	$command = '-profile';
+}
+my $param_string = shift;
+
+my $outfile=   "./clustalw.tmp" ;
+
+my $commandstring = "$program"." $command"." $instring".
+			" -output=gcg"." -outfile=$outfile". " $param_string";
+
+my $status = system($commandstring);
+
+  die "Clustalw call crashed: $? \n" unless $status==0;
+
+
+# retrieve alignment (Note: MSF format for AlignIO = GCG format of clustalw)
+my $in  = Bio::AlignIO->new(-file => $outfile, '-format' => 'MSF');
+my $aln = $in->next_aln();
+return $aln;
+}
+
+
+=head2  _setinput()
+
+ Title   :  _setinput
+ Usage   :  Internal function, not to be called directly	
+Function:   Create input file for clustalw program
+ Example :
+ Returns : name of file containing clustalw data input
+ Args    : Seq or Align object reference or input file name
+
+
+=cut
+
+sub _setinput {
+  my ($input, $infilename, $seq, $temp, $suffix);
+  $input = shift;
+  $suffix = shift;   #used to distinguish alignment files
+#  If $input is not a reference it better be the name of a file with the sequence/
+#  alignment data...
+	unless (ref $input) {
+		# check that file exists or throw
+		$infilename = $input;
+		unless (-e $input) {return 0;}
+		return $infilename;
+	}
+#  $input may be an array of BioSeq objects...
+	if (ref($input) eq "ARRAY") {
+        #  Open temporary file for both reading & writing of BioSeq array
+		$infilename = 'tmp.fa';
+                $temp =  Bio::SeqIO->new(-file=> ">$infilename", '-format' => 'Fasta');
+   		foreach $seq (@$input) {
+		  unless (ref($seq) eq "Bio::Seq")
+   	            {return 0;}
+		  $temp->write_seq($seq);
+		}
+		return $infilename;
+ 	  }
+#  $input may be a SimpleAlign object.
+	  if (ref($input) eq "Bio::SimpleAlign") {
+       #  Open temporary file for both reading & writing of SimpleAlign object
+		$infilename = 'tmp1.fa' if ($suffix ==1);
+		$infilename = 'tmp2.fa' if ($suffix ==2);
+#		$infilename = "tmp$suffix.fa";
+                $temp =  Bio::AlignIO->new(-file=> ">$infilename", '-format' => 'Fasta');
+                $temp->write_aln($input);
+		return $infilename;
+ 	  }
+
+#  or $input may be a single BioSeq object (to be added to a previous alignment)
+	if (ref($input) eq "Bio::Seq" && $suffix==2) {
+        #  Open temporary file for both reading & writing of BioSeq object
+		$infilename = 'tmp.fa';
+                $temp =  Bio::SeqIO->new(-file=> ">$infilename", '-format' => 'Fasta');
+		$temp->write_seq($input);
+		return $infilename;
+        }
+return 0;
+}
+
+
+=head2  _setparams()
+
+ Title   :  _setparams
+ Usage   :  Internal function, not to be called directly	
+Function:   Create parameter inputs for clustalw program
+ Example :
+ Returns : parameter string to be passed to clustalw during align or profile_align
+ Args    : name of calling object
+
+=cut
+
+sub _setparams {
+my ($attr, $value, $self);
+
+$self = shift;
+
+my $param_string = "";
+ for  $attr ( @clustal_params ) {
+	$value = $self->$attr();
+	next unless (defined $value);
+	my $attr_key = lc $attr;      #put params in format expected by clustalw
+	$attr_key = '-'.$attr_key;
+	$param_string .= '"'.$attr_key.'='.$value.'",';
+ }
+
+ for  $attr ( @clustalw_switches) {
+	$value = $self->$attr();
+	next unless ($value);
+	my $attr_key = lc $attr;      #put switches in format expected by clustalw
+	$attr_key = '-'.$attr_key;
+	$param_string .= '"'.$attr_key.'",';
+ }
+
+if ($self->quiet()) { $param_string .= '  >/dev/null';}
+
+return $param_string;
+}
---
+++ t/Clustalw.t
@@ -0,0 +1,171 @@
+## Bioperl Test Harness Script for Modules
+##
+
+
+# Before `make install' is performed this script should be runnable with
+# `make test'. After `make install' it should work as `perl test.t'
+
+#-----------------------------------------------------------------------
+## perl test harness expects the following output syntax only!
+## 1..3
+## ok 1  [not ok 1 (if test fails)]
+## 2..3
+## ok 2  [not ok 2 (if test fails)]
+## 3..3
+## ok 3  [not ok 3 (if test fails)]
+##
+## etc. etc. etc. (continue on for each tested function in the .t file)
+#-----------------------------------------------------------------------
+
+
+## We start with some black magic to print on failure.
+BEGIN { $| = 1; print "1..9\n";
+	use vars qw($loaded); }
+
+END {print "not ok 1\n" unless $loaded;}
+
+use Bio::Tools::Alignment::Clustalw;
+use Bio::SimpleAlign;
+use Bio::AlignIO;
+use Bio::SeqIO;
+use strict;
+
+$loaded = 1;
+print "ok 1\n";    # 1st test passes.
+
+
+## End of black magic.
+##
+## Insert additional test code below but remember to change
+## the print "1..x\n" in the BEGIN block to reflect the
+## total number of tests that will be run. 
+
+
+## Create clustalw alignment factory
+my @params = ('ktuple' => 2, 'matrix' => 'BLOSUM');
+my  $factory = Bio::Tools::Alignment::Clustalw->new(@params);
+
+if( $factory ) {
+    print "ok 2\n";
+} else {
+    print "not ok 2 , couldn't create alignment factory \n";	
+}
+
+
+## Set parameter test
+
+my $ktuple = 3;
+$factory->ktuple($ktuple);
+
+my $new_ktuple = $factory->ktuple();
+
+if( $new_ktuple == 3) {
+    print "ok 3\n";
+} else {
+    print "not ok 3 , couldn't set factory parameter\n";	
+}
+
+
+## Get parameter test
+
+my $what_matrix = $factory->matrix();
+
+if( $what_matrix eq 'BLOSUM') {
+    print "ok 4\n";
+} else {
+    print "not ok 4 , couldn't get factory parameter\n";	
+}
+
+
+## Alignment test (from fasta file)
+my $bequiet = 1;
+$factory->quiet($bequiet);  # Suppress clustal messages to terminal
+
+my $inputfilename = 't/cysprot.fa';
+my $aln;
+
+# If the clustalw program isn't found and executable at the expected location,
+# there is no point in executing the remaining tests...
+
+my $clustal_present = Bio::Tools::Alignment::Clustalw->exists_clustal();
+unless ($clustal_present) {
+	warn "Clustalw program not found. Skipping tests 5 to 9.\n";
+    	print "ok 5\n", "ok 6\n", "ok 7\n", "ok 8\n", "ok 9\n";
+	exit 0;
+}
+$aln = $factory->align($inputfilename);
+if( $aln->{order}->{'0'} eq 'CATH_HUMAN-1-335' ) {
+    print "ok 5\n";
+} else {
+    print "not ok 5 , failed clustalw alignment using input file\n";	
+}
+
+## Alignment test (from BioSeq array)
+
+my $str = Bio::SeqIO->new(-file=> 't/cysprot.fa', '-format' => 'Fasta');
+my @seq_array =();
+
+while ( my $seq = $str->next_seq() ) {
+	push (@seq_array, $seq) ;
+    }
+
+my $seq_array_ref = \@seq_array;
+
+$aln = $factory->align($seq_array_ref);
+
+if( $aln->{order}->{'0'} eq 'CATH_HUMAN-1-335' ) {
+    print "ok 6\n";
+} else {
+    print "not ok 6 , failed clustalw alignment using BioSeq array\n";	
+}
+
+
+
+## Profile alignment test (from alignment files)
+
+	
+my $profile1 = 't/cysprot1a.msf';
+my $profile2 = 't/cysprot1b.msf';
+$aln = $factory->profile_align($profile1,$profile2);
+
+if( $aln->{order}->{'1'} eq 'CATH_HUMAN-1-335' ) {
+    print "ok 7\n";
+} else {
+    print "not ok 7 , failed clustalw profile alignment using input file\n";	
+}
+
+## Profile alignment test (from SimpleAlign objects)
+
+my $str1 = Bio::AlignIO->new(-file=> 't/cysprot1a.msf');
+my $aln1 = $str1->next_aln();
+my $str2 = Bio::AlignIO->new(-file=> 't/cysprot1b.msf');
+my $aln2 = $str2->next_aln();
+
+$aln = $factory->profile_align($aln1,$aln2);
+
+if( $aln->{order}->{'1'} eq 'CATH_HUMAN-1-335' ) {
+    print "ok 8\n";
+} else {
+    print "not ok 8 , failed clustalw profile alignment using SimpleAlign input\n";	
+}
+
+
+## Test aligning (single) new sequence to an alignment
+
+$str1 = Bio::AlignIO->new(-file=> 't/cysprot1a.msf');
+$aln1 = $str1->next_aln();
+$str2 = Bio::SeqIO->new(-file=> 't/cysprot1b.fa');
+my $seq = $str2->next_seq();
+$aln = $factory->profile_align($aln1,$seq);
+
+
+if( $aln->{order}->{'1'} eq 'CATH_HUMAN-1-335' ) {
+    print "ok 9\n";
+} else {
+    print "not ok 9 , failed adding new sequence to alignment\n";	
+}
+
+
+
+
+
