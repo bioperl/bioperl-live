93112ec3179e3ab0320afb7112549071510ad529
lapp <lapp@bioperl.org>
2001-01-31 19:00:40 +0000

    Initial revision.
    svn path=/bioperl-live/trunk/; revision=1934

--- Bio/Seq/RichSeqI.pm
+++ Bio/Seq/RichSeqI.pm
@@ -38,8 +38,7 @@ and other Bioperl modules. Send your comments and suggestions preferably
  to one of the Bioperl mailing lists.
 Your participation is much appreciated.
 
-  vsns-bcd-perl@lists.uni-bielefeld.de          - General discussion
-  vsns-bcd-perl-guts@lists.uni-bielefeld.de     - Technically-oriented discussion
+  bioperl-l@bioperl.org                 - General discussion
   http://bio.perl.org/MailList.html             - About the mailing lists
 
 =head2 Reporting Bugs
