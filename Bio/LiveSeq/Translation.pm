2af22100c3a5d382470c37d62ee597858ce97f7b
heikki <heikki@bioperl.org>
2001-03-28 10:50:04 +0000

    commented out verbose warning
    svn path=/bioperl-live/branches/branch-07/; revision=2215

--- Bio/LiveSeq/Translation.pm
+++ Bio/LiveSeq/Translation.pm
@@ -232,7 +232,7 @@ sub position {
     return (0);
   } elsif ($position > 0) {
     if ($modulus != 1) {
-      carp "Attention! Label $label is not in frame (1st position of triplet) with protein";
+      #carp "Attention! Label $label is not in frame (1st position of triplet) with protein";
       if ($modulus == 2) {
 	return ($position / 3 + 1);
       } else { # i.e. modulus == 0
