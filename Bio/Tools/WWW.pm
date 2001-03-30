ee4c0efa322dcd6b7564d1a06a6b704904e95884
lapp <lapp@bioperl.org>
2001-03-30 07:23:45 +0000

    Corrected mailing lists.
    svn path=/bioperl-live/branches/branch-07/; revision=2230

--- Bio/Tools/WWW.pm
+++ Bio/Tools/WWW.pm
@@ -135,9 +135,8 @@ User feedback is an integral part of the evolution of this and other Bioperl mod
 Send your comments and suggestions preferably to one of the Bioperl mailing lists.
 Your participation is much appreciated.
 
-    vsns-bcd-perl@lists.uni-bielefeld.de          - General discussion
-    vsns-bcd-perl-guts@lists.uni-bielefeld.de     - Technically-oriented discussion
-    http://bio.perl.org/MailList.html             - About the mailing lists
+  bioperl-l@bioperl.org                  - General discussion
+  http://www.bioperl.org/MailList.shtml  - About the mailing lists
 
 =head2 Reporting Bugs
 
