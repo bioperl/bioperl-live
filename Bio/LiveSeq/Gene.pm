84b945ef8c0e4b9a856de90983663d7cc3396dbb
heikki <heikki@bioperl.org>
2001-04-03 08:49:23 +0000

    doc changes from 07 branch
    svn path=/bioperl-live/trunk/; revision=2252

--- Bio/LiveSeq/Gene.pm
+++ Bio/LiveSeq/Gene.pm
@@ -12,10 +12,11 @@
 
 =head1 NAME
 
-  Bio::LiveSeq::Gene - Range abstract class for LiveSeq
+Bio::LiveSeq::Gene - Range abstract class for LiveSeq
 
 =head1 SYNOPSIS
 
+  # documentation needed
 
 =head1 DESCRIPTION
 
