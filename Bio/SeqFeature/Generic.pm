3d52968acd318f641d3d4af37f290d2ab64d49ce
jason <jason@bioperl.org>
2001-04-17 20:35:56 +0000

    bug #947 fixed
    svn path=/bioperl-live/trunk/; revision=2306

--- Bio/SeqFeature/Generic.pm
+++ Bio/SeqFeature/Generic.pm
@@ -815,7 +815,6 @@ sub _from_gff_string {
 
 sub _expand_region {
     my ($self, $feat) = @_;
-
     if(! $feat->isa('Bio::SeqFeatureI')) {
 	$self->warn("$feat does not implement Bio::SeqFeatureI");
     }
@@ -825,10 +824,10 @@ sub _expand_region {
 	$self->end($feat->end());
 	$self->strand($feat->strand) unless defined($self->strand());
     } else {
-	my ($start, $end, $strand) = $self->union($feat);
-	$self->start($start);
-	$self->end($end);
-	$self->strand($strand);
+	my $range = $self->union($feat);
+	$self->start($range->start);
+	$self->end($range->end);
+	$self->strand($range->strand);
     }
 }
 
