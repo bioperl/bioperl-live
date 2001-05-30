ffb70e2a0de75fe9ccf64ed7587b78a208edc0d2
sac <sac@bioperl.org>
2001-05-30 21:27:23 +0000

    Fixed bug in sub print_table: Call to _table_custom().
    svn path=/bioperl-live/trunk/; revision=2384

--- examples/blast/blast_config.pl
+++ examples/blast/blast_config.pl
@@ -603,7 +603,7 @@ sub print_table {
 		       print $bo->table($opt_desc); last SWITCH;};
 	    /2/ && do{ print $bo->table_labels_tiled($opt_desc) if not $_printed_labels; 
 		       print $bo->table_tiled($opt_desc); last SWITCH;};
-	    /3/ && do{ print $_table_custom($bo); last SWITCH;};
+	    /3/ && do{ print _table_custom($bo); last SWITCH;};
 	}
     } else {
 	push @nohits, $bo->name.", File: ".($bo->file || '<STDIN>');
