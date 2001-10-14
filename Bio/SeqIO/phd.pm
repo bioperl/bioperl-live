870231449424ab312934307dac95707fa7265e96
jason <jason@bioperl.org>
2001-10-14 21:49:01 +0000

    simplify argument scheme and allow use of Root::IO print/read facilities
    svn path=/bioperl-live/trunk/; revision=2748

--- Bio/SeqIO/phd.pm
+++ Bio/SeqIO/phd.pm
@@ -79,8 +79,8 @@ use Bio::Seq::SeqWithQuality;
  Returns : Bio::Seq::SeqWithQuality object
  Args    : NONE
  Notes   : This is really redundant because AFAIK there is no such thing as
-	a .phd file that contains more then one sequence. It is included as
-	an interface thing and for consistency.
+  	   a .phd file that contains more then one sequence. It is included as
+	   an interface thing and for consistency.
 
 =cut
 
@@ -95,7 +95,7 @@ sub next_seq {
 =head2 _next_phd()
 
  Title   : _next_phd()
- Usage   : $seq = $stream->_next_phd() (but don't do this. use next_seq()
+ Usage   : $seq = $stream->_next_phd() (but don\'t do this. use next_seq()
 	instead.)
  Function: returns the next phred sequence in the stream
  Returns : Bio::Seq::SeqWithQuality object
@@ -120,80 +120,63 @@ sub _next_phd {
 =cut
 
 sub next_primary_phd {
-		# print("CSM next_primary_phd!\n");
-	my( $self, $as_next_qual ) = @_;
-	my $entry;
-	# if (!($entry = $self->_readline)) { return; }
-	my ($qual,$seq);
-        my $in_dna = 0;
-        my $base_number = 0;
-	my $done;
-	my (@lines, @bases, @qualities,$id);
-	while ($entry = $self->_readline) {
-		return if (!$entry);
-		chomp($entry);
-		if ($entry =~ /^BEGIN_SEQUENCE\s+(\S+)/) {
-			$self->warn("Setting id to $1\n") if($self->verbose > 0);
-			$id = $1;
-			$entry = $self->_readline();
-		}
-		if ($entry =~ /^BEGIN_DNA/) {
-			$entry =~ /^BEGIN_DNA/;
-			$in_dna = 1;
-			$entry = $self->_readline();
-		}
-		if ($entry =~ /^END_DNA/) {
-			$in_dna = 0;
-		}
-		if ($entry =~ /^END_SEQUENCE/) {
-		}
-		if (!$in_dna) { next;  }
-		$entry =~ /(\S+)\s+(\S+)/;
-		push @bases,$1;
-		push @qualities,$2;
-		push(@lines,$entry);
+    # print("CSM next_primary_phd!\n");
+    my( $self, $as_next_qual ) = @_;
+    my $entry;
+    # if (!($entry = $self->_readline)) { return; }
+    my ($qual,$seq);
+    my $in_dna = 0;
+    my $base_number = 0;
+    my $done;
+    my (@lines, @bases, @qualities,$id);
+    while ($entry = $self->_readline) {
+	return if (!$entry);
+	chomp($entry);
+	if ($entry =~ /^BEGIN_SEQUENCE\s+(\S+)/) {
+	    $self->debug("Setting id to $1\n");
+	    $id = $1;
+	    $entry = $self->_readline();
 	}
-	$self->warn("Creating objects with id = $id\n") if( $self->verbose > 0);
-	my $swq = Bio::Seq::SeqWithQuality->new(
-					-seq	=>	join('',@bases),
-					-qual        => \@qualities,
-					-id         => $id,
-					-primary_id => $id,
-					-display_id => $id,
-                              );
-	return $swq;
+	if ($entry =~ /^BEGIN_DNA/) {
+	    $entry =~ /^BEGIN_DNA/;
+	    $in_dna = 1;
+	    $entry = $self->_readline();
+	}
+	if ($entry =~ /^END_DNA/) {
+	    $in_dna = 0;
+	}
+	if ($entry =~ /^END_SEQUENCE/) {
+	}
+	if (!$in_dna) { next;  }
+	$entry =~ /(\S+)\s+(\S+)/;
+	push @bases,$1;
+	push @qualities,$2;
+	push(@lines,$entry);
+    }
+    $self->debug("Creating objects with id = $id\n");
+    my $swq = Bio::Seq::SeqWithQuality->new(
+					    -seq        => join('',@bases),
+					    -qual       => \@qualities,
+					    -id         => $id,
+					    -primary_id => $id,
+					    -display_id => $id,
+					    );
+    return $swq;
 }
 
-=head2 _initialize()
+=head2 write_seq
 
- Title   : _initialize()
- Usage   :
- Function: Bioperl initialize.
- Returns :
- Args    :
- Notes   :
-
-=cut
-
-sub _initialize {
-  my($self,@args) = @_;
-  return unless my $make = $self->SUPER::_initialize(@args);
-}
-
-
-=head2 write_phd
-
- Title   : write_phd(-SeqWithQuality => $swq, <comments>)
- Usage   : $obj->write_swq(     -SeqWithQuality => $swq,);
+ Title   : write_seq(-SeqWithQuality => $swq, <comments>)
+ Usage   : $obj->write_seq(     -SeqWithQuality => $swq,);
  Function: Write out an scf.
  Returns : Nothing.
  Args    : Requires: a reference to a SeqWithQuality object to form the
-        basis for the scf. Any other arguments are assumed to be comments
-        and are put into the comments section of the scf. Read the
-        specifications for scf to decide what might be good to put in here.
+           basis for the scf. Any other arguments are assumed to be comments
+           and are put into the comments section of the scf. Read the
+           specifications for scf to decide what might be good to put in here.
  Notes   : These are the comments that reside in the header of a phd file
-	at the present time. If not provided in the parameter list for
-	write_phd(), the following default values will be used:
+	   at the present time. If not provided in the parameter list for
+	   write_phd(), the following default values will be used:
 	CHROMAT_FILE: $swq->id()
 	ABI_THUMBPRINT: 0
 	PHRED_VERSION: 0.980904.e
@@ -205,53 +188,82 @@ sub _initialize {
 	CHEM: unknown
 	DYE: unknown
 
-
 =cut
 
-sub write_phd {
-	my ($self,%args) = @_;
-        my %comments;
-	my @phredstack;
-        my ($label,$arg);
-        my $swq = $args{-SeqWithQuality};
-	unless (ref($swq) eq "Bio::Seq::SeqWithQuality") {
-		$self->throw("You must pass a Bio::Seq::SeqWithQuality object to write_scf as a parameter named \"SeqWithQuality\"");
-	}
-	my $id = $swq->id();
-	if (!$id) { $id = "UNDEFINED in SeqWithQuality Object"; }
-	push @phredstack,("BEGIN_SEQUENCE $id","","BEGIN_COMMENT","");
-	if ($args{-CHROMAT_FILE}) { push @phredstack,"CHROMAT_FILE: ".$args{-CHROMAT_FILE}; }
-	else { push @phredstack,"CHROMAT_FILE: undefined in write_phd"; }
-	if ($args{-ABI_THUMBPRINT}) { push @phredstack,"ABI_THUMBPRINT: ".$args{-ABI_THUMBPRINT}; }
-	else { push @phredstack,"ABI_THUMBPRINT: 0"; }
-	if ($args{-PHRED_VERSION}) { push @phredstack,"PHRED_VERSION: ".$args{-PHRED_VERSION}; }
-	else { push @phredstack,"PHRED_VERSION: 0.980904.e"; }
-	if ($args{-CALL_METHOD}) { push @phredstack,"CALL_METHOD: ".$args{-CALL_METHOD}; }
-	else { push @phredstack,"CALL_METHOD: phred"; }
-	if ($args{-QUALITY_LEVELS}) { push @phredstack,"".$args{-QUALITY_LEVELS}; }
-	else { push @phredstack,"QUALITY_LEVELS: 99"; }
-	my $localtime = localtime();
-	if ($args{-TIME}) { push @phredstack,"TIME: ".$args{-TIME}; }
-	else { push @phredstack,"TIME: $localtime"; }
-	if ($args{-TRACE_ARRAY_MIN_INDEX}) { push @phredstack,"TRACE_ARRAY_MIN_INDEX: ".$args{-TRACE_ARRAY_MIN_INDEX}; }
-	else { push @phredstack, "TRACE_ARRAY_MIN_INDEX: 0"; }
-	if ($args{-TRACE_ARRAY_MAX_INDEX}) { push @phredstack,"TRACE_ARRAY_MAX_INDEX: ".$args{-TRACE_ARRAY_MAX_INDEX}; }
-	else { push @phredstack, "TRACE_ARRAY_MAX_INDEX: unknown";}
-	if ($args{-CHEM}) { push @phredstack,"CHEM: ".$args{-CHEM}; }
-	else { push @phredstack, "CHEM: unknown"; }
-	if ($args{-DYE} ) { push @phredstack,"DYE: ".$args{-DYE}; }
-	else { push @phredstack, "DYE: unknown"; }
-	push @phredstack,("END_COMMENT","","BEGIN_DNA");
-	my $fh = $self->_filehandle();
-	foreach (@phredstack) { print $fh ($_."\n"); }
-	my $length = $swq->length();
-	if ($length eq "DIFFERENT") {
-		$self->throw("Can't create the phd because the sequence and the quality in the SeqWithQuality object are of different lengths.");
-	}
-	for (my $curr = 1; $curr<=$length; $curr++) {
-		print $fh (uc($swq->baseat($curr))." ".$swq->qualat($curr)."\n");
-	}
-	print $fh ("END_DNA\n\nEND_SEQUENCE\n");
+sub write_seq {
+    my ($self,@args) = @_;
+    my @phredstack;
+    my ($label,$arg);
+
+    my ($swq, $chromatfile, $abithumb, 
+	$phredversion, $callmethod,
+	$qualitylevels,$time,
+	$trace_min_index,
+	$trace_max_index,
+	$chem, $dye
+	) = $self->_rearrange([qw(SEQWITHQUALITY
+				  CHROMAT_FILE
+				  ABI_THUMBPRINT
+				  PHRED_VERSION
+				  CALL_METHOD
+				  QUALITY_LEVELS
+				  TIME
+				  TRACE_ARRAY_MIN_INDEX
+				  TRACE_ARRAY_MAX_INDEX
+				  CHEM
+				  DYE
+				  )], @args);
+
+    unless (ref($swq) eq "Bio::Seq::SeqWithQuality") {
+	$self->throw("You must pass a Bio::Seq::SeqWithQuality object to write_scf as a parameter named \"SeqWithQuality\"");
+    }
+    my $id = $swq->id();
+    if (!$id) { $id = "UNDEFINED in SeqWithQuality Object"; }
+    push @phredstack,("BEGIN_SEQUENCE $id","","BEGIN_COMMENT","");
+
+    $chromatfile = 'undefined in write_phd' unless defined $chromatfile;
+    push @phredstack,"CHROMAT_FILE: $chromatfile"; 
+
+    $abithumb = 0 unless defined $abithumb;
+    push @phredstack,"ABI_THUMBPRINT: $abithumb"; 
+
+    $phredversion = "0.980904.e" unless defined $phredversion;
+    push @phredstack,"PHRED_VERSION: $phredversion"; 
+
+    $callmethod = 'phred' unless defined $callmethod;
+    push @phredstack,"CALL_METHOD: $callmethod"; 
+
+    $qualitylevels = 99 unless defined $qualitylevels;
+    push @phredstack,"QUALITY_LEVELS: $qualitylevels"; 
+
+    $time = localtime() unless defined $time;
+    push @phredstack,"TIME: $time"; 
+
+    $trace_min_index = 0 unless defined $trace_min_index;
+    push @phredstack,"TRACE_ARRAY_MIN_INDEX: $trace_min_index";
+
+    $trace_max_index = 'unknown' unless defined $trace_max_index;
+    push @phredstack,"TRACE_ARRAY_MAX_INDEX: $trace_max_index";
+
+    $chem = 'unknown' unless defined $chem;
+    push @phredstack,"CHEM: $chem";
+
+    $dye = 'unknown' unless defined $dye;
+    push @phredstack, "DYE: $dye";
+
+    push @phredstack,("END_COMMENT","","BEGIN_DNA");
+
+    foreach (@phredstack) {  $self->_print($_."\n"); }
+
+    my $length = $swq->length();
+    if ($length eq "DIFFERENT") {
+	$self->throw("Can't create the phd because the sequence and the quality in the SeqWithQuality object are of different lengths.");
+    }
+    for (my $curr = 1; $curr<=$length; $curr++) {
+	$self->_print (uc($swq->baseat($curr))." ".
+		       $swq->qualat($curr)."\n");
+    }
+    $self->_print ("END_DNA\n\nEND_SEQUENCE\n");
 }
 
 1;
