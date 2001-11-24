c87912344bee3f219670963dc4dda7dfa8ddee67
jason <jason@bioperl.org>
2001-11-24 17:16:06 +0000

    commit for bioperl scripts project
    svn path=/bioperl-live/trunk/; revision=2939

---
+++ scripts/prosite2perl.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl -w
+# $Id$
+# prosite2perl -- convert Prosite patterns to Perl regular expressions
+#
+# Jordan Dimov (jdimov@cis.clarion.edu)
+#
+# Submitted to bioperl scripts project 2001/08/03 
+#
+# Description: 
+# Prosite patterns to Perl regular expressions.
+# The prositeRegEx($) sub accepts a string
+# containing a Prosite pattern and returns a
+# string containing a valid Perl regex.  The code
+# is self-explanatory.
+
+sub prositeRegEx($);
+
+while (<>) {
+  chomp ($_);
+  print prositeRegEx ($_), "\n";
+}
+
+sub prositeRegEx ($) {
+  my $regex = shift;
+  $regex =~ s/[\-\.]//g;    
+  $regex =~ s/\{/[^/g; 
+  $regex =~ tr/x()<>}/.{}^$]/;
+  return ($regex);
+}
