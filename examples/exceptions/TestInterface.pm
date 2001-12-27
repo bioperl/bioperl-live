5013c8b6a53a1fe4a605c5f7871fb00c7fd0bcf3
sac <sac@bioperl.org>
2001-12-27 16:32:42 +0000

    Examples for working with Error.pm-based exceptions in the context of Bioperl. See examples/exceptions/README for more info.
    svn path=/bioperl-live/branches/steve_chervitz/; revision=3120

---
+++ examples/exceptions/TestInterface.pm
@@ -0,0 +1,38 @@
+=head1 NAME
+
+TestInterface - A simple subclass of Interface
+
+=head1 DESCRIPTION
+
+This module demonstrates how to use the generic Bio::Root::Interface
+superclass.
+
+=head1 AUTHOR
+
+Steve Chervitz <steve_chervitz@affymetrix.com>
+
+=cut
+
+package TestInterface;
+
+use Bio::Root::Interface;
+
+@ISA = qw( Bio::Root::Interface );
+
+sub data {
+    my $self = shift;
+    $self->throw_not_implemented;
+}
+
+sub foo {
+    my $self = shift;
+    $self->throw_not_implemented;
+}
+
+sub bar {
+    my $self = shift;
+    $self->throw_not_implemented;
+}
+
+
+1;
