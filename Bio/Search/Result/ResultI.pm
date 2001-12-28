32a322ddf10591a99c53575b244441d24f00fb45
sac <sac@bioperl.org>
2001-12-28 06:51:30 +0000

    Modified to inpcorporate ideas from Bio::Tools::Blast and Jason Stajich's ReportI.
    svn path=/bioperl-live/trunk/; revision=3134

--- Bio/Search/Result/ResultI.pm
+++ Bio/Search/Result/ResultI.pm
@@ -1,31 +1,56 @@
-
+#-----------------------------------------------------------------
+# $Id$
 #
-# BioPerl module for Bio::Search::Result::ResultI.pm
+# BioPerl module Bio::Search::Result::ResultI
 #
-# Cared for by Aaron Mackey <amackey@virginia.edu>
+# Cared for by Steve Chervitz <steve_chervitz@affymetrix.com>
 #
-# Copyright Aaron Mackey
+# Originally created by Aaron Mackey <amackey@virginia.edu>
 #
 # You may distribute this module under the same terms as perl itself
+#-----------------------------------------------------------------
 
 # POD documentation - main docs before the code
 
 =head1 NAME
 
-Bio::Search::Result::ResultI.pm - Abstract interface to Result objects
+Bio::Search::Result::ResultI- Abstract interface to Search Result objects
 
 =head1 SYNOPSIS
 
-    These objects are generated automatically by Bio::Search::Processor
-objects, and wouldn't be used directly.
+Bio::Search::Result::ResultI objects cannot be instantiated since this
+module defines a pure interface.
+
+Given an object that implements the Bio::Search::Result::ResultI  interface,
+you can do the following things with it:
+
+    while( $hit = $result->next_hits()) { ... }
+
+    $id = $result->query_name();
+
+    $id = $result->query_description();
+
+    $name = $result->database_name()
+
+    $size = $result->database_letters()
+
+    $num_entries = $result->database_entries()
+
+    my $gap_ext = $result->get_parameter('gapext')
+
+    my @params = $result->available_parameters
+
+    my $gap_ext = $result->get_statistic('kappa')
+
+    my @statnames = $result->available_statistics
 
 =head1 DESCRIPTION
 
-Bio::Search::Result::* objects are data structures containing the results from
-the execution of a search algorithm.  As such, it may contain various
-algorithm specific information as well as details of the execution, but will
-contain a few fundamental elements, including the ability to return
-Bio::Search::Hit::* objects.
+Bio::Search::Result::ResultI objects are data structures containing
+the results from the execution of a search algorithm.  As such, it may
+contain various algorithm specific information as well as details of
+the execution, but will contain a few fundamental elements, including
+the ability to return Bio::Search::Hit::HitI objects.
 
 =head1 FEEDBACK
 
@@ -48,9 +73,18 @@ Report bugs to the Bioperl bug tracking system to help us keep track
   bioperl-bugs@bio.perl.org
   http://bio.perl.org/bioperl-bugs/
 
-=head1 AUTHOR - Aaron Mackey
+=head1 AUTHOR - Aaron Mackey, Steve Chervitz
 
-Email amackey@virginia.edu
+Email amackey@virginia.edu  (original author)
+Email steve_chervitz@affymetrix.com
+
+=head1 COPYRIGHT
+
+Copyright (c) 1999-2001 Aaron Mackey, Steve Chervitz. All Rights Reserved.
+
+=head1 DISCLAIMER
+
+This software is provided "as is" without warranty of any kind.
 
 =head1 APPENDIX
 
@@ -64,106 +98,195 @@ The rest of the documentation details each of the object methods. Internal metho
 
 package Bio::Search::Result::ResultI;
 
-use vars qw(@ISA);
 use strict;
+use vars qw(@ISA);
 
-# Object preamble - inherits from Bio::Root::Object
+use Bio::AnalysisResultI;
 
-use Bio::Root::Root;
-
-@ISA = qw(Bio::Root::Root);
+@ISA = qw( Bio::AnalysisResultI );
 
 
-=head2 get_query_id
+=head2 next_hit
 
- Title   : get_query_id
- Usage   : $id = $result->get_query_id();
- Function: Used to obtain the string identifier of the query used by the
-           algorithm.
- Returns : a scalar string.
- Args    : <none>
+ Title   : next_hit
+ Usage   : while( $hit = $result->next_hits()) { ... }
+ Function: Returns the next available Hit object, representing potential
+           matches between the query and various entities from the database.
+ Returns : a Bio::Search::Hit::HitI object or undef if there are no more.
+ Args    : none
+
 
 =cut
 
-sub get_query_id {
-   my ($self,@args) = @_;
-
-   return $self->throw("Abstract interface call.");
+sub next_hits {
+    my ($self,@args) = @_;
+    $self->throw_not_implemented;
 }
 
-=head2 get_library_name
+=head2 query_name
 
- Title   : get_library_name
- Usage   : $name = $result->get_library_name()
- Function: Used to obtain the name of the library that the query was searched
+ Title   : query_name
+ Usage   : $id = $result->query_name();
+ Function: Get the string identifier of the query used by the
+           algorithm that performed the search.
+ Returns : a string.
+ Args    : none
+
+=cut
+
+sub query_name {
+    my ($self,@args) = @_;
+    $self->throw_not_implemented;
+}
+
+=head2 query_length
+
+ Title   : query_length
+ Usage   : $id = $result->query_length();
+ Function: Get the length of the query sequence
+           used in the search.
+ Returns : a number
+ Args    : none
+
+=cut
+
+sub query_length {
+    my ($self,@args) = @_;
+    $self->throw_not_implemented;
+}
+
+=head2 query_description
+
+ Title   : query_description
+ Usage   : $id = $result->query_description();
+ Function: Get the description of the query sequence
+           used in the search.
+ Returns : a string
+ Args    : none
+
+=cut
+
+sub query_description {
+    my ($self,@args) = @_;
+    $self->throw_not_implemented;
+}
+
+
+=head2 database_name
+
+ Title   : database_name
+ Usage   : $name = $result->database_name()
+ Function: Used to obtain the name of the database that the query was searched
            against by the algorithm.
  Returns : a scalar string
- Args    : <none>
+ Args    : none
 
 =cut
 
-sub get_library_name {
-   my ($self,@args) = @_;
+sub database_name {
+    my ($self,@args) = @_;
 
-   return $self->throw("Abstract interface call.");
+    $self->throw_not_implemented;
 }
 
-=head2 get_library_size
+=head2 database_letters
 
- Title   : get_library_size
- Usage   : $size = $result->get_library_size()
- Function: Used to obtain the size of library that was searched against.
+ Title   : database_letters
+ Usage   : $size = $result->database_letters()
+ Function: Used to obtain the size of database that was searched against.
  Returns : a scalar integer (units specific to algorithm, but probably the
-           total number of residues in the library, if available) or undef if
+           total number of residues in the database, if available) or undef if
            the information was not available to the Processor object.
- Args    : <none>
+ Args    : none
 
 
 =cut
 
-sub get_library_size {
-   my ($self,@args) = @_;
+sub database_letters {
+    my ($self,@args) = @_;
 
-   return $self->throw("Abstract interface call.");
+    $self->throw_not_implemented;
 }
 
-=head2 get_library_count
+=head2 database_entries
 
- Title   : get_library_count
- Usage   : $count = $result->get_library_count()
- Function: Used to obtain the number of entries contained in the library.
- Returns : a scalar integer representing the number of entities in the library
+ Title   : database_entries
+ Usage   : $num_entries = $result->database_entries()
+ Function: Used to obtain the number of entries contained in the database.
+ Returns : a scalar integer representing the number of entities in the database
            or undef if the information was not available.
- Args    : <none>
+ Args    : none
 
 
 =cut
 
-sub get_library_count {
-   my ($self,@args) = @_;
+sub database_entries {
+    my ($self,@args) = @_;
 
-   return $self->throw("Abstract interface call.");
+    $self->throw_not_implemented;
 }
 
-=head2 get_hits
+=head2 get_parameter
 
- Title   : get_hits
- Usage   : @hits = $result->get_hits();
- Function: Used to obtain the array of hit objects, representing potential
-           matches between the query and various entities from the library.
- Returns : an array of Bio::Search::Hit::* object (specific type depends on
-           algorithm), or undef if there were none.
- Args    : <none>
-
+ Title   : get_parameter
+ Usage   : my $gap_ext = $result->get_parameter('gapext')
+ Function: Returns the value for a specific parameter used
+           when running this result
+ Returns : string
+ Args    : name of parameter (string)
 
 =cut
 
-sub get_hits {
+sub get_parameter{
    my ($self,@args) = @_;
-
-   return $self->throw("Abstract interface call.");
+   $self->_abstractDeath;
 }
 
+=head2 available_parameters
+
+ Title   : available_parameters
+ Usage   : my @params = $result->available_parameters
+ Function: Returns the names of the available parameters
+ Returns : Return list of available parameters used for this result
+ Args    : none
+
+=cut
+
+sub available_parameters{
+   my ($self) = @_;
+   $self->_abstractDeath;
+}
+
+=head2 get_statistic
+
+ Title   : get_statistic
+ Usage   : my $gap_ext = $result->get_statistic('kappa')
+ Function: Returns the value for a specific statistic available 
+           from this result
+ Returns : string
+ Args    : name of statistic (string)
+
+=cut
+
+sub get_statistic{
+   my ($self,@args) = @_;
+   $self->_abstractDeath;
+}
+
+=head2 available_statistics
+
+ Title   : available_statistics
+ Usage   : my @statnames = $result->available_statistics
+ Function: Returns the names of the available statistics
+ Returns : Return list of available statistics used for this result
+ Args    : none
+
+=cut
+
+sub available_statistics{
+   my ($self) = @_;
+   $self->_abstractDeath;
+}
 
 1;
 
