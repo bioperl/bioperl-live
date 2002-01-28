a69575851693322eee45b981818171e843836444
birney <birney@bioperl.org>
2002-01-28 00:49:10 +0000

    added Registry and Failover classes
    svn path=/bioperl-live/trunk/; revision=3329

---
+++ Bio/DB/Failover.pm
@@ -0,0 +1,137 @@
+# POD documentation - main docs before the code
+
+# $Id$
+
+
+=head1 NAME
+
+Bio::DB::Failover - A Bio::DB::RandomAccessI compliant class which wraps a priority list of DBs
+
+=head1 SYNOPSIS
+
+    $failover = Bio::DB::Failover->new();
+  
+    $failover->add_database($db);
+
+    # fail over Bio::DB::RandomAccessI.pm
+
+    # this will check each database in priority, returning when
+    # the first one succeeds
+
+    $seq = $failover->get_Seq_by_id($id);
+
+=head1 DESCRIPTION
+
+This module provides fail over access to a set of Bio::DB::RandomAccessI objects
+
+
+=head1 CONTACT
+
+Ewan Birney originally wrote this class.
+
+=head2 Reporting Bugs
+
+Report bugs to the Bioperl bug tracking system to help us keep track
+the bugs and their resolution. Bug reports can be submitted via email
+or the web:
+
+    bioperl-bugs@bio.perl.org                   
+    http://bio.perl.org/bioperl-bugs/           
+
+=head1 APPENDIX
+
+The rest of the documentation details each of the object
+methods. Internal methods are usually preceded with a _
+
+=cut
+
+
+# Let the code begin...
+
+package Bio::DB::Failover;
+
+use vars qw(@ISA);
+use strict;
+
+use Bio::Root::Root;
+use Bio::DB::RandomAccessI;
+@ISA = qw(Bio::DB::RandomAccessI Bio::Root::Root);
+
+
+sub new {
+    my ($class,@args) = @_;
+
+    my $self = Bio::Root::Root->new();
+    bless $self,$class;
+
+    $self->{'database'} = [];
+
+    return $class;
+}
+
+
+sub add_database {
+    my ($self,@db) = @_;
+
+
+    foreach my $db ( @db ) {
+	if( !ref $db || !$db->isa('Bio::DB::RandomAccessI') ) {
+	    $self->throw("Database objects $db is a not a Bio::DB::RandomAccessI");
+	    next;
+	}
+
+	push(@{$self->{'database'}},$db);
+    }
+    
+}
+
+
+sub get_Seq_by_id {
+    my ($self,$id) = @_;
+
+    if( !defined $id ) {
+	$self->throw("no id is given!");
+    }
+
+    foreach my $db ( @{$self->{'database'}} ) {
+	my $seq;
+	eval {
+	    $seq = $db->get_Seq_by_id($db);
+	};
+	if( defined $seq ) {
+	    return $seq;
+	}
+    }
+
+    return undef;
+}
+
+
+
+sub get_Seq_by_acc {
+    my ($self,$id) = @_;
+
+    if( !defined $id ) {
+	$self->throw("no id is given!");
+    }
+
+    foreach my $db ( @{$self->{'database'}} ) {
+	my $seq;
+	eval {
+	    $seq = $db->get_Seq_by_acc($db);
+	};
+	if( defined $seq ) {
+	    return $seq;
+	}
+    }
+
+    return undef;
+}
+
+
+## End of Package
+
+1;
+
+__END__
+
---
+++ Bio/DB/Registry.pm
@@ -0,0 +1,160 @@
+# POD documentation - main docs before the code
+
+# $Id$
+
+
+=head1 NAME
+
+Bio::DB::Registry - Access to the Open Bio Database Access registry scheme
+
+=head1 SYNOPSIS
+
+    $registry = Bio::DB::Registry->new();
+
+    $db = $registry->get_database('embl');
+
+    # $db is a Bio::DB::SeqI implementing class
+
+=head1 DESCRIPTION
+
+This module provides access to the Open Bio Database Access scheme,
+which provides a cross language and cross platform specification of how
+to get to databases. 
+
+
+=head1 CONTACT
+
+Ewan Birney originally wrote this class.
+
+=head2 Reporting Bugs
+
+Report bugs to the Bioperl bug tracking system to help us keep track
+the bugs and their resolution. Bug reports can be submitted via email
+or the web:
+
+    bioperl-bugs@bio.perl.org                   
+    http://bio.perl.org/bioperl-bugs/           
+
+=head1 APPENDIX
+
+The rest of the documentation details each of the object
+methods. Internal methods are usually preceded with a _
+
+=cut
+
+
+# Let the code begin...
+
+package Bio::DB::Registry;
+
+use vars qw(@ISA);
+use strict;
+
+use Bio::Root::Root;
+@ISA = qw(Bio::Root::Root);
+
+my %implement = (
+		 'bsane-corba'      => 'Bio::CorbaClient::SeqDB',
+		 'index-berkeleydb' => 'XYZ');
+
+sub new {
+    my ($class) = shift;
+
+    my $self = Bio::Root::Root->new();
+    bless $self,$class;
+
+    # open files in order
+
+    $self->_load_registry();
+
+    return $self;
+}
+
+
+sub _load_registry {
+    my ($self) = @_;
+
+    my $home = (getpwuid($>))[7];
+    
+    if( -e "$home/.bioinformatics/sequence.ini" ) {
+	open(F,"$home/.bioinformatics/sequence.ini");
+    } elsif ( -e "/etc/bioinformatics/sequence.ini" ) {
+	open(F,"$home/.bioinformatics/sequence.ini");
+    } else {
+	# waiting for information
+	$self->throw("Oooops. We haven't implemented web fall back position yet");
+    }
+
+    while( <F> ) {
+	if( /^#/ ) {
+	    next;
+	}
+	if( /^\s/ ) {
+	    next;
+	}
+
+	if( /\[(\w+)\]/ )  {
+	    my $db;
+	    $db = $1;
+	    my $hash = {};
+	    while( <F> ) {
+		/^#/ && next;
+		/^\s/ && last;
+		my ($tag,$value) = split;
+		$hash->{$tag} = $value;
+	    }
+	    if( !exists $self->{$db} ) {
+		$self->{$db} = {};
+		$self->{$db}->{'services'} = [];
+	    }
+
+	    push(@{$self->{$db}->{'services'}},$hash);
+	    next; # back to main loop
+	}
+
+	$self->warn("Uninterpretable line in registry, $_");
+    }
+}
+
+	
+sub get_database {
+    my ($self,$dbname) = @_;
+
+    if( !defined $dbname ) {
+	$self->throw("must get_database with a database name");
+    }
+
+    if( !exists $self->{$dbname} ) {
+	$self->throw("No database in with $dbname in registry");
+    }
+
+    if( exists $self->{$dbname}->{'active'} ) {
+	return $self->{$dbname}->{'active'};
+    }
+
+    if( scalar(@{$self->{$dbname}->{'services'}}) > 1 ) {
+	$self->throw("Sorry ... have not implemented multiple database failbacks yet. Complain to Ewan as he wrote this!");
+    }
+
+    my ($config) = @{$self->{$dbname}->{'services'}};
+    
+    my $class = $implement{$config->{'protocol'}};
+
+    require $class;
+
+    my $db = $class->new( %$config);
+
+    $self->{$db}->{'active'} = $db;
+
+
+    return $db;
+}
+    
+
+
+## End of Package
+
+1;
+
+__END__
+
