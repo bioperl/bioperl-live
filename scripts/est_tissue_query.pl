e0e54d26979a4d0e4c99fa602e57fadf7a8b9e10
jason <jason@bioperl.org>
2002-02-27 15:26:51 +0000

    use Bio::SearchIO instead of BPlite
    svn path=/bioperl-live/trunk/; revision=3493

--- scripts/est_tissue_query.pl
+++ scripts/est_tissue_query.pl
@@ -1,4 +1,7 @@
-#!/usr/local/bin/perl -w
+#!/usr/bin/perl -w
+
+# This script will report the names of the tissues which were seen
+# in a BLAST/FASTA report against an EST (or cDNA possibly) library.
 
 # this script assumes you have a directory which you have downloaded
 # gbestXX.seq.gz from ncbi genbank release.  This will run faster if 
@@ -6,11 +9,17 @@
 # on demand.  Be sure that there is sufficient space and the uid
 # has write permission on the files and in that directory if you
 # plan to run this script on compressed files.
+
+# Alternatively you can use this with the -r option and it will 
+# use the remote sequence databases either genbank or embl to 
+# retrieve the specific EST (so one can attempt to guess the tissue type)
 ##
 # cmd line options are
 # -i/--index=indexname
 # -d/--dir=dir where gbest data files are located
 # -b/--blast=filename blast filename which compared against an EST db
+# -f/--format=(blast|blastxml|fasta) - type of search output either 
+#                                      from BLAST or FASTA suites 
 # -c/--cache=filename cache for accession number to tissue
 # -p/pvalue=pvalue pvalue to limit search to
 # -r/--remote=[GenBank|EMBL] use remote db for searching
@@ -18,7 +27,7 @@
 use strict;
 use DB_File;
 use Bio::SeqIO;
-use Bio::Tools::BPlite;
+use Bio::SearchIO;
 use Bio::DB::EMBL;
 use Bio::DB::GenBank;
 use Bio::Index::GenBank;
@@ -36,11 +45,13 @@ my $pvalue;     # Max P-Value allowed when parsing blastfile
 my $remote;     # flag for remote database 
 my $db;         # generic database handle
 my %accessions; # cache results
+my $format = 'blast';
 
 &GetOptions( 'd|dir:s'   => \$dir,
 	     'i|index:s' => \$index,
 	     'v|verbose' => \$VERBOSE,
 	     'b|blast:s' => \$blastfile,
+	     'f|format:s' => \$format,
 	     'c|cache:s' => \$cache,
 	     'p|pvalue:s'       => \$pvalue,
 	     'r|remote:s'=> \$remote);
@@ -83,36 +94,39 @@ if(! $blastfile || ! -r $blastfile ) {
     die("Must specify a valid blastfile");
 }
 
-my $parser = new Bio::Tools::BPlite(-file => $blastfile);
+my $parser = new Bio::SearchIO(-format => $format,
+			       -file => $blastfile);
 
 my %tissues_seen = ();
-SUBJECT: while( my $sbjct = $parser->nextSbjct ) {
-    if( defined $pvalue ) {
-	while( my $hsp = $sbjct->nextHSP ) {
-	    if( $hsp->P > $pvalue ) {
-		print "skipping ", $sbjct->name, "\n";		
-		# skip this Subject if it contains a pvalue of > $pvalue
-		next SUBJECT;
-	    }
-	}	
-    }
-    my  ($id) = split(/\s+/, $sbjct->name);
-    # get the last value
-    my @ids = split(/\|/, $id);
-    $id = pop @ids;
-    my ($tissuetype) = get_tissue($id);
-    if( defined $tissuetype ) {
-	push @{$tissues_seen{$tissuetype}}, $sbjct->name;
-    } else { 
-	print STDERR "could not find tissue for $id\n" if( $VERBOSE);
-    }
-}
+my ($result,$hit,$hsp);
+while( my $result = $parser->next_result )  {
+  HIT: while( my $hit = $result->next_hit ) {
+      if( defined $pvalue ) {
+	  while( my $hsp = $hit->next_hsp ) {
+	      if( $hsp->evalue > $pvalue ) {
+		  print "skipping ", $hit->name, " because of low evalue \n";
+		  # skip this Subject if it contains a pvalue of > $pvalue
+		  next HIT;
+	      }
+	  }
+      }
+      my  ($id) = split(/\s+/, $hit->name);
+      # get the last value
+      my @ids = split(/\|/, $id);
+      $id = pop @ids;
+      my ($tissuetype) = get_tissue($id);
+      if( defined $tissuetype ) {
+	  push @{$tissues_seen{$tissuetype}}, $hit->name;
+      } else { 
+	  print STDERR "could not find tissue for $id\n" if( $VERBOSE);
+      }
+  }
+  print "tissues seen for: ", $result->query_name, "\n";
 
-print "tissues seen for: ", $parser->query, "\n";
-
-foreach my $tissue ( sort keys %tissues_seen ) {
-    print "* $tissue\n-----------\n\t", 
-    join("\n\t",@{$tissues_seen{$tissue}}), "\n\n";
+  foreach my $tissue ( sort keys %tissues_seen ) {
+      print "* $tissue\n-----------\n\t", 
+      join("\n\t",@{$tissues_seen{$tissue}}), "\n\n";
+  }
 }
 
 # cleanup -- avoid segfault here
@@ -126,7 +140,7 @@ sub get_tissue {
     if( $tissue = $accessions{$id} ) {
 	return $tissue;
     }
-    
+
     my $seq = $db->get_Seq_by_acc($id);
     return undef unless(  $seq );
 
@@ -134,15 +148,15 @@ sub get_tissue {
 	if( $feature->primary_tag eq 'source' ) {
 	    foreach my $tag ( sort { $b cmp $a }
 			      $feature->all_tags ) {
-		    if( $tag =~ /tissue/i  || 
-			( ! $tissue && 
-			  $tag =~ /clone_lib/i ) ){
-			($tissue) = $feature->each_tag_value($tag);
-			$accessions{$seq->display_id} = $tissue;
-			return $tissue;
-		    }
+		if( $tag =~ /tissue/i  || 
+		    ( ! $tissue && 
+		      $tag =~ /clone_lib/i ) ){
+		    ($tissue) = $feature->each_tag_value($tag);
+		    $accessions{$seq->display_id} = $tissue;
+		    return $tissue;
 		}
 	    }
-	}	    
+	}
+    }	    
     return undef;
 }
