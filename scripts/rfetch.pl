eed9b3733feff6dec25cc371d7c33f92614a4e86
james <james@bioperl.org>
2002-03-01 09:12:57 +0000

    simple rfetch updates more options etc...
    svn path=/bioperl-live/trunk/; revision=3545

--- scripts/rfetch.pl
+++ scripts/rfetch.pl
@@ -1,22 +1,32 @@
-#!/usr/local/bin/perl
+#!/usr/bin/perl
 
-
+#
+# Fetch sequence data via OBDA registry system
+#
+# usage: rfetch -i <file_with_accession_list> -a -v -d embl -s start -e end
+#
 
 use Bio::DB::Registry;
+use Bio::SeqIO;
 use Getopt::Long;
 use strict;
 
-my $database = 'embl';
+my $database = 'embl_biosql';
 my $start    = undef;
 my $end      = undef;
 my $format   = 'fasta';
-
+my $file     = undef;
+my $acc      = undef;
+my $verbose  = undef;
 
 &GetOptions(
-	   'd|database:s' => \$database,
-	   's|start:i' => \$start,
-	   'e|end:i'   => \$end,
-	   'f|format:s' => \$format
+	    'd|database:s' => \$database,
+	    's|start:i' => \$start,
+	    'e|end:i'   => \$end,
+	    'f|format:s' => \$format,
+	    'i|input:s' => \$file,
+	    'a|acc'     => \$acc,
+	    'v|verbose' => \$verbose,
 	   );
 
 
@@ -26,8 +36,29 @@ my $db = $registry->get_database($database);
 
 my $seqout = Bio::SeqIO->new( '-format' => $format, '-fh' => \*STDOUT);
 
-foreach my $id ( @ARGV ) {
-  my $seq = $db->get_by_Seq_id($id);
+my @ids;
+
+if( defined $file ) {
+  open(F,$file) || die "cannot open $file $!";
+  while( <F> ) {
+    my ($id) = split;
+    push(@ids,$id);
+  }
+} else {
+  @ids = @ARGV;
+}
+
+foreach my $id ( @ids ) {
+  my $seq;
+  if( $verbose ){
+    print STDERR "fetching $id\n";
+  }
+
+  if( $acc ) {
+    $seq = $db->get_Seq_by_acc($id);
+  } else {
+    $seq = $db->get_Seq_by_id($id);
+  }
 
   if( defined $start && defined $end ) {
     $seq = $seq->trunc($start,$end);
