d722506b291f1344d23c44cb74460096b0d8120d
jason <jason@bioperl.org>
2002-03-17 23:09:14 +0000

    registry example
    svn path=/bioperl-live/trunk/; revision=3739

---
+++ examples/use_registry.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl -w
+use strict;
+
+use Bio::DB::Registry;
+use Bio::SeqIO;
+use strict;
+
+my $registry = new Bio::DB::Registry();
+
+print "services are ", join(',', $registry->services), "\n";
+my $db = $registry->get_database("embl");
+my $seq = $db->get_Seq_by_id("J02231");
+my $out = new Bio::SeqIO;
+$out->write_seq($seq);
