5e983b1488f5feb01b00612c40a3bd8dd5b74e6d
jason <jason@bioperl.org>
2002-04-18 13:16:41 +0000

    remove abstractDeath --branch
    svn path=/bioperl-live/branches/branch-1-0-0/; revision=3844

--- Bio/MapIO/mapmaker.pm
+++ Bio/MapIO/mapmaker.pm
@@ -31,7 +31,7 @@ Bio::MapIO::mapmaker - A Mapmaker Map reader
 =head1 DESCRIPTION
 
 This object contains code for parsing and processing Mapmaker output
-and creating Bio::Map::MapI objects from it.
+and creating L<Bio::Map::MapI> objects from it.
 
 =head1 FEEDBACK
 
@@ -90,7 +90,7 @@ use Bio::Map::Marker;
  Title   : next_tree
  Usage   : my $map = $factory->next_map;
  Function: Get a map from the factory
- Returns : Bio::Map::MapI
+ Returns : L<Bio::Map::MapI>
  Args    : none
 
 =cut
@@ -144,7 +144,7 @@ sub next_map{
 
 sub write_map{
    my ($self,@args) = @_;
-   $self->_abstractDeath('write_map');
+   $self->throw_not_implemented();
 }
 
 1;
