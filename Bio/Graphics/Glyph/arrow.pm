c6f8137526f617b99b69b4dfb8ac77eac3444d7d
lstein <lstein@bioperl.org>
2002-05-04 19:58:10 +0000

    fixed tick mark code in arrow.pm so that ticks dont disappear when looking at reversed feature in absolute coordinate system
    svn path=/bioperl-live/trunk/; revision=3899

--- Bio/Graphics/Glyph/arrow.pm
+++ Bio/Graphics/Glyph/arrow.pm
@@ -92,8 +92,7 @@ sub draw_parallel {
     my $stop     = $start + $self->feature->length - 1;
 
     my $offset   = $relative ? $self->feature->start-1 : 0;
-    # my $reversed = $self->feature->strand < 0;
-    my $reversed = $self->feature->start > $self->feature->end;  # shouldn't happen, but be prepared!
+    my $reversed = $self->feature->strand < 0 && $relative;
 
     my $units    = $self->option('units') || $self->calculate_units($start,$self->feature->length);
     my $divisor  = $UNITS{$units} || 1;
