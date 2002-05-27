3ec7a1c98198b694e9bed4d11d7936accf14eef7
jason <jason@bioperl.org>
2002-05-27 18:23:58 +0000

    simplify parsing loop for extra textfields
    svn path=/bioperl-live/trunk/; revision=3990

--- Bio/Tools/GFF.pm
+++ Bio/Tools/GFF.pm
@@ -265,48 +265,69 @@ sub _from_gff2_string {
    if ( $strand eq '+' ) { $feat->strand(1); }
    if ( $strand eq '.' ) { $feat->strand(0); }
 
-   #  <Begin Inefficient Code from Mark Wilkinson>
-   # this routine is necessay to allow the presence of semicolons in quoted text
-   # Semicolons are the delimiting character for new tag/value attributes.
-   # it is more or less a "state" machine, with the "quoted" flag going up and down
-   # as we pass thorugh quotes to distinguish free-text semicolon and hash symbols from GFF control characters
 
-   #$attribs =~ s/\#(.*)$//;				 # remove comments field (format:  #blah blah blah...  at the end of the GFF line)
-   my @att = split //, $attribs;         # split into individual characters
-   my $num = $#att;                 # count them
-   my $flag = 0;
-	my @parsed;		# this is needed to hold the characters that have been parsed	
-	for (my $a = 0; $a <= $num ; $a +=1){   # run through each character one at a time and check it
-		if ($att[$a] eq "\""){$flag=($flag==0)?1:0}  # flag up on entering quoted text, down on leaving it
-		if (($att[$a] eq ";") && $flag){$att[$a] = "INSERT_SEMICOLON_HERE"}  # replace semicolon with an unusual message if the quoted text flag is up
-		if (($att[$a] eq "#") && !$flag){last}  # an unquoted hash symbol means the beginning of the comments field - discard
-		push @parsed, $att[$a]                  # take the parsed character and push it onto the parsed list
-	}
+   #  <Begin Inefficient Code from Mark Wilkinson> 
+   # this routine is necessay to allow the presence of semicolons in
+   # quoted text Semicolons are the delimiting character for new
+   # tag/value attributes.  it is more or less a "state" machine, with
+   # the "quoted" flag going up and down as we pass thorugh quotes to
+   # distinguish free-text semicolon and hash symbols from GFF control
+   # characters
+   
+   
+   my $flag = 0; # this could be changed to a bit and just be twiddled
+   my @parsed;
 
-	$attribs = join "", @parsed; # rejoin into a single string
+   # run through each character one at a time and check it
+   # NOTE: changed to foreach loop which is more efficient in perl
+   # --jasons
 
-   # <End Inefficient Code>   Please feel free to fix this and make it more "perlish"
+   foreach my $a ( split //, $attribs ) { 
+       # flag up on entering quoted text, down on leaving it
+       if( $a eq '"') { $flag = ( $flag == 0 ) ? 1:0 }
+       elsif( $a eq ';' && $flag ) { $a = "INSERT_SEMICOLON_HERE"}
+       elsif( $a eq '#' && ! $flag ) { last } 
+       push @parsed, $a;
+   }
+   $attribs = join "", @parsed; # rejoin into a single string
+
+   # <End Inefficient Code>   
+   # Please feel free to fix this and make it more "perlish"
 
    my @key_vals = split /;/, $attribs;   # attributes are semicolon-delimited
 
    foreach my $pair ( @key_vals ) {
-       $pair =~ s/INSERT_SEMICOLON_HERE/;/g;  # replace semicolons that were removed from free-text above.
-       my ($blank, $key, $values) = split  /^\s*([\w\d]+)\s/, $pair;	# separate the key from the value
+       # replace semicolons that were removed from free-text above.
+       $pair =~ s/INSERT_SEMICOLON_HERE/;/g;        
 
-       my @values;								
+       # separate the key from the value
+       my ($blank, $key, $values) = split  /^\s*([\w\d]+)\s/, $pair; 
 
-       while ($values =~ s/"(.*?)"//){          # free text is quoted, so match each free-text block and remove it from the $values string
-       		push @values, $1;          # and push it on to the list of values (tags may have more than one value... and the value may be undef)
+
+       if( defined $values ) {
+	   my @values;
+	   # free text is quoted, so match each free-text block
+	   # and remove it from the $values string
+	   while ($values =~ s/"(.*?)"//){
+	       # and push it on to the list of values (tags may have
+	       # more than one value... and the value may be undef)	       
+	       push @values, $1;
+	   }
+
+	   # and what is left over should be space-separated
+	   # non-free-text values
+
+	   my @othervals = split /\s+/, $values;  
+	   foreach my $othervalue(@othervals){
+	       # get rid of any empty strings which might 
+	       # result from the split
+	       if (CORE::length($othervalue) > 0) {push @values, $othervalue}  
+	   }
+
+	   foreach my $value(@values){
+	       $feat->add_tag_value($key, $value);
+	   }
        }
-
-       my @othervals = split /\s+/, $values;  # and what is left over should be space-separated non-free-text values
-       foreach my $othervalue(@othervals){
-       		if (CORE::length($othervalue) > 0){push @values, $othervalue}  # get rid of any empty strings which might result from the split
-       }
-
-       foreach my $value(@values){
-       	   	$feat->add_tag_value($key, $value);
-      }
    }
 }
 
