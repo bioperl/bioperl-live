93f60665c0a730af6f9b449f7012b4f13499ba20
jason <jason@bioperl.org>
2002-06-23 18:21:17 +0000

    changes to calculate alignment and HSP trimming better
    svn path=/bioperl-live/branches/branch-1-0-0/; revision=4203

--- Bio/Search/HSP/GenericHSP.pm
+++ Bio/Search/HSP/GenericHSP.pm
@@ -666,8 +666,37 @@ sub get_aln {
     my $aln = new Bio::SimpleAlign;
     my $hs = $self->hit_string();
     my $qs = $self->query_string();
-    $hs =~ s/[\/\\]/\-/g;
-    $qs =~ s/[\/\\]/\-/g;
+
+    if( $self->algorithm  =~ /FAST/i ) {
+	# fasta reports some extra 'regional' sequence information
+	# we need to clear out first
+	# this seemed a bit insane to me at first, but it appears to 
+	# work --jason
+	
+	# we infer the end of the regional sequence where the first
+	# non space is in the homology string
+	# then we use the HSP->length to tell us how far to read
+	# to cut off the end of the sequence
+
+	# one possible problem is the sequence which 
+	
+	my ($start) = 0;
+	if( $self->homology_string() =~ /^(\s+)/ ) {
+	    $start = CORE::length($1);
+	}
+	$hs = substr($hs, $start,$self->length('total'));
+	$qs = substr($qs, $start,$self->length('total'));
+	foreach my $seq ( $qs,$hs)  {
+	    foreach my $f ( '\\', '/') {
+		my $index = index($seq,$f);
+		while( $index >=0 ) {
+		    substr($hs,$index,1) = '';
+		    substr($qs,$index,1) = '';
+		    $index = index($seq,$f,$index+1);
+		}
+	    }
+	}
+    }
     my $seqonly = $qs;
     $seqonly =~ s/\-//g;
     
