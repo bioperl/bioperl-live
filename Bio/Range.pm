43e476bf9576f41b1414b73715ff4663392eda75
heikki <heikki@bioperl.org>
2002-06-24 11:08:05 +0000

    minor doc format fixes
    svn path=/bioperl-live/branches/branch-1-0-0/; revision=4217

--- Bio/Range.pm
+++ Bio/Range.pm
@@ -50,7 +50,7 @@ User feedback is an integral part of the evolution of this and other
 Bioperl modules. Send your comments and suggestions preferably to one
 of the Bioperl mailing lists.  Your participation is much appreciated.
 
-  bioperl-l@bioperl.org          - General discussion
+  bioperl-l@bioperl.org                         - General discussion
   http://bio.perl.org/MailList.html             - About the mailing lists
 
 =head2 Reporting Bugs
@@ -68,8 +68,8 @@ Email heikki@ebi.ac.uk
 
 =head1 APPENDIX
 
-The rest of the documentation details each of the object methods. Internal metho
-ds are usually preceded with a _
+The rest of the documentation details each of the object
+methods. Internal methods are usually preceded with a _
 
 =cut
 
