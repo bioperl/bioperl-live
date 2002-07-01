e18783f2e5c8814eedbba1012f00954336f311d0
sac <sac@bioperl.org>
2002-07-01 22:31:14 +0000

    Migrated changes from trunk version 1.50
    svn path=/bioperl-live/branches/branch-1-0-0/; revision=4285

--- Bio/Root/RootI.pm
+++ Bio/Root/RootI.pm
@@ -109,7 +109,6 @@ BEGIN {
     $ID        = 'Bio::Root::RootI';
     $VERSION   = 1.0;
     $Revision  = '$Id$ ';
-    $VERSION   = 1.0;
     $DEBUG     = 0;
     $VERBOSITY = 0;
 }
@@ -179,7 +178,7 @@ sub throw{
 
    my $std = $self->stack_trace_dump();
 
-   my $out = "-------------------- EXCEPTION --------------------\n".
+   my $out = "\n-------------------- EXCEPTION --------------------\n".
        "MSG: ".$string."\n".$std."-------------------------------------------\n";
    die $out;
 
@@ -211,7 +210,7 @@ sub warn{
     } elsif( $verbose == -1 ) {
 	return;
     } elsif( $verbose == 1 ) {
-	my $out = "-------------------- WARNING ---------------------\n".
+	my $out = "\n-------------------- WARNING ---------------------\n".
 		"MSG: ".$string."\n";
 	$out .= $self->stack_trace_dump;
 	
@@ -219,7 +218,7 @@ sub warn{
 	return;
     }    
 
-    my $out = "-------------------- WARNING ---------------------\n".
+    my $out = "\n-------------------- WARNING ---------------------\n".
        "MSG: ".$string."\n".
 	   "---------------------------------------------------\n";
     print STDERR $out;
@@ -261,6 +260,7 @@ sub deprecated{
    }
 }
 
+		     
 =head2 verbose
 
  Title   : verbose
@@ -422,8 +422,10 @@ sub _rearrange {
     # they are named or simply listed. If they are listed, we
     # can just return them. 
 
-    return @param unless (defined($param[0]) && $param[0]=~/^-/); 
-    
+    # The mod test fixes bug where a single string parameter beginning with '-' gets lost.
+    # This tends to happen in error messages such as: $obj->throw("-id not defined")
+    return @param unless (defined($param[0]) && $param[0]=~/^-/ && ($#param+1) % 2 == 0);
+
     # Tester
 #    print "\n_rearrange() named parameters:\n";
 #    my $i; for ($i=0;$i<@param;$i+=2) { printf "%20s => %s\n", $param[$i],$param[$i+1]; }; <STDIN>;
