242c4ad1afd7b2a94edbb02b31d12f4639a3155a
benb <benb@bioperl.org>
2002-07-25 22:35:31 +0000

    The regexp in _set_seq inserted an extra space on the end of each alignment line.  In _set_residues(), these lines get concatenated together but the blank space still remains between each line.  This screws up the generation of the $identicalList_query, $identicalList_sbjct, etc., because the characters in the query and sbjct sequences (with spaces) don't line up with the match line sequence (which, because it's parsed differently doesn't have the spaces). I was getting incorrect seq_inds() coordinates because of this.
    svn path=/bioperl-live/branches/branch-1-0-0/; revision=4454

--- Bio/Search/HSP/BlastHSP.pm
+++ Bio/Search/HSP/BlastHSP.pm
@@ -927,7 +927,7 @@ sub _set_seq {
     my $numGaps   = 0;
 
     foreach( @data ) {
-	if( m/(\d+) *(\D+) *(\d+)/) {
+  if( m/(\d+) *([^\d\s]+) *(\d+)/) {
 	    push @ranges, ( $1, $3 ) ;
 	    push @sequence, $2;
 	} else {
