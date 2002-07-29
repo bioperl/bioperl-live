0db76abfaac5f39ecc1e26ecf5ffdc596d29aeb3
jason <jason@bioperl.org>
2002-07-29 21:43:43 +0000

    print some more info
    svn path=/bioperl-live/trunk/; revision=4470

--- scripts/parse_codeml.pl
+++ scripts/parse_codeml.pl
@@ -4,7 +4,8 @@ use strict;
 use Bio::Tools::Phylo::PAML;
 use Bio::Root::IO;
 
-my $parser = new Bio::Tools::Phylo::PAML(-file => Bio::Root::IO->catfile(qw(t data codeml.mlc)));
+my $parser = new Bio::Tools::Phylo::PAML(-file    => shift,
+					 -verbose => shift);
 
 my $result = $parser->next_result;
 
@@ -18,8 +19,21 @@ my $NGmatrix = $result->get_NGmatrix();
 # everything above it is undefined.  Each of the defined cells is a
 # hashref of estimates for "dN", "dS", "omega" (dN/dS ratio), "t",
 # "S" and "N".  If a ML matrix, "lnL" will also be defined.
-printf "The ML omega ratio for sequences %s vs %s was: %g\n",
-    $otus[0]->id, $otus[1]->id, $MLmatrix->[0]->[1]->{omega};
 
-printf "The NG omega ratio for sequences %s vs %s was: %g\n",
-    $otus[0]->id, $otus[1]->id, $NGmatrix->[0]->[1]->{'omega'};
+my @otus = $result->get_seqs();
+my $MLmatrix = $result->get_MLmatrix();
+my $NGmatrix = $result->get_NGmatrix();
+for( my $i=0;$i<scalar @$MLmatrix;$i++) {
+    for( my $j = $i+1; $j < scalar @{$MLmatrix->[$i]}; $j++ ) { 
+	printf "The ML omega ratio for sequences %s vs %s was: %g\n",
+	$otus[$i]->id, $otus[$j]->id, $MLmatrix->[$i]->[$j]->{omega};
+    }
+}
+
+for( my $i=0;$i<scalar @$MLmatrix;$i++) {
+    for( my $j = $i+1; $j < scalar @{$MLmatrix->[$i]}; $j++ ) { 
+	
+	printf "The NG omega ratio for sequences %s vs %s was: %g\n",
+	$otus[$i]->id, $otus[$j]->id, $NGmatrix->[$i]->[$j]->{'omega'};
+    }
+}
