812b9abc02ee77084347449cba275885fdc84514
shawnh <shawnh@bioperl.org>
2002-09-27 19:32:02 +0000

    Added docs
    svn path=/bioperl-live/trunk/; revision=4804

--- Bio/Tools/Prints.pm
+++ Bio/Tools/Prints.pm
@@ -1,3 +1,60 @@
+#
+# Cared for by Bala Savikalpa
+#
+# 
+#
+# You may distribute this module under the same terms as perl itself
+# POD documentation - main docs before the code
+
+=head1 NAME
+
+Bio::Tools::Prints - Parser for FingerPRINTScanII program 
+
+=head1 SYNOPSIS
+
+  use Bio::Tools::Prints;
+  my  $parser = Bio::Tools::Prints->new(-fh=>$filehandle);
+
+  while(my $prints_feature = $parser->next_result){
+    push @features, $prints_feature;
+  }
+
+=head1 DESCRIPTION
+
+FingerPRINTScan II is a PRINTS fingerprint identification algorithm.
+Copyright (C) 1998,1999  Phil Scordis
+
+=head1 FEEDBACK
+
+=head2 Mailing Lists
+
+User feedback is an integral part of the evolution of this and other
+Bioperl modules. Send your comments and suggestions preferably to one
+of the Bioperl mailing lists.  Your participation is much appreciated.
+
+  bioperl-l@bioperl.org          - General discussion
+  http://bio.perl.org/MailList.html             - About the mailing lists
+
+=head2 Reporting Bugs
+
+Report bugs to the Bioperl bug tracking system to help us keep track
+ the bugs and their resolution.  Bug reports can be submitted via
+ email or the web:
+
+  bioperl-bugs@bio.perl.org
+  http://bio.perl.org/bioperl-bugs/
+
+=head1 AUTHOR - Bala Savikalpa
+
+Email: fugui@worf.fugu-sg.org
+
+=head1 APPENDIX
+
+The rest of the documentation details each of the object
+methods. Internal methods are usually preceded with a _
+
+=cut
+
 package Bio::Tools::Prints;
 use vars qw(@ISA);
 use strict;
@@ -9,6 +66,16 @@ use Bio::SeqFeature::Generic;
 @ISA = qw(Bio::Root::Root Bio::Root::IO Bio::Tools::AnalysisResult);
 
 
+=head2 new
+
+ Title   : new
+ Usage   : my $obj = new Bio::Tools::Prints(-fh=>$filehandler);
+ Function: Builds a new Bio::Tools::Prints object
+ Returns : Bio::Tools::Prints
+ Args    : -filename 
+           -fh (filehandle) 
+
+=cut
 
 sub new {
       my($class,@args) = @_;
@@ -19,6 +86,16 @@ sub new {
       return $self;
 }
 
+=head2 next_result
+
+ Title   : next_result
+ Usage   : my $r = $prints_parser->next_result
+ Function: Get the next result set from parser data
+ Returns : L<Bio::SeqFeature::Generic>
+ Args    : none
+
+
+=cut
 
 sub next_result {
       my ($self) = @_;
@@ -87,6 +164,16 @@ sub next_result {
         
 }
 
+=head2 create_feature
+
+ Title   : create_feature
+ Usage   : my $r = $prints_parser->create_feature
+ Function: Creates a Seqfeature Generic object
+ Returns : L<Bio::SeqFeature::Generic>
+ Args    :  
+
+=cut
+
 sub create_feature {
        my ($self, $feat,$sequenceId) = @_;
 
@@ -109,6 +196,17 @@ sub create_feature {
         
 }
 
+=head2 print_sac
+
+ Title   : print_sac
+ Usage   : $prints_parser->print_sac($print_sac)
+ Function: get/set for pritn_sac 
+ Returns : 
+ Args    : 
+
+=cut
+
+
 sub print_sac{
     my($self,$printsac)=@_;
  
@@ -119,6 +217,16 @@ sub print_sac{
     return $self->{'print_sac'};
 
 }
+
+=head2 seqname
+
+ Title   : seqname
+ Usage   : $prints_parser->seqname($seqname)
+ Function: get/set for seqname
+ Returns :
+ Args    :
+
+=cut
 
 sub seqname{
     my($self,$seqname)=@_;
