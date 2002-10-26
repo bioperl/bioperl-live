312184e26657f7ca045b3e1fdef837abada679d1
sac <sac@bioperl.org>
2002-10-26 09:10:43 +0000

    Removed Error.pm dependency so it's more likely to work out of the box.
    svn path=/bioperl-live/trunk/; revision=4976

--- scripts/searchio/resultwriter.pl
+++ scripts/searchio/resultwriter.pl
@@ -41,56 +41,56 @@ use lib '../../';
 
 use Bio::SearchIO;
 use Bio::SearchIO::Writer::ResultTableWriter;
-use Error qw(:try);
+use Bio::SearchIO::Writer::HTMLResultWriter;
 
 print "\nUsing SearchIO->new()\n";
 
-try {
 
-    # Note that all parameters for the $in, $out, and $writer objects are optional.
-    # Default in = STDIN; Default out = STDOUT; Default writer = all columns
-    # In this example, we're reading from STDIN and  writing to STDOUT
-    # and using the default columns for the writer.
-    # We're also telling the script to timeout if input isn't received
-    # within 10 sec. (Note the clock is still ticking when you background the job.)
-    # Setting verbose to 1 is useful for debugging.
-    my $in = Bio::SearchIO->new( -format => 'psiblast', 
-				 -signif => 0.1, 
-				 -verbose => 0, 
-				 -timeout_sec => 10 );
-    # not specifying any columns to get the default.
-    my $writer = Bio::SearchIO::Writer::ResultTableWriter->new();
-    my $out    = Bio::SearchIO->new( -format => 'psiblast', 
-				     -writer => $writer,
-                                     -file => ">resultwriter.out");
+# Note that all parameters for the $in, $out, and $writer objects are optional.
+# Default in = STDIN; Default out = STDOUT; Default writer = all columns
+# In this example, we're reading from STDIN and  writing to STDOUT
+# and using the default columns for the writer.
+# We're also telling the script to timeout if input isn't received
+# within 10 sec. (Note the clock is still ticking when you background the job.)
+# Setting verbose to 1 is useful for debugging.
+my $in = Bio::SearchIO->new( -format => 'psiblast', 
+                             -signif => 0.1, 
+                             -verbose => 0, 
+                             -timeout_sec => 10 );
+# not specifying any columns to get the default.
+my $writer = Bio::SearchIO::Writer::ResultTableWriter->new();
+my $out    = Bio::SearchIO->new( -format => 'psiblast', 
+                                 -writer => $writer,
+                                 -file => ">resultwriter.out");
 
-    while ( my $result = $in->next_result() ) {
-#       printf STDERR "Report %d: $result\n", $in->report_count;
+my $writerhtml = new Bio::SearchIO::Writer::HTMLResultWriter();
+my $outhtml = new Bio::SearchIO(-writer => $writerhtml,
+                                -file   => ">searchio.html");
+
+
+while ( my $result = $in->next_result() ) {
+    eval {
+        # printf STDERR "Report %d: $result\n", $in->report_count;
         $out->write_result($result, ($in->report_count - 1 ? 0 : 1) );
+        
+        $outhtml->write_result($result, 1);
 
-	# To get at the statistical parameters:
+        # To get at the statistical parameters:
         # Calling raw_statistics() returns a list containing the
         # unparsed lines of the parameters section of the report.
         # Here we're only interested in parameters beginning with "effective".
-	 print "Report Stats, effective data:\n";
-	 foreach( $result->raw_statistics) {
-	     print "$_" if /^effective/i;
-	 }
-
+        #         print "Report Stats, effective data:\n";
+        #         foreach( $result->raw_statistics) {
+        #             print "$_" if /^effective/i;
+        #         }
+        
 	## For a simple progress monitor, uncomment this line:
 	#print STDERR "."; print STDERR "\n" if $in->report_count % 50 == 0;
+    };
+    if($@) {
+        warn "Warning: Blast parsing or writing exception caught for $result:\n$@\n";
     }
-
-    printf STDERR "\n%d Blast report(s) processed.\n", $in->report_count;
-    printf STDERR "Output sent to file: %s\n",  $out->file if $out->file;
-
 }
-catch Bio::Root::Exception with {
-    my $err = shift;
-    print STDERR "\nCaught Bio::Root::Exception:\n\n$err\n";
-}
-otherwise {
-    my $err = shift;
-    print STDERR "\nAn unanticipated exception occurred of type ", ref $err, "\n\n";
-    print STDERR "$err\n";
-};
+
+printf STDERR "\n%d Blast report(s) processed.\n", $in->report_count;
+printf STDERR "Output sent to file: %s\n",  $out->file if $out->file;
