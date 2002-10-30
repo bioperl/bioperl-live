58877d51a9d148173d639c1956d5bfff4e74428b
allenday <allenday@bioperl.org>
2002-10-30 23:19:44 +0000

    probei isa featurei, nothing more.  i'm trying to conform with mged here, but i can see people wanting to use 'probe' at some point in the future. if not, we just cvs rm it.
    svn path=/bioperl-live/trunk/; revision=5005

--- Bio/Expression/ProbeI.pm
+++ Bio/Expression/ProbeI.pm
@@ -16,9 +16,7 @@ Do not use this module directly
 
 =head1 DESCRIPTION
 
-This provides a standard bioperl interface class for representing
-DNA and RNA probes.  It cannot be instantiated directly, but serves
-as an abstract base class for implementors.
+This class ISA Bio::Expression::FeatureI, nothing more.
 
 =head1 FEEDBACK
 
@@ -57,82 +55,7 @@ package Bio::Expression::ProbeI;
 use strict;
 use Bio::Root::Root;
 
-use base qw(Bio::Root::Root Bio::PrimarySeqI);
+use base qw(Bio::Expression::FeatureI);
 use vars qw($DEBUG);
-
-=head2 value()
-
-  Title   : value
-  Usage   : $val = $probe->value()
-  Function: get/set the probe's observed value
-  Returns : A numeric value
-  Args    : a new numeric value (optional)
-
-=cut
-
-sub value {
-  my($self,$arg) = @_;
-  if($arg){
-    $self->throw(__PACKAGE__ . "::value only accepts numeric values") unless $arg =~ /^[\d.]+$/;
-    $self->{value} = $arg;
-  }
-  return $self->{value} || 0;
-}
-
-=head2 value_units()
-
-  Title   : value_units
-  Usage   : $units = $probe->units()
-  Function: get/set the units of the probe's observed value
-  Returns : A string or undef
-  Args    : a new string (optional)
-
-=cut
-
-sub value_units {
-  my($self,$arg) = @_;
-  $self->{value_units} = $arg if defined $arg;
-  return $self->{value_units};
-}
-
-=head2 standard_deviation()
-
-  Title   : standard_deviation
-  Usage   : $std_dev = $probe->standard_deviation()
-  Function: get/set the probe's standard deviation of value()
-  Returns : A numeric value
-  Args    : a new numeric value (optional)
-  Comments: no calculation is done here
-
-=cut
-
-sub standard_deviation {
-  my($self,$arg) = @_;
-  if($arg){
-    $self->throw(__PACKAGE__ . "::standard_deviation only accepts numeric values") unless $arg =~ /^[\d.]+$/;
-    $self->{standard_deviation} = $arg;
-  }
-  return $self->{deviation} || 0;
-}
-
-=head2 sample_count()
-
-  Title   : sample_count
-  Usage   : $sample_count = $probe->sample_count()
-  Function: get/set the number of samples used to calculate
-            value()
-  Returns : An integer
-  Args    : a new integer (optional)
-
-=cut
-
-sub sample_count {
-  my($self,$arg) = @_;
-  if($arg){
-    $self->throw(__PACKAGE__ . "::sample_count only accepts integers") unless $arg =~ /^[\d]+$/;
-    $self->{sample_count} = $arg;
-  }
-  return $self->{sample_count} || 0;
-}
 
 1;
