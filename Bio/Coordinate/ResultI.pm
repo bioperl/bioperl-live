1d49dfbbb4baa8168f96da295a55bd28eaf11275
heikki <heikki@bioperl.org>
2003-02-12 10:58:35 +0000

    initial commit
    svn path=/bioperl-live/trunk/; revision=5473

---
+++ Bio/Coordinate/ResultI.pm
@@ -0,0 +1,80 @@
+# $Id$
+#
+# bioperl module for Bio::Coordinate::ResultI
+#
+# Cared for by Heikki Lehvaslaiho <heikki@ebi.ac.uk>
+#
+# Copyright Heikki Lehvaslaiho
+#
+# You may distribute this module under the same terms as perl itself
+
+# POD documentation - main docs before the code
+
+=head1 NAME
+
+Bio::Coordinate::ResultI - Interface to identify coordinate mapper results
+
+=head1 SYNOPSIS
+
+  # not to be used directly
+
+=head1 DESCRIPTION
+
+ResultI identifies Bio::LocationIs returned by
+Bio::Coordinate::MapperI implementing classes from other locations.
+
+=head1 FEEDBACK
+
+=head2 Mailing Lists
+
+User feedback is an integral part of the evolution of this and other
+Bioperl modules. Send your comments and suggestions preferably to the
+Bioperl mailing lists  Your participation is much appreciated.
+
+  bioperl-l@bioperl.org                        - General discussion
+  http://bio.perl.org/MailList.html             - About the mailing lists
+
+=head2 Reporting Bugs
+
+report bugs to the Bioperl bug tracking system to help us keep track
+ the bugs and their resolution.  Bug reports can be submitted via
+ email or the web:
+
+  bioperl-bugs@bio.perl.org
+  http://bugzilla.bioperl.org/
+
+=head1 AUTHOR - Heikki Lehvaslaiho
+
+Email:  heikki@ebi.ac.uk
+Address:
+
+     EMBL Outstation, European Bioinformatics Institute
+     Wellcome Trust Genome Campus, Hinxton
+     Cambs. CB10 1SD, United Kingdom
+
+=head1 CONTRIBUTORS
+
+Additional contributors names and emails here
+
+=head1 APPENDIX
+
+The rest of the documentation details each of the object
+methods. Internal methods are usually preceded with a _
+
+=cut
+
+
+# Let the code begin...
+
+package Bio::Coordinate::ResultI;
+use vars qw(@ISA );
+use strict;
+
+# Object preamble
+use Bio::LocationI;
+
+@ISA = qw(Bio::LocationI);
+
+
+1;
+
