95e3cf182ff35eb26572a64d6d2fb151dbfe89b7
lstein <lstein@bioperl.org>
2003-02-20 01:05:04 +0000

    added Bio::SeqIO::swissprot for the purposes of OBDA naming compatibility; this is just a subclass of Bio::SeqIO::swiss and doesnt add new functionality
    svn path=/bioperl-live/trunk/; revision=5523

---
+++ Bio/SeqIO/swissprot.pm
@@ -0,0 +1,74 @@
+# $Id$
+#
+# BioPerl module for Bio::SeqIO::swiss
+#
+# Cared for by Elia Stupka <elia@ebi.ac.uk>
+#
+# Copyright Elia Stupka
+#
+# You may distribute this module under the same terms as perl itself
+
+# POD documentation - main docs before the code
+
+=head1 NAME
+
+Bio::SeqIO::swissprot - Swissprot sequence input/output stream
+
+=head1 SYNOPSIS
+
+It is probably best not to use this object directly, but
+rather go through the SeqIO handler system. Go:
+
+    $stream = Bio::SeqIO->new(-file => $filename, -format => 'swissprot');
+
+    while ( my $seq = $stream->next_seq() ) {
+	# do something with $seq
+    }
+
+=head1 DESCRIPTION
+
+This is an alias for the "swiss" format, included here just for OBDA
+naming compatibility.
+=head1 FEEDBACK
+
+=head2 Mailing Lists
+
+User feedback is an integral part of the evolution of this
+and other Bioperl modules. Send your comments and suggestions preferably
+ to one of the Bioperl mailing lists.
+Your participation is much appreciated.
+
+  bioperl-l@bioperl.org          - General discussion
+  http://bio.perl.org/MailList.html             - About the mailing lists
+
+=head2 Reporting Bugs
+
+Report bugs to the Bioperl bug tracking system to help us keep track
+ the bugs and their resolution.
+ Bug reports can be submitted via email or the web:
+
+  bioperl-bugs@bio.perl.org
+  http://bugzilla.bioperl.org/
+
+=head1 AUTHOR - Elia Stupka
+
+Email elia@ebi.ac.uk
+
+Describe contact details here
+
+=head1 APPENDIX
+
+The rest of the documentation details each of the object methods. Internal methods are usually preceded with a _
+
+=cut
+
+
+# Let the code begin...
+
+
+package Bio::SeqIO::swissprot;
+use vars qw(@ISA);
+use Bio::SeqIO::swiss;
+@ISA = 'Bio::SeqIO::swiss';
+
+1;
