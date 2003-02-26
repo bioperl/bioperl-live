0e17f949fadbe9b60589252a0f3cb1b5becff40a
jason <jason@bioperl.org>
2003-02-26 21:40:28 +0000

    remove email address
    svn path=/bioperl-live/trunk/; revision=5610

--- Bio/DB/Flat/BDB.pm
+++ Bio/DB/Flat/BDB.pm
@@ -45,7 +45,6 @@ Report bugs to the Bioperl bug tracking system to help us keep track
 the bugs and their resolution.  Bug reports can be submitted via
 email or the web:
 
-  bioperl-bugs@bio.perl.org
   http://bugzilla.bioperl.org/
 
 =head1 AUTHOR - Lincoln Stein
