46e9810b6b912b83d460fe0e0a9e93026453c91c
sac <sac@bioperl.org>
2003-03-07 19:28:20 +0000

    Modules for use with the examples/root exceptions scripts.
    svn path=/bioperl-live/trunk/; revision=5721

---
+++ examples/root/lib/TestInterface.pm
@@ -0,0 +1,38 @@
+=head1 NAME
+
+TestInterface - A simple subclass of Interface
+
+=head1 DESCRIPTION
+
+This module demonstrates how to use the generic Bio::Root::RootI
+superclass.
+
+=head1 AUTHOR
+
+Steve Chervitz E<lt>sac@bioperl.orgE<gt>
+
+=cut
+
+package TestInterface;
+
+use Bio::Root::RootI;
+
+@ISA = qw( Bio::Root::RootI );
+
+sub data {
+    my $self = shift;
+    $self->throw_not_implemented;
+}
+
+sub foo {
+    my $self = shift;
+    $self->throw_not_implemented;
+}
+
+sub bar {
+    my $self = shift;
+    $self->throw_not_implemented;
+}
+
+
+1;
