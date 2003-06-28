066d1a2e57a96cb7c690219fb2dcc634ccaa07db
jason <jason@bioperl.org>
2003-06-28 17:09:15 +0000

    adding more popstats, need more work and might be moved to PopGen
    svn path=/bioperl-live/trunk/; revision=6336

--- Bio/Tree/Statistics.pm
+++ Bio/Tree/Statistics.pm
@@ -12,19 +12,38 @@
 
 =head1 NAME
 
-Bio::Tree::Statistics - Calculate certain statistics for a Tree
+Bio::Tree::Statistics - Calculate certain statistics for a coalescent population
 
 =head1 SYNOPSIS
 
-Give standard usage here
+  use Bio::Tree::Statistics;
+  use Bio::Tree::Tree;
+  use Bio::Tree::RandomFactory;
+  my $factory = new Bio::Tree::RandomFactory( -sample_size => 6,
+					      -maxcount => 50);
+
+
+  my $tree = $factory->next_tree;
+  
+  $factory->add_Mutations($tree,20);
+
+  my $stats = new Bio::Tree::Statistics();
+  my $pi = $stats->pi($tree);
+  my $D = $stats->tajima_d($tree);
 
 =head1 DESCRIPTION
 
 This object is a place to accumulate routines for calculating various
-tree statistics from population genetic and phylogenetic methods.  
+statistics from the coalescent simulation.  This module will likely
+move to Bio::PopGen as it is more appropriate there.
 
-Currently Fu and Li's D is implemented.
-Tajima's D planned.
+Currently implemented:
+ Fu and Li's D  (fu_and_li_D)
+ Fu and Li's D* (fu_and_li_D_star)
+ Fu and Li's F  (fu_and_li_F)
+ Tajima's D     (tajima_D)
+ theta          (theta)
+ pi --- not currently working. (pi)
 
 References forthcoming.
 
@@ -47,7 +66,7 @@ the web:
 
   http://bugzilla.bioperl.org/
 
-=head1 AUTHOR - Aaron Mackey
+=head1 AUTHOR - Jason Stajich
 
 Email jason@bioperl.org
 
@@ -109,34 +128,41 @@ sub fu_and_li_D{
    my @nodes = $tree->get_nodes();
    my $muttotal =0;
    my $tipmutcount = 0;
-   my $sampsize = 0;
+   my $sample_size = 0;
    foreach my $n ( @nodes ) {
+   
+       if( ! $n->isa('Bio::Tree::AlleleNode') ) {
+	   $self->warn("Cannot run fu_and_li_D without AlleleNodes");
+	   return 0;
+       }
+       $muttotal += scalar $n->get_marker_names;
+       
        if ($n->is_Leaf() ) {
-	   $sampsize++;
+	   $sample_size++;	   
 	   $tipmutcount += $n->get_marker_names();
        }
-       $muttotal += $n->get_marker_names();
    }
-
+   
    if( $muttotal <= 0 ) { 
        $self->warn("mutation total was not > 0, cannot calculate a Fu and Li D");
        return 0;
    }
+
    my $a = 0;
-   for(my $k= 1; $k < $sampsize; $k++ ) {
+   for(my $k= 1; $k < $sample_size; $k++ ) {
         $a += ( 1 / $k );
     }
    
    my $b = 0;
-    for(my $k= 1; $k < $sampsize; $k++ ) {
+    for(my $k= 1; $k < $sample_size; $k++ ) {
         $b += ( 1 / $k**2 );
     }
  
-    my $c = 2 * ( ( ( $sampsize * $a ) - (2 * ( $sampsize -1 ))) /
-                  ( ( $sampsize - 1) * ( $sampsize - 2 ) ) );
+    my $c = 2 * ( ( ( $sample_size * $a ) - (2 * ( $sample_size -1 ))) /
+                  ( ( $sample_size - 1) * ( $sample_size - 2 ) ) );
  
-    my $v = 1 + ( ( $a**2 / ( $b + $a**2 ) ) * ( $c - ( ( $sampsize + 1) /
-                                                        ( $sampsize - 1) ) ));
+    my $v = 1 + ( ( $a**2 / ( $b + $a**2 ) ) * ( $c - ( ( $sample_size + 1) /
+                                                        ( $sample_size - 1) ) ));
  
     my $u = $a - 1 - $v;
     my $D = ( $muttotal - (  $a * $tipmutcount) ) /
@@ -145,5 +171,219 @@ sub fu_and_li_D{
     return $D;
 }
 
+# fu_and_li_D*
+
+sub fu_and_li_D_star {
+    my ($self,$tree) = @_;
+    my ($sample_size, $seg_sites, $singletons);
+    my %markers;
+    foreach my $n ( $tree->get_nodes() ) {
+	if( ! $n->isa('Bio::Tree::AlleleNode') ) {
+	    $self->warn("Cannot run fu_and_li_D_star without AlleleNodes");
+	    return 0;
+	}
+	foreach my $m ( $n->get_marker_names ) { $markers{$m}++ }
+	$sample_size++ if $n->is_Leaf();
+    }
+    $seg_sites = scalar keys %markers;
+
+    my $a = 0;
+    for(my $k= 1; $k < $sample_size; $k++ ) {
+	$a += ( 1 / $k );
+    }
+
+    my $a1 = 0;
+    for(my $k= 1; $k <= $sample_size; $k++ ) {
+	$a1 += ( 1 / $k );
+    }
+
+    my $b = 0;
+    for(my $k= 1; $k < $sample_size; $k++ ) {
+	$b += ( 1 / $k**2 );
+    }
+
+    my $c = 2 * ( ( ( $sample_size * $a ) - (2 * ( $sample_size -1 ))) / 
+		  ( ( $sample_size - 1) * ( $sample_size - 2 ) ) );
+
+    my $d = $c + ( ($sample_size -2) / ($sample_size - 1)**2 ) +
+	( 2 / ($sample_size -1) * 
+	  ( (3/2) - ( (2*$a1 - 3) / ($sample_size -2) ) - 
+	    ( 1/ $sample_size) ) 
+	  );
+    my $v_star = ( ( ($sample_size/($sample_size-1) )**2)*$b + (($a**2)*$d) -
+		 (2*( ($sample_size*$a*($a+1)) )/(($sample_size-1)**2)) )  /
+		   (($a**2) + $b);
+
+    my $u_star = ( ($sample_size/($sample_size-1))*
+		   ($a - ($sample_size/
+			  ($sample_size-1)))) - $v_star;
+
+    my $D_star = ( (($sample_size/($sample_size-1))*$seg_sites) -
+		   ($a*$seg_sites) ) / 
+		   ( sqrt( ($u_star*$seg_sites) + ($v_star*($seg_sites**2)) ));
+    return $D_star;
+}
+
+sub fu_and_li_F {
+    my ($self,$tree) = @_;
+    
+    my ($sample_size, $seg_sites, $pi, $ext_mutations);
+    my %markers;
+    foreach my $n ( $tree->get_nodes() ) {
+	if( ! $n->isa('Bio::Tree::AlleleNode') ) {
+	    $self->warn("Cannot run fu_and_li_F without AlleleNodes");
+	    return 0;
+	}
+	my @names = $n->get_marker_names;
+	foreach my $m ( @names ) { $markers{$m}++ }
+	if ($n->is_Leaf() ) {
+	    $sample_size++;
+	    $ext_mutations += scalar @names;
+	}
+    }
+    $seg_sites = keys %markers;
+    $pi = $self->pi($tree);
+
+    my $a = 0;
+    for(my $k= 1; $k < $sample_size; $k++ ) {
+	$a += ( 1 / $k );
+    }
+
+    my $a1 = 0;
+    for(my $k= 1; $k <= $sample_size; $k++ ) {
+	$a1 += ( 1 / $k );
+    }
+
+    my $b = 0;
+    for(my $k= 1; $k < $sample_size; $k++ ) {
+	$b += ( 1 / $k**2 );
+    }
+
+    my $c = 2 * ( ( ( $sample_size * $a ) - (2 * ( $sample_size -1 ))) / 
+		  ( ( $sample_size - 1) * ( $sample_size - 2 ) ) );
+
+    my $v_F = ( $c + ( (2*(($sample_size**2)+$sample_size+3)) / 
+		       ( (9*$sample_size)*($sample_size-1) ) ) -
+		(2/($sample_size-1)) ) / ( ($a**2)+$b );
+
+    my $u_F = ( 1 + ( ($sample_size+1)/(3*($sample_size-1)) )-
+		( 4*( ($sample_size+1)/(($sample_size-1)**2) ))*
+		($a1 - ((2*$sample_size)/($sample_size+1))) ) /
+		($sample_size - $v_F);
+
+    my $F = ($pi - $ext_mutations) / ( sqrt( ($u_F*$seg_sites) +
+					     ($v_F*($seg_sites**2)) ) );
+
+    return $F;
+}
+
+
+sub tajima_D {
+    my ($self,$tree) = @_;
+    my @compare;
+    # we are calculating pi - all pairwise differences between 
+    # tips
+    my $pi = $self->pi($tree);
+    my $a1 = 0; 
+    my ($muttotal,$sample_size,$ext_mutations);
+    my @nodes = $tree->get_nodes();
+    foreach my $n ( @nodes ) {
+       if( ! $n->isa('Bio::Tree::AlleleNode') ) {
+	   $self->warn("Cannot run tajima_D without AlleleNodes");
+	   return 0;
+       }       
+       $muttotal += $n->get_marker_names();
+       if ($n->is_Leaf() ) {
+	   $sample_size++;	   
+	   $ext_mutations += $n->get_marker_names();
+       }
+   }
+    for(my $k= 1; $k < $sample_size; $k++ ) {
+	$a1 += ( 1 / $k );
+    }
+
+     my $a2 = 0;
+     for(my $k= 1; $k < $sample_size; $k++ ) {
+	 $a2 += ( 1 / $k**2 );
+     }
+
+    my $b1 = ( $sample_size + 1 ) / ( 3* ( $sample_size - 1) );
+    my $b2 = ( 2 * ( $sample_size ** 2 + $sample_size + 3) ) / 
+	     ( ( 9 * $sample_size) * ( $sample_size - 1) );
+    my $c1 = $b1 - ( 1 / $a1 );
+    my $c2 = $b2 - ( ( $sample_size + 2 ) /
+		     ( $a1 * $sample_size))+( $a2 / $a1 ** 2);
+    my $e1 = $c1 / $a1;
+    my $e2 = $c2 / ( $a1**2 + $a2 );
+    
+    my $D = ( $pi - ( $muttotal / $a1 ) ) / 
+	sqrt ( ($e1 * $muttotal) + (( $e2 * $muttotal) * ( $muttotal - 1)));
+
+    return $D;
+}
+
+sub pi {
+    my ($self, $tree) = @_;
+    my ($count,$total,$sample_size) = (0,0,0);
+    
+    my %markers;
+    my @nodes = $tree->get_nodes;
+    foreach my $n ( @nodes ) {
+	if( ! $n->isa('Bio::Tree::AlleleNode') ) {
+	    $self->warn("Cannot run pi without AlleleNodes");
+	    return 0;
+	}
+	# this could be a bit expensive...
+	for my $m ( $n->get_marker_names ) { $markers{$m}++ }
+	$sample_size++ if $n->is_Leaf();
+    }
+   
+    my @marker_names = keys %markers;
+
+    for( my $i = 0; $i < $sample_size; $i++ ) {
+	for( my $j=$i+1; $j < $sample_size; $j++) {	    
+	    $total++;
+	    for my $name ( @marker_names ) {
+		if( $nodes[$i]->has_marker($name) &&
+		    $nodes[$j]->has_marker($name) ) {
+		    $self->debug(join('',"alleles for $i $name ", 
+				      $nodes[$i]->get_alleles($name), "\n"));
+		    $self->debug(join('', "\t"x$j,"alleles for $j $name ", 
+				      $nodes[$j]->get_alleles($name), "\n"));
+		}
+	    }
+	    #( $nodes[$i]->get_alleles($name)->[0] != 
+	    #  $nodes[$j]->get_alleles($name)->[0] ));
+	}
+    }
+    my $pi = $count / $total;
+    $self->debug( "total=$total count=$count pi=$pi\n");
+
+    return $pi;
+}
+
+sub theta {
+    my ($self,$tree) = @_;
+    my $sample_size = 0;
+    my @nodes = $tree->get_nodes();
+    my %markers;
+    foreach my $n ( @nodes ) {
+	if( ! $n->isa('Bio::Tree::AlleleNode') ) {
+	    $self->warn("Cannot run theta without AlleleNodes");
+	    return 0;
+	}
+	for my $m ( $n->get_marker_names ) { $markers{$m}++ }
+	if ($n->is_Leaf() ) {
+	    # this could be a bit expensive...
+	    $sample_size++;
+	}
+    }
+    my $seg_sites = scalar keys %markers;
+    my $a1 = 0; 
+    for(my $k= 1; $k < $sample_size; $k++ ) {
+	$a1 += ( 1 / $k );
+    }    
+    return $seg_sites / $a1;
+}
 
 1;
