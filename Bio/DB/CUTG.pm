ce869fa6748022bf02fd6c55602a2dbf2a78261e
radams <radams@bioperl.org>
2003-09-05 11:11:38 +0000

    read/write methods removed
    svn path=/bioperl-live/trunk/; revision=6800

--- Bio/DB/CUTG.pm
+++ Bio/DB/CUTG.pm
@@ -21,24 +21,21 @@ at http://www.kazusa.or.jp/codon.
        use Bio::DB::CUTG;
 
        my $db = Bio::DB::CUTG->new(-sp =>'Pan troglodytes');
-       $db->get_web_request();
-       $db->write_data(-file => '>OUT');
-       $db->get_local_request(-file => 'OUT');
+       my $CUT = $db->get_request();
 
-       my $cdtable = $db->next_data;
 
 =head1 DESCRIPTION
 
 
 This class retrieves and objectifies codon usage tables either from
-the web database or from a local file. The idea is that you can 
+a web database . The idea is that you can 
 initially retrieve a CUT from the web database, and write it to file
-in a way that can be read in later. The next_data method returns a 
-Bio::CodonUsage::Table object. 
+in a way that can be read in later, using the Bio::CodonUsage::IO module.
+
 
 For  a web query, two parameters need to be specified: species(sp) and 
 genetic code id (gc). The database is searched 
-using regular expressions therefore the full latin name 
+using regular expressions, therefore the full latin name 
 must be given to specify the organism. If the species name
 is ambiguous the first CUT in the list is retrieved. 
 Defaults are Homo sapiens and 1(standard genetic code).
@@ -46,12 +43,16 @@ If you are retrieving CUTs from organisms using other genetic codes this needs
 to be  put in as a parameter. Parameters can be entered in the constructor
 or in the get_web_request ()method. Allowable parameters are listed in the
 $QUERY_KEYS hash reference variable.
+
+I intend at a later date to allow retrieval of multiple codon tables
+e.g., from a wildcard search. 
  
 =head1 SEE ALSO
 
 L<Bio::Tools::CodonTable>, 
 L<Bio::WebAgent>,
-L<Bio::CodonUsage::Table>
+L<Bio::CodonUsage::Table>,
+L<Bio::CodonUsage::IO>
 
 =head1 FEEDBACK
 
@@ -92,7 +93,8 @@ methods. Internal methods are usually preceded with a _
 
 package Bio::DB::CUTG;
 use Bio::WebAgent;
-use Bio::Root::IO;
+use Bio::CodonUsage::IO;
+use IO::String;
 use vars qw($URL @ISA $QUERY_KEYS);
 
 @ISA = qw(Bio::WebAgent);
@@ -122,6 +124,7 @@ sub new {
 	my $self = $class->SUPER::new(@args);
 	return $self;
 }
+
 =head2 query_keys
 
  Title   : query_keys
@@ -182,7 +185,7 @@ sub gc {
 			$self->{'_gc'} = shift;
 			}
 		else {
-			$self->warn("invalid genetic code index - setting to default");
+			$self->warn("invalid genetic code index - setting to standard default (1)");
 			$self->{'_gc'} = 1;
 			}
 		}
@@ -190,81 +193,29 @@ sub gc {
 
 	}
 
-=head2  get_local_request
 
- Title  : get_local_request
- Usage  : $db->get_local_request();
- Purpose: To read in a formatted file of a CUT
- Returns: void 
- Args   : File name(mandatory), genetic code id(optional)
-
-=cut
-
-sub get_local_request {
-
-	my ($self, @args) = @_;
-	$self->{'_source'} = 'file';
-	$self->{'_raw_cud'} = ''; #reinitialise
-	my $ct = Bio::Root::IO->new(@args);
-	while (my $l = $ct->_readline() ) {
-		$self->{'_raw_cud'}.=  $l;
-	}
-	
-}
-
-=head2  write_data
-
- Title       : write_data
- Usage       : $db->write_data(-file =>">filename");
- Purpose     : To write  a formatted file of a CUT
- Returns     : void 
- Args        : File name(mandatory)
- Description : At present just writes raw DB entry to file. NO functionality
-               for directly writing Bio::CodonUsage::Table objects yet.
-
-=cut
-
-sub write_data {
-
-	##currently just writes text from web retrieval to a text file.
-	my ($self, @args)= @_;
-	my $ct = Bio::Root::IO->new(@args);
-	if (defined ($self->{'_raw_cud'} )) {
-		my $title = "#Codon usage table for " . $self->sp . "\n";
-		my ($content) = $self->{'_raw_cud'}=~/.*<PRE>(.+)<\/PRE>/s ;
-		$content 			||= $self->{'_raw_cud'};
-		$ct->_print("$title" . "$content");
-		return;
-		}
-	else {
-		$self->warn(" cannot yet write object to file, need raw text file");
-		return;
-		}
-}
-
-=head2  get_web_request
+=head2  get_request
 
  Title  : get_web_request
- Usage  : $db->get_web_request();
+ Usage  : my $cut = $db->get_web_request();
  Purpose: To query remote CUT with a species name
- Returns: void 
+ Returns: a new codon usage table object 
  Args   : species  name(mandatory), genetic code id(optional)
 
 =cut
 	
 
-sub get_web_request {
+sub get_request {
 	my ($self, @args) = @_;
 	_check_args(@args);
 	shift;
-	### can out in parameters here as well
+	### can put in parameters here as well
 	while( @_ ) {
 	my $key = shift;
         $key =~ s/^-//;
         $self->$key(shift);
     }	
 	$self->url($URL);
-	$self->{'_source'} = 'www';
 
 	###1st of all search DB to check species exists and is unique
 	my $nameparts =  join "+", $self->sp =~ /(\w+)/g;
@@ -301,34 +252,28 @@ sub get_web_request {
 
 
 	######## now get codon table , all defaults established now
+
+	##construct URL##
 	 $nameparts =  join "+", $self->sp =~ /(\w+)/g;
 	my $CT_url = $self->url . "/codon/cgi-bin/showcodon.cgi?species="
 				. $nameparts . "+%5B" . $self->_db . "%5D&aa=" . $self->gc . "&style=GCG";
+
+	## retrieve data in html##
 	my $rq2 = HTTP::Request->new(GET=>$CT_url);
-	my $reply2 = $self->request($rq2);
+	my $content2 = $self->request($rq2)->content;
 
-	my $content2 = $reply2->content;
-	$self->{'_raw_cud'} = ''; #reinitialise
-	($self->{'_raw_cud'}) = $content2 =~ /\[.+\]:(.*)Genetic/s;
-	$self->{'_raw_cud'}=~ s/End/Ter/g;
-	  
+	## strip html tags, basic but works here
+	 $content2 =~ s/<[^>]+>//sg;
+	$content2 =~ s/Format.*//sg;
+
+	### and pass to Bio::CodonUsage::IO for parsing
+	my $iostr = IO::String->new($content2);
+	 my $io = Bio::CodonUsage::IO->new (-fh=>$iostr);
+
+	##return object ##
+	return $io->next_data;
 	}
 
-=head2  next_data
-
- Title  : next_data
- Usage  : my $cut = $db->next_data();
- Purpose: To  obtain a Bio:CodonUsage::Table object from a returned query
- Returns: A  Bio:CodonUsage::Table object
- Args   : none
-
-=cut
-
-sub next_data {
-	my $self = shift;
-	my $cut = $self->_parse;
-	return $cut;
-	}
 
 
 sub _check_args {
@@ -355,52 +300,6 @@ sub _db {
 	return $self->{'_db'};
 }
 
-sub _parse {
-	my $self = shift;
-	my $cdtableobj = Bio::CodonUsage::Table->new();
-	#### get stats from outside main table, just for web retrieved version
-	if ($self->{'_source'} eq 'www') {
-		my ($gcstring) = $self->{'_raw_cud'} =~ /(Coding.+)Genetic/s;
-		($cdtableobj->{'_coding_gc'}{'all'},
-		$cdtableobj->{'_coding_gc'}{'1'},
-		$cdtableobj->{'_coding_gc'}{'2'},
-		$cdtableobj->{'_coding_gc'}{'3'}) = $gcstring =~ /(\d\d\.\d\d)/sg;
-
-		($cdtableobj->{'_cds_count'}) = $self->{'_raw_cud'} =~ /\s(\d+)\sCDS/;
-
-	### now just keep main table ########
-		$self->{'_raw_cud'} =~ s/.*<PRE>(.+)<\/PRE>.*/$1/s;
-		}
-	elsif ($self->{'_source'} eq 'file') {
-		my $sp = $self->{'_raw_cud'} =~ /^#Codon.+for\s(.+)$/;
-		$self->sp($sp);
-		}
-															
-		
-	## now parse main table #####
-	my @lines = split "\n", $self->{'_raw_cud'};
-	for my $line (@lines) {
-		## ignore  spurious lines ##
-		next if $line =~ /^$/ || $line !~ /^\w\w\w\s/;
-		 $line =~ /(\w\w\w)\s+(\w+)\s+(\d+\.\d+)\s+(\d+\.\d+)\s+(\d+\.\d+)/;
-		if (defined ($1)) {
-			$cdtableobj->{'_table'}{$1}{$2} = {
-						'abs_count'=>$3,
-						 'per1000'=> $4,
-						 'rel_freq'=> $5,
-						};
-			}
-		else {
-			print "line is $line";
-			}
-		}
-		## check has been parsed ok ##
-		if (scalar keys %{$cdtableobj->{'_table'}} != 21) {
-			$cdtableobj->warn("probable parsing error - should be 21 entries for 20aa + stop codon");
-		}
-		return $cdtableobj;
-		
-}
 
 
 	return 1;
