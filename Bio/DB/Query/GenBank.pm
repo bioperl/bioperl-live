ceaf5f7da73572751552b9572ab8fa4cff5807d7
lstein <lstein@bioperl.org>
2003-09-09 21:29:00 +0000

    fixed documentation errors in Bio::DB::Genbank
    svn path=/bioperl-live/branches/branch-1-2/; revision=6837

--- Bio/DB/Query/GenBank.pm
+++ Bio/DB/Query/GenBank.pm
@@ -23,11 +23,11 @@ Bio::DB::Query::GenBank - Build a GenBank Entrez Query
 					    -mindate => '2001',
 					    -maxdate => '2002');
    my $count = $query->count;
-   my @ids   = $query->get_Ids;
+   my @ids   = $query->ids;
 
    # get a genbank database handle
-   $gb = new Bio::DB::GenBank;
-   my $stream = $db->get_Stream_by_query($query);
+   my $gb = new Bio::DB::GenBank;
+   my $stream = $gb->get_Stream_by_query($query);
    while (my $seq = $stream->next_seq) {
       ...
    }
