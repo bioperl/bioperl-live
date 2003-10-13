a47a4a1ebb03afbcecb43b9082a90269944fc91b
lapp <lapp@bioperl.org>
2003-10-13 20:02:43 +0000

    Replaced call to phased out each_synonym by get_synonyms.
    svn path=/bioperl-live/branches/branch-1-2/; revision=7031

--- Bio/Annotation/OntologyTerm.pm
+++ Bio/Annotation/OntologyTerm.pm
@@ -124,19 +124,19 @@ sub new{
     
     my ($term,$name,$label,$identifier,$definition,$ont,$tag) =
 	$self->_rearrange([qw(TERM
-			      NAME
-			      LABEL
-			      IDENTIFIER
-			      DEFINITION
-			      ONTOLOGY
-			      TAGNAME)],
-			  @args);
+                          NAME
+                          LABEL
+                          IDENTIFIER
+                          DEFINITION
+                          ONTOLOGY
+                          TAGNAME)],
+                      @args);
     if($term) {
-	$self->term($term);
+        $self->term($term);
     } else {
-	$self->name($name || $label) if $name || $label;
-	$self->identifier($identifier) if $identifier;
-	$self->definition($definition) if $definition;
+        $self->name($name || $label) if $name || $label;
+        $self->identifier($identifier) if $identifier;
+        $self->definition($definition) if $definition;
     }
     $self->ontology($ont || $tag) if $ont || $tag;
 
@@ -184,7 +184,7 @@ sub hash_tree{
    $h->{'name'} = $self->name();
    $h->{'identifier'} = $self->identifier();
    $h->{'definition'} = $self->definition();
-   $h->{'synonyms'} = [$self->each_synonym()];
+   $h->{'synonyms'} = [$self->get_synonyms()];
 }
 
 
