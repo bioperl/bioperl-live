47106343b59f87a7f2dec70d3c83abffb40545a5
heikki <heikki@bioperl.org>
2003-11-13 13:17:48 +0000

    fixed trunc() when strand is -1. Also made end() calculate its value based on the length of the sequence and start. no need to set end() expicitely any more
    svn path=/bioperl-live/trunk/; revision=7176

--- Bio/LocatableSeq.pm
+++ Bio/LocatableSeq.pm
@@ -126,7 +126,7 @@ sub start{
       my $value = shift;
       $self->{'start'} = $value;
     }
-    return $self->{'start'};
+    return $self->{'start'} || 1;
 
 }
 
@@ -145,20 +145,25 @@ sub end {
    if( @_ ) {
       my $value = shift;
       my $string = $self->seq;
-      if ($string and $self->start) {
-	  my $s2 = $string;
-	  $string =~ s/[.-]+//g;
-	  my $len = CORE::length $string;
-	  my $new_end = $self->start + $len - 1 ;
+      if ($self->seq) {
+          my $len = $self->_ungapped_len;
 	  my $id = $self->id;
-	  $self->warn("In sequence $id residue count gives value $len.
-Overriding value [$value] with value $new_end for Bio::LocatableSeq::end().")
-	      and $value = $new_end if $new_end != $value and $self->verbose > 0;
+	  $self->warn("In sequence $id residue count gives end value $len.
+Overriding value [$value] with value $len for Bio::LocatableSeq::end().")
+	      and $value = $len if $len != $value and $self->verbose > 0;
       }
 
       $self->{'end'} = $value;
     }
-    return $self->{'end'};
+
+   return $self->{'end'} || $self->_ungapped_len;
+}
+
+sub _ungapped_len {
+    my $self = shift;
+    my $string = $self->seq;
+    $string =~ s/[.-]+//g;
+    return $self->start + CORE::length($string) - 1 ;
 
 }
 
@@ -286,7 +291,8 @@ sub column_from_residue_number {
 	# the following bit of "magic" allows the main loop logic to be the
 	# same regardless of the strand of the sequence
 	($start,$end,$inc,$test)= ($self->strand == -1)?
-	(scalar(@residues-1),0,-1,sub{$i >= $end}) : (0,scalar(@residues-1),1,sub{$i <= $end});
+            (scalar(@residues-1),0,-1,sub{$i >= $end}) :
+                (0,scalar(@residues-1),1,sub{$i <= $end});
 
 	for ($i=$start; $test->(); $i+= $inc) {
 	    if ($residues[$i] ne '.' and $residues[$i] ne '-') {
@@ -308,22 +314,21 @@ sub column_from_residue_number {
 =head2 location_from_column
 
  Title   : location_from_column
- Usage   : $loc = $ali->location_from_column( $seq_number, $column_number)
+ Usage   : $loc = $ali->location_from_column($column_number)
  Function:
 
-           This function gives the residue number in the sequence with
-           the given name for a given position in the alignment
-           (i.e. column number) of the given. Gaps complicate this
-           process and force the output to be a L<Bio::Range> where
-           values can be undefined. For example, for the alignment
+           This function gives the residue number for a given position
+           in the alignment (i.e. column number) of the given. Gaps
+           complicate this process and force the output to be a
+           L<Bio::Range> where values can be undefined. For example,
+           for the sequence:
 
-  	     Seq1/91-97 AC..DEF.G.
-  	     Seq2/1-9   .CYHDEFKGK
+  	     Seq/91-97 .AC..DEF.G.
 
-           location_from_column( Seq1/91-97, 3 ) position 93
-           location_from_column( Seq1/91-97, 2 ) position 92^93
-           location_from_column( Seq1/91-97, 10) position 97^98
-           location_from_column( Seq2/1-9, 1 )   position undef
+           location_from_column( 3 ) position 93
+           location_from_column( 2 ) position 92^93
+           location_from_column(10 ) position 97^98
+           location_from_column( 1 ) position undef
 
            An exact position returns a Bio::Location::Simple object
            where where location_type() returns 'EXACT', if a position
@@ -359,10 +364,10 @@ sub location_from_column {
     my $pos = CORE::length $s;
 
     my $start = $self->start || 0 ;
-    my $relative_pos = $self->strand() == -1?
-    $self->end - $pos + 1
-    : $pos + $start - 1;
     my $strand = $self->strand() || 1;
+    my $relative_pos = ($strand == -1)
+        ? ($self->end - $pos + 1)
+            : ($pos + $start - 1);
     if ($self->subseq($column, $column) =~ /[a-zA-Z]/ ) {
 	$loc = new Bio::Location::Simple
 	    (-start => $relative_pos,
@@ -426,19 +431,16 @@ sub revcom {
 =cut
 
 sub trunc {
-
     my ($self, $start, $end) = @_;
     my $new = $self->SUPER::trunc($start, $end);
+    $new->strand($self->strand);
+
+    # end will be automatically calculated
+    $start = $end if $self->strand == -1;
 
     $start = $self->location_from_column($start);
-    $start ? ($start = $start->start) : ($start = 1);
-
-    $end = $self->location_from_column($end);
-    $end = $end->start if $end;
-
-    $new->strand($self->strand);
+    $start ? ($start = $start->end) : ($start = 1);
     $new->start($start) if $start;
-    $new->end($end) if $end;
 
     return $new;
 }
