deafe7fd5630c1f4a29407326cc3fbca66b53a27
skirov <skirov@bioperl.org>
2003-11-19 19:06:44 +0000

    Ooops.. misssed some()
    svn path=/bioperl-live/trunk/; revision=7221

--- Bio/Matrix/PSM/IO/mast.pm
+++ Bio/Matrix/PSM/IO/mast.pm
@@ -112,8 +112,8 @@ sub new {
 	    $self->_readline;
 	    %instances=_get_genes($self);
 	    $self->{instances}=\%instances;
-      	if !(%instances) {
-        	$self->warn "Your MAST analysis did not find any matches satisfying the current thershold.\nSee MAST documentation for more information.\n";
+      	if (!(%instances)) {
+        	$self->warn ("Your MAST analysis did not find any matches satisfying the current thershold.\nSee MAST documentation for more information.\n");
         	return $self; #The header might be useful so we return the object, not undef
       	}
 	    next;
