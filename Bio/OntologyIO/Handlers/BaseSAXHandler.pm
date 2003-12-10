544faae7e6589fb0463804a06b0761a0ae35e408
juguang <juguang@bioperl.org>
2003-12-10 06:48:50 +0000

    add BaseSAXHandler and modify InterPro_BioSQL_Handler to inherit it
    svn path=/bioperl-live/trunk/; revision=7330

---
+++ Bio/OntologyIO/Handlers/BaseSAXHandler.pm
@@ -0,0 +1,212 @@
+#
+#
+#
+
+=head1 NAME
+
+Bio::OntologyIO::Handlers::BaseSAXHandler
+
+=head1 DESCRIPTION
+
+This module is an abstract module, serving as the base of any SAX Handler 
+implementation. It tries to offer the framework that SAX handlers generally 
+need, such as tag_stack, char_store, etc.
+
+In the implementation handler, you can take advantage of this based module by
+the following suggestions.
+
+1) In start_element,
+
+ sub start_element {
+     my $self=shift;
+     my $tag=$_[0]->{Name};
+     my %args=%{$_[0]->{Attributes}};
+     # Your code here.
+     
+     # Before you enclose this method, write these 2 line.
+     $self->_visited_count_inc($tag);
+     $self->_push_tag($tag);
+ }
+
+2) In end_element,
+
+ sub end_element {
+     my $self=shift;
+     my $tag=shift->{Name};
+     # Your code here.
+
+     # Before you enclode this method, wirte these 2 lines.
+     $self->_visited_count_dec($tag);
+     $self->_pop_tag;
+ }
+
+3) In characters, or any other methods where you may used the tag stack or 
+count
+ sub characters {
+     my $self=shift;
+     my $text=shift->{Data};
+     
+     $self->_chars_hash->{$self->_top_tag} .= $text;
+
+ }
+    $count = $self->_visited_count('myTag');
+    $tag = $self->_top_tag;
+
+
+=head1 AUTHOR
+
+Juguang Xiao, juguang@tll.org.sg
+
+=head2 APPENDIX
+
+The rest of the documentation details each of the object methods.
+Interal methods are usually preceded with a _
+
+=cut
+
+package Bio::OntologyIO::Handlers::BaseSAXHandler;
+use strict;
+use vars qw(@ISA);
+use Bio::Root::Root;
+@ISA=qw(Bio::Root::Root);
+
+
+sub new {
+    my ($class, @args) = @_;
+    my $self=$class->SUPER::new(@args);
+    $self->_initialize(@args);
+    return $self;
+}
+
+sub _initialize {
+    my $self = shift;
+    $self->{_tag_stack} = [];
+    $self->{_visited_count} = {};
+    $self->{_chars_hash} = {};
+}
+
+=head2 _tag_stack
+
+  Title   : _tag_stack
+  Usage   : @tags = $self->_tag_stack;
+  Function: Get an array of tags that have been accessed but not enclosed.
+  Return  : 
+  Args    :    
+
+=cut
+
+sub _tag_stack {
+    return @{shift->{_tag_stack}};
+}
+
+=head2 _push_tag
+
+=cut
+
+sub _push_tag {
+    my($self,$tag)=@_;
+    push @{$self->{_tag_stack}}, $tag;
+}
+
+=head2 _pop_tag
+
+=cut
+
+sub _pop_tag {
+    my $self=shift;
+    return pop @{$self->{_tag_stack}};
+}
+
+=head2 _top_tag
+
+  Title   : _top_tag
+  Usage   : $top = $self->_top_tag;
+  Function: get the top tag in the tag stack.
+  Return  : a tag name
+  Args    : [none]   
+
+=cut
+
+sub _top_tag {
+    my $self = shift;
+    my @stack=@{$self->{_tag_stack}};
+    return $stack[-1];
+# get the last element in an array while remaining it in. There are few  ways
+# 1) $stack[-1]
+# 2) $stack[$#stack]
+# 3) $stack[@stack-1]
+}
+
+
+=head2 _chars_hash
+
+  Title   : _chars_hash
+  Usage   : $hash= $self->_chars_hash;
+  Function: return the character cache for the specific tag
+  Return  : a hash reference, which is intent for character storage for tags
+  Args    : [none]
+
+=cut
+
+sub _chars_hash {
+    return shift->{_chars_hash};
+}
+
+=head2 _visited_count_inc
+
+  Title   : _vistied_count_inc
+  Usage   : $self->vistied_count_inc($tag); # the counter for the tag increase
+  Function: the counter for the tag increase
+  Return  : the current count after this increment
+  Args    : the tag name [scalar]
+
+=cut
+
+sub _visited_count_inc {
+    my ($self, $tag) = @_;
+    my $visited_count=$self->{_visited_count};
+    if(exists $visited_count->{$tag}){
+        $visited_count->{$tag}++;
+    }else{
+        $visited_count->{$tag}=1;
+    }
+    return $visited_count->{$tag};
+}
+
+=head2 _visited_count_dec
+
+  Title   : _visited_count_dec
+  Usage   : $self->_visited_count_dec($tag);
+  Function: the counter for the tag decreases by one
+  Return  : the current count for the specific tag after the decrement
+  Args    : the tag name [scalar]
+
+=cut
+
+sub _visited_count_dec {
+    my ($self, $tag) = @_;
+    my $visited_count=$self->{_visited_count};
+    if(exists $visited_count->{$tag}){
+        $visited_count->{$tag}--;
+    }else{
+        $self->throw("'$tag' has not been visited yet. How to decrease it?!");
+    }
+    return $visited_count->{$tag};
+}
+
+=head2 _visited_count
+
+  Title   : _visited_count
+  Usage   : $count = $self->_visited_count
+  Function: return the counter for the tag
+  Return  : the current counter for the specific tag
+  Args    : the tag name [scalar]
+
+=cut
+
+sub _visited_count {
+    my ($self, $tag) = @_;
+    return $self->{_visited_count}->{$tag};
+}
+
+1;
--- Bio/OntologyIO/Handlers/InterPro_BioSQL_Handler.pm
+++ Bio/OntologyIO/Handlers/InterPro_BioSQL_Handler.pm
@@ -22,23 +22,29 @@ the ontology data in memory, almost impossible to operate it!
 
 Juguang Xiao, juguang@tll.org.sg
 
+=head2 APPENDIX
+
+The rest of the documentation details each of the object methods.
+Interal methods are usually preceded with a _
+
 =cut
 
 package Bio::OntologyIO::Handlers::InterPro_BioSQL_Handler;
 use strict;
 use vars qw(@ISA);
-use Bio::Root::Root;
+use Bio::OntologyIO::Handlers::BaseSAXHandler;
 use Bio::Ontology::Ontology;
 use Bio::Ontology::Term;
 use Bio::Ontology::RelationshipType;
 use Bio::Ontology::Relationship;
-@ISA = qw(Bio::Root::Root);
+@ISA = qw(Bio::OntologyIO::Handlers::BaseSAXHandler);
 
 my $is_a_rel;
 my $count=0;
-sub new {
-    my ($class, @args) = @_;
-    my $self = $class->SUPER::new(@args);
+
+sub _initialize {
+    my($self,@args)=@_;
+    $self->SUPER::_initialize(@args);
     my ($db, $version) = $self->_rearrange(
         [qw(DB  VERSION)], @args);
     defined $db or $self->throw('db must be assigned');
@@ -48,11 +54,8 @@ sub new {
         -definition => "InterPro, $version"
     );
     $self->_ontology($ontology);
-    $self->{_states} = {};
-    $self->{_contents} = {};
     $is_a_rel = Bio::Ontology::RelationshipType->get_instance('IS_A');
     $is_a_rel->ontology($ontology);
-    return $self;
 }
 
 sub start_document {
@@ -69,13 +72,12 @@ sub start_document {
     $ont->add_term($self->create_term(
         -identifier=>'Binding_site', -name=>'Binding_site'));
 
-    
 }
 
 sub start_element {
-    my ($self, $element) = @_;
-    my $tag=$element->{Name};
-    my %args=%{$element->{Attributes}};
+    my $self=shift;
+    my $tag=$_[0]->{Name};
+    my %args=%{$_[0]->{Attributes}};
     my $ont = $self->_ontology;
 
     if($tag eq 'interpro'){
@@ -96,13 +98,14 @@ sub start_element {
         );
         $self->_relationship($rel);
     }
-    $self->_states->{$tag}=1;
+    $self->_visited_count_inc($tag);
+    $self->_push_tag($tag);
 }
 
 
 sub end_element {
-    my ($self, $element)=@_;
-    my $tag=$element->{Name};
+    my $self=shift;
+    my $tag=shift->{Name};
     if($tag eq 'interpro'){
         my $rel = $self->_relationship;
         my $db = $self->_db;
@@ -120,23 +123,28 @@ sub end_element {
 #        print $count++, "\n";
     }elsif($tag eq 'name'){
         my $rel = $self->_relationship;
-        $rel->subject_term->name($self->_name);
+        $rel->subject_term->name($self->_chars_hash->{name});
+        $self->_chars_hash->{name}='';
     }elsif($tag eq 'abstract'){
         my $rel = $self->_relationship;
-        my $abstract = $self->_contents->{abstract};
+        my $abstract = $self->_chars_hash->{abstract};
         $abstract =~ s/\n/ /g;
         $rel->subject_term->definition($abstract);
-        $self->_contents->{abstract} = '';
+        $self->_chars_hash->{abstract} = '';
     }
-    $self->_states->{$tag}=0;
+    $self->_pop_tag;
+    $self->_visited_count_dec($tag);
 }
 
 
-sub characters{
-    my ($self, $characters) = @_;
-    my $text = $characters->{Data};
-    $self->_name($text) if $self->_states->{name};
-    $self->_contents->{abstract} .= $text if $self->_states->{abstract};
+sub characters {
+    my $self=shift;
+    my $text = shift->{Data};
+
+    my $top_tag =$self->_top_tag;
+    $self->_chars_hash->{$top_tag} .= $text;
+    
+#    $self->_chars_hash->{abstract} .= $text if $self->_visited_count('abstract');
 }
 
 sub create_term {
@@ -167,16 +175,4 @@ sub _relationship {
     return $self->{_relationship};
 }
 
-sub _name {
-    my $self=shift;
-    $self->{_name}=shift if @_;
-    return $self->{_name};
-}
-
-sub _contents {
-    return shift->{_contents};
-}
-sub _states {
-    return shift->{_states};
-}
 1;
