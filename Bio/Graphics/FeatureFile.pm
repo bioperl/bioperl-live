13e62835b52acf03f4973076840e2c985276c142
lstein <lstein@bioperl.org>
2004-02-02 20:35:46 +0000

    backed out Jason's very sensible paranoia because it breaks ability to deliberately make an undef setting
    svn path=/bioperl-live/trunk/; revision=7605

--- Bio/Graphics/FeatureFile.pm
+++ Bio/Graphics/FeatureFile.pm
@@ -720,7 +720,12 @@ sub _setting {
   my $config = $self->{config} or return;
   return keys %{$config} unless @_;
   return keys %{$config->{$_[0]}} if @_ == 1;
-  return $config->{$_[0]}{$_[1]}  if @_ > 1 && exists $config->{$_[0]};
+
+  # this seems reasonable, but it prevents deliberate assignment of
+  # undef to settings
+  # return $config->{$_[0]}{$_[1]}  if @_ > 1 && exists $config->{$_[0]};
+
+  return $config->{$_[0]}{$_[1]}  if @_ > 1;
 }
 
 
