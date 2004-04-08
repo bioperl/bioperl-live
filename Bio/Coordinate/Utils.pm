bc250373343b819641c6d30cd2c9bc1caa9eb197
jason <jason@bioperl.org>
2004-04-08 20:36:23 +0000

    new code to map from sequence to position in alignment rather than position in the other sequence for a pair
    svn path=/bioperl-live/trunk/; revision=7831

--- Bio/Coordinate/Utils.pm
+++ Bio/Coordinate/Utils.pm
@@ -22,6 +22,12 @@ Bio::Coordinate::Utils - Additional methods to create Bio::Coordinate objects
 
     $mapper = Bio::Coordinate::Utils->from_align($aln, 1);
 
+    # Build a set of mappers which will map, for each sequence,
+    # that sequence position in the alignment (exon position to alignment 
+    # position) 
+    my @mappers = Bio::Coordinate::Utils->from_seq_to_alignmentpos($aln);
+
+
 =head1 DESCRIPTION
 
 This class is a holder of methods that work on or create
@@ -60,6 +66,8 @@ Address:
      EMBL Outstation, European Bioinformatics Institute
      Wellcome Trust Genome Campus, Hinxton
      Cambs. CB10 1SD, United Kingdom
+
+Jason Stajich jason at bioperl.org
 
 =head1 APPENDIX
 
@@ -106,7 +114,7 @@ sub from_align {
    my ($self, $aln, $ref ) = @_;
 
    $aln->isa('Bio::Align::AlignI') ||
-       $self->throw('Not a Bio::Align::AlignI object but ['. ref($self). ']');
+       $self->throw('Not a Bio::Align::AlignI object but ['. ref($aln). ']');
 
    # default reference sequence to the first sequence
    $ref ||= 1;
@@ -135,20 +143,20 @@ sub from_align {
 	   (-seq_id => $seq2->id,
 	    -start  => $seq2->location_from_column($start)->start,
 	    -end    => $seq2->location_from_column($end)->start,
-	    -strand => $seq2->strand );
+	    -strand => $seq2->strand );       
        
        my $pair = Bio::Coordinate::Pair->new
 	   (-in  => $match1,
 	    -out => $match2
 	    );
-       if( ! $pair->test ) {
+       unless( $pair->test ) {
 	   $self->warn(join("",
 			    "pair align did not pass test ($start..$end):\n",
 			    "\tm1=",$match1->to_FTstring(), " len=",
 			    $match1->length, 
 			    " m2=", $match2->to_FTstring()," len=", 
 			    $match2->length,"\n"));
-		   }
+       }
        $collection->add_mapper($pair);
    }
    return ($collection->each_mapper)[0] if $collection->mapper_count == 1;
@@ -156,6 +164,75 @@ sub from_align {
 
 }
 
+=head2 from_align
+
+ Title   : from_align
+ Usage   : $mapper = Bio::Coordinate::Utils->from_align($aln, 1);
+ Function:
+           Create a mapper out of an alignment.
+           The mapper will map the position of a sequence into that position
+           in the alignment.
+
+           Will work on alignments of >= 2 sequences 
+ Returns : An array of Bio::Coordinate::MapperI
+ Args    : Bio::Align::AlignI object
+
+=cut
+
+
+sub from_seq_to_alignmentpos {
+    my ($self, $aln ) = @_;
+
+    $aln->isa('Bio::Align::AlignI') ||
+	$self->throw('Not a Bio::Align::AlignI object but ['. ref($aln). ']');
+
+    # default reference sequence to the first sequence
+    my @mappers;
+    $aln->map_chars('\.','-');
+    for my $seq ( $aln->each_seq ) { 
+	my $collection = Bio::Coordinate::Collection->new(-return_match=>1);
+	my $cs = $seq->seq();
+	# do we change this over to use index and substr for speed?
+	while ( $cs =~ /([^\-]+)/g) {
+	    # alignment coordinates
+	    my $lenmatch = length($1);
+	    my $start = pos($cs) - $lenmatch +1;
+	    my $end   = $start + $lenmatch -1;
+
+	    my $match1 = Bio::Location::Simple->new
+		(-seq_id => $seq->id,
+		 -start  => $seq->location_from_column($start)->start,
+		 -end    => $seq->location_from_column($end)->start,
+		 -strand => $seq->strand );
+
+	    my $match2 = Bio::Location::Simple->new
+		(-seq_id => 'alignment',
+		 -start  => $start,
+		 -end    => $end,
+		 -strand => 0 );
+
+	    my $pair = Bio::Coordinate::Pair->new
+		(-in  => $match1,
+		 -out => $match2
+		 );
+	    unless ( $pair->test ) {
+		$self->warn(join("",
+				 "pair align did not pass test ($start..$end):\n",
+				 "\tm1=",$match1->to_FTstring(), " len=",
+				 $match1->length, 
+				 " m2=", $match2->to_FTstring()," len=", 
+				 $match2->length,"\n"));
+	    }
+	    $collection->add_mapper($pair);
+	}
+	if( $collection->mapper_count == 1) {
+	    push @mappers, ($collection->each_mapper)[0];
+	} else { 
+	    push @mappers, $collection;
+	}
+    }
+    return @mappers;
+}
 
 
 1;
