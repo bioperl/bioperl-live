a8ef4bffd80525d641e754fc2ee5d5b1cc791079
bosborne <bosborne@bioperl.org>
2005-02-18 03:40:00 +0000

    Add id_parser method
    svn path=/bioperl-live/trunk/; revision=8769

--- Bio/Index/GenBank.pm
+++ Bio/Index/GenBank.pm
@@ -11,8 +11,8 @@
 
 =head1 NAME
 
-Bio::Index::GenBank - Interface for indexing (multiple) GenBank
-.seq files (ie flat file GenBank format).
+Bio::Index::GenBank - Interface for indexing one or more GenBank
+files (i.e. flat file GenBank format).
 
 =head1 SYNOPSIS
 
@@ -22,8 +22,8 @@ Bio::Index::GenBank - Interface for indexing (multiple) GenBank
     use strict;
 
     my $Index_File_Name = shift;
-    my $inx = Bio::Index::GenBank->new('-filename' => $Index_File_Name, 
-				       '-write_flag' => 'WRITE');
+    my $inx = Bio::Index::GenBank->new(-filename => $Index_File_Name, 
+				       -write_flag => 'WRITE');
     $inx->make_index(@ARGV);
 
     # Print out several sequences present in the index
@@ -33,7 +33,7 @@ Bio::Index::GenBank - Interface for indexing (multiple) GenBank
     use strict;
 
     my $Index_File_Name = shift;
-    my $inx = Bio::Index::GenBank->new('-filename' => $Index_File_Name);
+    my $inx = Bio::Index::GenBank->new(-filename => $Index_File_Name);
     my $seqio = new Bio::SeqIO(-format => 'gcg');
     foreach my $id (@ARGV) {
         my $seq = $inx->fetch($id); # Returns Bio::Seq object
@@ -42,15 +42,31 @@ Bio::Index::GenBank - Interface for indexing (multiple) GenBank
 
     # alternatively
     my ($id, $acc);
-    my $seq1 = $inx->get_Seq_by_id($id);
-    my $seq2 = $inx->get_Seq_by_acc($acc);   
+    my $seq1 = $inx->get_Seq_by_id($locus);
+    my $seq2 = $inx->get_Seq_by_acc($acc);
 
 =head1 DESCRIPTION
 
-Inherits functions for managing dbm files from Bio::Index::Abstract.pm,
-and provides the basic funtionallity for indexing GenBank files, and
-retrieving the sequence from them. Heavily snaffled from James Gilbert's
-Fasta system. Note: for best results 'use strict'.
+By default the index that's created uses the LOCUS, ACCESSION, and
+VERSION identifiers as keys. Inherits functions for managing dbm 
+files from Bio::Index::Abstract.pm, and provides the basic 
+functionality for indexing GenBank files, and retrieving the 
+sequence from them. For best results 'use strict'.
+
+You can also set or customize the unique key used to retrieve by 
+writing your own function and calling the id_parser() method.
+For example:
+
+   $inx->id_parser(\&get_id);
+   # make the index
+   $inx->make_index($file_name);
+
+   # here is where the retrieval key is specified
+   sub get_id {
+      my $line = shift;
+      $line =~ /clone="(\S+)"/;
+      $1;
+   }
 
 Details on configuration and additional example code are available in the
 biodatabases.pod file.
@@ -71,9 +87,8 @@ of the Bioperl mailing lists.  Your participation is much appreciated.
 
 Report bugs to the Bioperl bug tracking system to help us keep track
 the bugs and their resolution.  Bug reports can be submitted via
-email or the web:
+the web:
 
-  bioperl-bugs@bio.perl.org
   http://bugzilla.bioperl.org/
 
 =head1 AUTHOR - Ewan Birney
@@ -82,13 +97,12 @@ Email - birney@ebi.ac.uk
 
 =head1 APPENDIX
 
-The rest of the documentation details each of the object methods. Internal methods are usually preceded with a _
+The rest of the documentation details each of the object methods. 
+Internal methods are usually preceded with a _
 
 =cut
 
-
 # Let's begin the code...
-
 
 package Bio::Index::GenBank;
 
@@ -111,8 +125,8 @@ sub _version {
 =head2 _index_file
 
   Title   : _index_file
-  Usage   : $index->_index_file( $file_name, $i )
-  Function: Specialist function to index GenBank format files.
+  Usage   : $index->_index_file($file_name, $i)
+  Function: Specialized function to index GenBank format files.
             Is provided with a filename and an integer
             by make_index in its SUPER class.
   Example : 
@@ -127,55 +141,73 @@ sub _index_file {
 		 $i     # Index-number of file being indexed
 	  ) = @_;
 
-	my( $begin, # Offset from start of file of the start
-		         # of the last found record.
-		 $id,    # ID of last found record.
-		 @accs,   # accession of last record. Also put into the index
-	  );
+	my $begin = 0;
 
-	$begin = 0;
+	my $id_parser = $self->id_parser;
 
-	open GenBank, $file or $self->throw("Can't open file for read : $file");
+	open GENBANK,$file or 
+	  $self->throw("Can't open file for read : $file");
 
-	# Main indexing loop
-	$id = undef;
-	@accs = ();
-	while (<GenBank>) {
-		if( /^\/\// ) {
-			if( ! defined $id ) {
-				$self->throw("Got to a end of entry line for an GenBank flat file with no parsed ID. Considering this a problem!");
-				next;
-			}
-			if( ! @accs ) {
-				$self->warn("For id [$id] in GenBank flat file, got no accession number. Storing id index anyway");
-			}
-			$self->add_record($id, $i, $begin);
-
-			foreach my $acc (@accs) {
-				if( $acc ne $id ) {
-					$self->add_record($acc, $i, $begin);
-				}
-			}
-			@accs = ();
-		} elsif (/^LOCUS\s+(\S+)/) {
-			$id = $1;
-			# not sure if I like this. Assumes tell is in bytes.
-			# we could tell before each line and save it.
-			$begin = tell(GenBank) - length( $_ ); 
-		} elsif (/^ACCESSION(.*)/) { # ignore ? if there.
-			@accs = ($1 =~ /\s*(\S+)/g);
-		} elsif( /^VERSION(.*)/) {
-			my $a = $1;
-			$a =~ s/^\s+//;
-			$a =~ s/\s+$//;
-			$a =~ s/GI\://;
-			push @accs, split(/\s+/,$a);
-		} else {
-			# do nothing
+	while (<GENBANK>) {
+		if (/^LOCUS/) {
+			$begin = tell(GENBANK) - length($_);
+		}
+		for my $id (&$id_parser($_)) {
+			$self->add_record($id, $i, $begin) if $id;
 		}
 	}
-	close GenBank;
-	return 1;
+	close GENBANK;
+	1;
+}
+
+=head2 id_parser
+
+  Title   : id_parser
+  Usage   : $index->id_parser( CODE )
+  Function: Stores or returns the code used by record_id to
+            parse the ID for record from a string.
+            Returns \&default_id_parser (see below) if not
+            set. An entry will be added to
+            the index for each string in the list returned.
+  Example : $index->id_parser( \&my_id_parser )
+  Returns : reference to CODE if called without arguments
+  Args    : CODE
+
+=cut
+
+sub id_parser {
+	my ($self,$code) = @_;
+
+	if ($code) {
+		$self->{'_id_parser'} = $code;
+	}
+	return $self->{'_id_parser'} || \&default_id_parser;
+}
+
+=head2 default_id_parser
+
+  Title   : default_id_parser
+  Usage   : $id = default_id_parser($line)
+  Function: The default parser for GenBank.pm
+  Returns : Array of specified id's
+  Args    : a line string
+
+=cut
+
+sub default_id_parser {
+	my $line = shift;
+	my @accs = ();
+	if ( $line =~ /^LOCUS\s+(\S+)/ ) {
+		push @accs,$1;
+	} elsif ( $line =~ /^ACCESSION(.*)/ ) {
+		(@accs) = $1 =~ /\s*(\S+)/g;
+	} elsif ( /^VERSION\s+(.*)/) {
+		my $a = $1;
+		$a =~ s/\s+$//;
+		$a =~ s/GI\://;
+		push @accs, split(/\s+/,$a);
+	}
+	@accs;
 }
 
 =head2 _file_format
@@ -187,13 +219,13 @@ sub _index_file {
  Returns : 
  Args    :
 
-
 =cut
 
 sub _file_format{
    my ($self,@args) = @_;
-
    return 'GenBank';
 }
 
 1;
+
+__END__
