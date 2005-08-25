529709ef09788866658c9abd1d7a510558895283
heikki <heikki@bioperl.org>
2005-08-25 07:34:52 +0000

    POD fixes: place holder for documentation
    svn path=/bioperl-live/trunk/; revision=9149

--- Bio/FeatureIO/interpro.pm
+++ Bio/FeatureIO/interpro.pm
@@ -1,3 +1,55 @@
+
+=head1 NAME
+
+Bio::FeatureIO::interpro - DESCRIPTION of Object
+
+=head1 SYNOPSIS
+
+Give standard usage here
+
+=head1 DESCRIPTION
+
+Add description here
+
+=head1 FEEDBACK
+
+=head2 Mailing Lists
+
+User feedback is an integral part of the evolution of this and other
+Bioperl modules. Send your comments and suggestions preferably to
+the Bioperl mailing list.  Your participation is much appreciated.
+
+  bioperl-l@bioperl.org              - General discussion
+  http://bioperl.org/MailList.shtml  - About the mailing lists
+
+=head2 Reporting Bugs
+
+Report bugs to the Bioperl bug tracking system to help us keep track
+of the bugs and their resolution. Bug reports can be submitted via
+the web:
+
+  http://bugzilla.bioperl.org/
+
+=head1 AUTHOR - Allen Day
+
+Email allenday@ucla.edu
+
+Describe contact details here
+
+=head1 CONTRIBUTORS
+
+Additional contributors names and emails here
+
+=head1 APPENDIX
+
+The rest of the documentation details each of the object methods.
+Internal methods are usually preceded with a _
+
+=cut
+
+
+# Let the code begin...
+
 package Bio::FeatureIO::interpro;
 
 use strict;
