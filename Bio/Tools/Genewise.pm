b21fd66ecbe161c8fad4cc4df1f2ded3c29249a7
bosborne <bosborne@bioperl.org>
2006-04-04 19:57:38 +0000

    Bug 1973
    svn path=/bioperl-live/trunk/; revision=9802

--- Bio/Tools/Genewise.pm
+++ Bio/Tools/Genewise.pm
@@ -215,61 +215,60 @@ sub parsed {
 }
   
 sub _parse_genes {
-    my ($self) = @_;
-    my @genes;
-    local ($/) = "//";
+	my ($self) = @_;
+	my @genes;
+	local ($/) = "//";
 
-    while ( defined($_ = $self->_readline) ) {
-      	$self->debug( $_ ) if( $self->verbose > 0);
-        if( /Score\s+(\-?\d+(\.\d+)?)/ ) {
-	    $self->_score($1);# unless defined $self->_score;
-	    
-	} 
-	if( /Query\s+(?:protein|model)\:\s+(\S+)/ ) {
-	    $self->_prot_id($1); #unless defined $self->_prot_id;
-	} 
+	while ( defined($_ = $self->_readline) ) {
+		$self->debug( $_ ) if( $self->verbose > 0);
+		if( /Score\s+(\-?\d+(\.\d+)?)/ ) {
+	      $self->_score($1);# unless defined $self->_score;    
+      } 
+      if( /Query\s+(?:protein|model)\:\s+(\S+)/ ) {
+	      $self->_prot_id($1); #unless defined $self->_prot_id;
+	   } 
 	
-	if( /Target Sequence\s+(\S+)/ ) {	
+     if( /Target Sequence\s+(\S+)/ ) {	
 	    $self->_target_id($1);# unless defined $self->_target_id;
-	}
-        next unless /Gene\s+\d+\n/;
+	  }
+     next unless /Gene\s+\d+\n/;
 
-        my @genes_txt = split(/Gene\s+\d+\n/,$_);
-        shift @genes_txt; #remove first empty entry
+     my @genes_txt = split(/Gene\s+\d+\n/,$_);
+     shift @genes_txt; #remove first empty entry
        
-        foreach my $gene_txt (@genes_txt) {
+     foreach my $gene_txt (@genes_txt) {
 	    # If genewise has assigned a strand to the gene as a whole
 	    # overall gene start and end
-	    my ($g_start, $g_end, 
-		$type) = $gene_txt =~ m/Gene\s+
-		                       (\d+)\s+       # start (1-based)
-				       (\d+)\s+       # end
-				       (?:\[(\w+)\])? # 
-				       /x;
+	    my ($g_start, $g_end, $type) = 
+			$gene_txt =~ m/Gene\s+
+								(\d+)[\s-]+    # start (1-based)
+								(\d+)\s+       # end
+								(?:\[(\w+)\])? # 
+								/x;
 	    my $g_strand;
 	    my $source_tag = $type ? "$Srctag". "_$type" : $Srctag;
 	    my $genes = new Bio::SeqFeature::Gene::GeneStructure
-		(-source => $source_tag);
+		 (-source => $source_tag);
 	    my $transcript = new Bio::SeqFeature::Gene::Transcript
-		(-source => $source_tag,
+		 (-source => $source_tag,
 		 -score  => $self->_score);
 	    ($g_start, $g_end, $g_strand) = $self->_get_strand($g_start,$g_end);
 	    $genes->strand($g_strand);
 
-	    #grab exon + supporting feature info
+	    # grab exon + supporting feature info
 	    my @exons;
 	    unless ( @exons = $gene_txt =~ m/(Exon .+\s+Supporting .+)/g ) {
-		@exons = $gene_txt =~ m/(Exon .+\s+)/g;
+	 	    @exons = $gene_txt =~ m/(Exon .+\s+)/g;
 	    }
 	    my $nbr = 1;
-	    #loop through each exon-supporting feature pair
+	    # loop through each exon-supporting feature pair
 	    foreach my $e (@exons){
-		my ($e_start,$e_end,
-		    $phase) = $e =~ m/Exon\s+
-			             (\d+)\s+        # start (1 based)
-				     (\d+)\s+        # end
-				     phase\s+(\d+)   # phase
-				     /x;
+		   my ($e_start,$e_end,$phase) = 
+                 $e =~ m/Exon\s+
+			                (\d+)[\s-]+     # start (1 based)
+				             (\d+)\s+        # end
+				             phase\s+(\d+)   # phase
+				             /x;
 		my $e_strand;
 		($e_start,$e_end,$e_strand) = $self->_get_strand($e_start,
 								 $e_end);
