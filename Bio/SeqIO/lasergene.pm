5c176ed0abea4985595d1bb28ea82c54142d20d4
tseemann <tseemann@bioperl.org>
2006-06-08 05:50:37 +0000

    Fixed to actually pass "perl -c", removed cruft, converted die to throw
    svn path=/bioperl-live/trunk/; revision=9962

--- Bio/SeqIO/lasergene.pm
+++ Bio/SeqIO/lasergene.pm
@@ -1,5 +1,5 @@
-#-----------------------------------------------------------------------------
-# PACKAGE : Bio::SeqIO::Lasergene
+#-----------------------------------------5~------------------------------------
+# PACKAGE : Bio::SeqIO::lasergene
 # AUTHOR  : Malcolm Cook <mec@stowers-institute.org>
 # CREATED : Feb 16 1999
 # REVISION: $Id$
@@ -8,12 +8,14 @@
 #
 # This code is based on the Bio::SeqIO::raw module with
 # the necessary minor tweaks necessary to get it to read (only) Lasergene formatted sequences 
+#
+# Cleaned up by Torsten Seemann 2006-06-08
 
 # POD documentation - main docs before the code
 
 =head1 NAME
 
-Bio::SeqIO::Lasergene - Lasergene sequence file input/output stream
+Bio::SeqIO::lasergene - Lasergene sequence file input/output stream
 
 =head1 SYNOPSIS
 
@@ -26,6 +28,10 @@ This object can product Bio::Seq::RichSeq objects from Lasergene sequence files.
 IT DOES NOT PARSE ANY ATTIBUTE VALUE PAIRS IN THE HEADER OF THE LASERGENE FORMATTED FILE.
 
 IT DOES NOT WRITE THESE FILES EITHER.
+
+=head1 REFERENCES
+
+  https://www.dnastar.com/products/lasergene.php
 
 =head1 FEEDBACK
 
@@ -79,39 +85,37 @@ use Bio::Seq;
  Returns : Bio::Seq object
  Args    :
 
-
 =cut
-use Bio::Annotation;
+
+use Bio::Annotation::Collection;
 use Bio::Annotation::Comment;
 
-sub next_seq{
-   my ($self,@args) = @_;
+sub next_seq {
+   my ($self) = @_;
    return unless defined($_ = $self->_readline()); # stream is empty! there is no next seq.
-   my $seq =  Bio::Seq::RichSeq->new();
-   my $comment = "";
+   my @comment;
    while (! /\^\^/) {
      chomp;
-     $comment .= $_ . ";  ";
+     push(@comment, $_);
 
      #could consider populating BioSeq fields here, but I'm not in that business now!!!
      #my ($att,@val) = split  /\:\w*/  ; #  m/(.*)\:(.*)/;     
      #print STDERR "att:$att;val:@val\n";
 
-     $_ = $self->_readline() or die "unexpected end of file";
+     $_ = $self->_readline() or $self->throw("unexpected end of lasergene file");
    }
-   $_ = $self->_readline() or die "unexpected end of file"; # this should be all and only sequence.
-   $seq->seq($_);
+   $_ = $self->_readline() or $self->throw("unexpected end of lasergene file"); # this should be all and only sequence.
 
-   my ($annotation,%params,@features) = (new Bio::Annotation::Collection);
-   my $commobj = Bio::Annotation::Comment->new();
-   $commobj->text($comment);
-   $annotation->add_Annotation('comment',$commobj);
+   my $seq = Bio::Seq::RichSeq->new(-seq => $_);
+   my $annotation = Bio::Annotation::Collection->new;
+   my $comment = join('; ', @comment);
+   $annotation->add_Annotation( 'comment', Bio::Annotation::Comment->new(-text => $comment) );
    $seq->annotation($annotation);
 
    return $seq;
 }
 
-=head2 write_seq
+=head2 write_seq (NOT IMPLEMENTED)
 
  Title   : write_seq
  Usage   : $stream->write_seq($seq)
@@ -119,19 +123,13 @@ sub next_seq{
  Returns : 1 for success and 0 for error
  Args    : Bio::Seq object
 
-
 =cut
 
-
-=head2 write_qual
-
- Title   : write_qual
- Usage   : $stream->write_qual($seq)
- Function: writes the $seq object into the stream
- Returns : 1 for success and 0 for error
- Args    : Bio::Seq object
-
-
-=cut
+sub write_seq {
+  my ($self, $seq) = @_;
+  $self->throw("write_seq() is not implemented for the lasergene format.");
+}
+        
 
 1;
+
