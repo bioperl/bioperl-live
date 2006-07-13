73ebdbd80ab7d9da3f552a2dfef314e9da51056d
lstein <lstein@bioperl.org>
2006-07-13 19:48:58 +0000

    fixed problem in which full color range was not utilized in truecolor images
    svn path=/bioperl-live/trunk/; revision=10131

--- Bio/Graphics/Panel.pm
+++ Bio/Graphics/Panel.pm
@@ -895,8 +895,12 @@ sub translate_color {
 sub colorClosest {
   my ($self,$gd,@c) = @_;
   return $self->{closestcache}{"@c"} if exists $self->{closestcache}{"@c"};
-  return $self->{closestcache}{"@c"} = $gd->colorClosest(@c) if $GD::VERSION < 2.04;
-  my ($value,$index);
+  return $self->{closestcache}{"@c"} = $gd->colorResolve(@c) if $GD::VERSION < 2.04;
+
+  my $index = $gd->colorResolve(@c);
+  return $self->{closestcache}{"@c"} = $index if $index >= 0;
+
+  my $value;
   for (keys %COLORS) {
     my ($r,$g,$b) = @{$COLORS{$_}};
     my $dist = ($r-$c[0])**2 + ($g-$c[1])**2 + ($b-$c[2])**2;
