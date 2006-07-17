8d8b5e5c4ffb1ec13bb0e0dcd94f59efc9488730
scain <scain@bioperl.org>
2006-07-17 17:02:50 +0000

    bumping biographics api version in prep for a gbrowse release
    svn path=/bioperl-live/trunk/; revision=10146

--- Bio/Graphics/Panel.pm
+++ Bio/Graphics/Panel.pm
@@ -19,7 +19,7 @@ my %COLORS;  # translation table for symbolic color names to RGB triple
 my $IMAGEMAP = 'bgmap00001';
 read_colors();
 
-sub api_version { 1.653 }
+sub api_version { 1.654 }
 
 # Create a new panel of a given width and height, and add lists of features
 # one by one
