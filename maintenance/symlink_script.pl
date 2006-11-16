aa7f77f8083d8c07ae494d5f2fa758693642504f
sendu <sendu@bioperl.org>
2006-11-16 10:41:49 +0000

    newline after printing message
    svn path=/bioperl-live/trunk/; revision=10896

--- maintenance/symlink_script.pl
+++ maintenance/symlink_script.pl
@@ -13,7 +13,7 @@ my %symlink_scripts = ('bp_bulk_load_gff.pl' => 'bp_pg_bulk_load_gff.pl');
 # well have just done a copy
 
 my $install_dir = $build->install_destination('script');
-print "install_dir is $install_dir\n";
+$build->log_info("Will try to install symlinks to $install_dir");
 my $orig_dir = $build->cwd;
 chdir($install_dir);
 
