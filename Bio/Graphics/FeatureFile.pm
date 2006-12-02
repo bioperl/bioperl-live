5a79fc8caaa7d728076acb279d7d29469a5fb0e5
lstein <lstein@bioperl.org>
2006-12-02 18:35:45 +0000

    needed an additional tweak for headerless gff files
    svn path=/bioperl-live/trunk/; revision=11017

--- Bio/Graphics/FeatureFile.pm
+++ Bio/Graphics/FeatureFile.pm
@@ -118,7 +118,6 @@ use strict;
 use Bio::Graphics::Feature;
 use Bio::DB::GFF::Util::Rearrange;
 use Carp 'cluck','carp','croak';
-# use Bio::DB::GFF;
 use IO::File;
 use Text::ParseWords 'shellwords';
 
@@ -498,6 +497,7 @@ sub parse_line {
 
   # conventional GFF file, with check for numeric start/end
   if (@tokens >= 8 && $tokens[3]=~ /^-?\d+$/ && $tokens[4]=~ /^-?\d+$/) {
+    require Bio::DB::GFF unless Bio::DB::GFF->can('split_group');
     my ($r,$source,$method,$start,$stop,$scor,$s,$phase,@rest) = @tokens;
     # sanity checks
     my $group = join ' ',@rest;
