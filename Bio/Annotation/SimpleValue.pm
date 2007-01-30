62bbf956e763b8b4553c61fd0bd118607f90dcea
mcook <mcook@bioperl.org>
2007-01-30 23:01:52 +0000

    provide fallback of operator overloading
    svn path=/bioperl-live/trunk/; revision=11165

--- Bio/Annotation/SimpleValue.pm
+++ Bio/Annotation/SimpleValue.pm
@@ -62,8 +62,12 @@ The rest of the documentation details each of the object methods. Internal metho
 
 package Bio::Annotation::SimpleValue;
 use strict;
-use overload '""' => sub { $_[0]->value};
-use overload 'eq' => sub { "$_[0]" eq "$_[1]" };
+
+use overload
+  '""' => sub { $_[0]->value},
+  'eq' => sub { "$_[0]" eq "$_[1]" },
+  fallback => 1
+  ;
 
 # Object preamble - inherits from Bio::Root::Root
 
