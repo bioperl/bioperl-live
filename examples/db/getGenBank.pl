38d7684d60e5c819b433ca8ef78aa6f321a13713
cjfields <cjfields@bioperl.org>
2007-09-14 16:37:11 +0000

    mail list patch from Jay Hannah
    svn path=/bioperl-live/trunk/; revision=11691

--- examples/db/getGenBank.pl
+++ examples/db/getGenBank.pl
@@ -21,8 +21,9 @@ $seq = $gb->get_Seq_by_acc('AF303112');
 
 $seqout->write_seq($seq);
 
-# if you want to get a bunch of sequences use the batch method
-my $seqio = $gb->get_Stream_by_batch([ qw(J00522 AF303112 2981014)]); 
+# feel free to pull multiple sequences...
+# if you want to get a bunch of sequences use the get_Stream_by_id/acc methods
+my $seqio = $gb->get_Stream_by_id([ qw(J00522 AF303112 2981014)]); 
 
 while( defined ($seq = $seqio->next_seq )) {
         $seqout->write_seq($seq);
