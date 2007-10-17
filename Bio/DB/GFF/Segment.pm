ad2687f7e6c7b9e35e36e1e70310a553bb34b50d
lstein <lstein@bioperl.org>
2007-10-17 01:22:29 +0000

    squelched warning about empty sequence in Bio::DB::GFF::Feature->seq() call when no DNA has been attached
    svn path=/bioperl-live/trunk/; revision=11725

--- Bio/DB/GFF/Segment.pm
+++ Bio/DB/GFF/Segment.pm
@@ -308,6 +308,7 @@ sub seq {
   my $self = shift;
   my $dna = $self->dna;
   require Bio::PrimarySeq unless Bio::PrimarySeq->can('new');
+  return Bio::PrimarySeq->new(-id => $self->display_name) unless $dna;
   return Bio::PrimarySeq->new(-seq => $dna,
 			      -id  => $self->display_name);
 }
