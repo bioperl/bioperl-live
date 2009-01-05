18dd2370d19192013421c291a0100f90049793a6
cjfields <cjfields@bioperl.org>
2009-01-05 04:23:35 +0000

    Bump RC to 2; will tag, bag, and ship tomorrow after tests
    svn path=/bioperl-live/branches/branch-1-6/; revision=15292

--- Bio/Root/Build.pm
+++ Bio/Root/Build.pm
@@ -102,7 +102,7 @@ BEGIN {
 use strict;
 use warnings;
 
-our $VERSION = '1.005009_001';
+our $VERSION = '1.005009_002';
 our @extra_types = qw(options excludes_os feature_requires test); # test must always be last in the list!
 our $checking_types = "requires|conflicts|".join("|", @extra_types);
 
