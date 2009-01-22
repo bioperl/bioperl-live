5074d8dfd25775356cb2355d6ed9f0acbf5bbf22
fangly <fangly@bioperl.org>
2009-01-22 18:11:35 +0000

    Minor cleaning
    svn path=/bioperl-live/trunk/; revision=15437

--- Bio/DB/Qual.pm
+++ Bio/DB/Qual.pm
@@ -1,5 +1,5 @@
 #
-# $Id: Qual.pm,v 1.23 2007/06/14 14:16:10 sendu Exp $
+# $Id$
 #
 # BioPerl module for Bio::DB::Qual
 #
@@ -422,7 +422,7 @@ These are optional arguments to pass in as well.
                for searching for qual
                files in directories. 
 
- -makeid       A code subroutine for     None
+ -makeid       A code subroutine for     none
                transforming qual IDs.
 
  -maxopen      Maximum size of           32
@@ -448,8 +448,8 @@ sub new {
     my $self = bless {
         debug      => $opts{-debug},
         makeid     => $opts{-makeid},
-        glob       => $opts{-glob} || '*.{qual,QUAL,qa,QA}',
-        maxopen    => $opts{-maxfh}   || 32,
+        glob       => $opts{-glob}    || '*.{qual,QUAL,qa,QA}',
+        maxopen    => $opts{-maxopen} || 32,
         dbmargs    => $opts{-dbmargs} || undef,
         fhcache    => {},
         cacheseq   => {},
