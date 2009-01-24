96aa99d7723c61e72a2ec11ee65b761e5d0467cd
cjfields <cjfields@bioperl.org>
2009-01-24 21:31:23 +0000

    Bump version to 1.6 for final release.
    svn path=/bioperl-live/branches/branch-1-6/; revision=15443

--- Bio/Root/Version.pm
+++ Bio/Root/Version.pm
@@ -85,7 +85,7 @@ methods. Internal methods are usually preceded with a _
 package Bio::Root::Version;
 use strict;
 
-our $VERSION = '1.005009_004';
+our $VERSION = '1.006000';
 $VERSION = eval $VERSION;
 
 sub import {
