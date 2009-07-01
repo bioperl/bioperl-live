5ddcc9ef55d60fc1b333bea5b2890cb77b85ade5
cjfields <cjfields@bioperl.org>
2009-07-01 00:32:15 +0000

    small revert to get write_qual working with sanger output
    svn path=/bioperl-live/trunk/; revision=15814

--- Bio/SeqIO/fastq.pm
+++ Bio/SeqIO/fastq.pm
@@ -121,8 +121,8 @@ sub _initialize {
 sub next_seq {
     my( $self ) = @_;
     
-	# separate out the parsing into a separate sub (next_chunk). We can probably
-	# add in a hook here to allow XS-based parsing vs. a pure perl solution
+    # separate out the parsing into a separate sub (next_chunk). We can probably
+    # add in a hook here to allow XS-based parsing vs. a pure perl solution
     
     while (defined(my $data = $self->next_dataset)) {
         # Are FASTQ sequences w/o any sequence valid?  Commenting out for now.
@@ -141,36 +141,36 @@ sub next_dataset {
     my ($data, $ct);
     my $mode = '-seq';
     
-	# speed this up by directly accessing the filehandle and in-lining the
-	# _readline stuff vs. making the repeated method calls. Tradeoff is speed
-	# over repeated code.
-	
-	# we can probably normalize line endings using PerlIO::eol or
-	# Encode::Newlines
-	
+    # speed this up by directly accessing the filehandle and in-lining the
+    # _readline stuff vs. making the repeated method calls. Tradeoff is speed
+    # over repeated code.
+    
+    # we can probably normalize line endings using PerlIO::eol or
+    # Encode::Newlines
+    
     my $fh = $self->_fh;
-	my $line = $self->{lastline} || <$fh>;
-	
+    my $line = $self->{lastline} || <$fh>;
+    
     FASTQ:
     while ($line) {
-		$line =~ s/\015\012/\012/;
-		$line =~ tr/\015/\n/;
+        $line =~ s/\015\012/\012/;
+        $line =~ tr/\015/\n/;
         if ($mode eq '-seq' && $line =~ m{^@([^\n]+)$}xmso) {
             $data->{-descriptor} = $1;
-			my ($id,$fulldesc);
-			if ($data->{-descriptor} =~ /^\s*(\S+)\s*(.*)/) {
-				($id,$fulldesc) = ($1, $2);
-			} else {
-				$self->throw("Can't parse fastq header");
-			}
-			$data->{-id} = $id;
-			$data->{-desc} = $fulldesc;
+            my ($id,$fulldesc);
+            if ($data->{-descriptor} =~ /^\s*(\S+)\s*(.*)/) {
+                ($id,$fulldesc) = ($1, $2);
+            } else {
+                $self->throw("Can't parse fastq header");
+            }
+            $data->{-id} = $id;
+            $data->{-desc} = $fulldesc;
             $ct->{-seq} = 0;
         } elsif ($mode eq '-seq' && $line =~ m{^\+([^\n]*)?}xmso) {
             $self->throw("No description line parsed") unless $data->{-descriptor};
-			if ($1 && $data->{-descriptor} ne $1) {
-				$self->throw("Quality descriptor [$1] doesn't match seq description ".$data->{-descriptor} );
-			}
+            if ($1 && $data->{-descriptor} ne $1) {
+                $self->throw("Quality descriptor [$1] doesn't match seq description ".$data->{-descriptor} );
+            }
             $mode = '-raw_quality';
             $ct->{-raw_quality} = 0;
         } else {
@@ -185,11 +185,11 @@ sub next_dataset {
             chomp $line;
             $data->{$mode} .= $line;
         }
-		$line = <$fh>;
-		if (!defined $line) {
-			delete $self->{lastline};
-			last FASTQ;
-		}
+        $line = <$fh>;
+        if (!defined $line) {
+            delete $self->{lastline};
+            last FASTQ;
+        }
     }
     
     # simple quality control tests
@@ -200,17 +200,17 @@ sub next_dataset {
         }
         
         my @qual = map {unpack("C",$_) - $self->{ord_start}}
-			unpack("A1" x length($data->{-raw_quality}), $data->{-raw_quality});
-			
-		# this should be somewhat rarer now, but needs to be present JIC
-		if ($self->variant eq 'solexa') {
-			# The conversion needs to be to PHRED score, but solexa (aka illumina 1.0)
-			# has Solexa qual units, not PHRED qual units.  Convert over...
-			# this doesn't account for very low scores yet!
-			@qual = map {sprintf("%.0f",(10 * log(1 + 10 ** ($_ / 10.0)) / log(10)))} @qual;
-		}
-		
-		$data->{-qual} = \@qual;
+            unpack("A1" x length($data->{-raw_quality}), $data->{-raw_quality});
+            
+        # this should be somewhat rarer now, but needs to be present JIC
+        if ($self->variant eq 'solexa') {
+            # The conversion needs to be to PHRED score, but solexa (aka illumina 1.0)
+            # has Solexa qual units, not PHRED qual units.  Convert over...
+            # this doesn't account for very low scores yet!
+            @qual = map {sprintf("%.0f",(10 * log(1 + 10 ** ($_ / 10.0)) / log(10)))} @qual;
+        }
+        
+        $data->{-qual} = \@qual;
     }
     
     return $data;
@@ -229,9 +229,6 @@ sub next_dataset {
 
 sub write_seq {
     my ($self,@seq) = @_;
-	if ($self->variant ne 'sanger') {
-		$self->throw($self->variant." is not supported yet with write_seq");
-	}
     foreach my $seq (@seq) {
         my $str = $seq->seq;
         my $top = $seq->display_id();
@@ -264,6 +261,9 @@ sub write_seq {
 
 sub write_qual {
    my ($self,@seq) = @_;
+    if ($self->variant ne 'sanger') {
+        $self->throw($self->variant." is not supported yet with write_seq");
+    }   
     foreach my $seq (@seq) {
         unless ($seq->isa("Bio::Seq::Quality")){
             $self->warn("You can write FASTQ without supplying a Bio::Seq::Quality object! ", ref($seq), "\n");
@@ -307,33 +307,34 @@ sub write_qual {
 
 sub write_fastq {
     my ($self,@seq) = @_;
+    if ($self->variant ne 'sanger') {
+        $self->throw($self->variant." is not supported yet with write_seq");
+    }
     foreach my $seq (@seq) {
-        unless ($seq->isa("Bio::Seq::Quality")){
-           $self->warn("You can't write FASTQ without supplying a Bio::Seq::Quality object! ", ref($seq), "\n");
-           next;
-        } 
-        my $str = $seq->seq;
-        my ($qstart, $qend) = ($self->{qual_start}, $self->{qual_end});
-        my @qual = @{$seq->qual};
-        my $top = $seq->display_id();
-        if ($seq->can('desc') and my $desc = $seq->desc()) {
-        $desc =~ s/\n//g;
-           $top .= " $desc";
-        }
-        if(length($str) == 0) {
-           $str = "\n";
-        }
-        my $qual = "" ;
-        if(scalar(@qual) > 0) {
-            for my $q (0..$#qual) {
-                $qual .= chr($qual[$q] + $qstart);
-            }
-        } else {
-            $qual = "\n";
-        }
-        
-        $self->_print ("\@",$top,"\n",$str,"\n") or return;
-        $self->_print ("+",$top,"\n",$qual,"\n") or return;
+		unless ($seq->isa("Bio::Seq::Quality")){
+			$self->warn("You can't write FASTQ without supplying a Bio::Seq::Quality object! ", ref($seq), "\n");
+			next;
+		}
+		my $str = $seq->seq;
+		my @qual = @{$seq->qual};
+		my $top = $seq->display_id();
+		if ($seq->can('desc') and my $desc = $seq->desc()) {
+			$desc =~ s/\n//g;
+			$top .= " $desc";
+		}
+		if(length($str) == 0) {
+			$str = "\n";
+		}
+		my $qual = "" ;
+		if(scalar(@qual) > 0) {
+			for (my $q = 0;$q<scalar(@qual);$q++){
+				$qual .= chr($qual[$q] + 33);
+			}
+		} else {
+		   $qual = "\n";
+		}
+		$self->_print ("\@",$top,"\n",$str,"\n") or return;
+		$self->_print ("+",$top,"\n",$qual,"\n") or return;
     }
     return 1;
 }
@@ -347,17 +348,17 @@ sub write_fastq {
            quality data.
            
            Current values accepted are:
-			'sanger'   (orginal FASTQ)
-				ASCII encoding from 33-126, PHRED quality score from 0 to 93
-			'solexa'   (aka illumina1.0)
-				ASCII encoding from 59-104, SOLEXA quality score from -5 to 40
-			'illumina' (aka illumina1.3)
-				ASCII encoding from 64-104, PHRED quality score from 0 to 40
-			
-			(Derived from the MAQ website):
-			For 'solexa', scores are converted to PHRED qual scores using:
-				$Q = 10 * log(1 + 10 ** (ord($sq) - 64) / 10.0)) / log(10)
-			
+            'sanger'   (orginal FASTQ)
+                ASCII encoding from 33-126, PHRED quality score from 0 to 93
+            'solexa'   (aka illumina1.0)
+                ASCII encoding from 59-104, SOLEXA quality score from -5 to 40
+            'illumina' (aka illumina1.3)
+                ASCII encoding from 64-104, PHRED quality score from 0 to 40
+            
+            (Derived from the MAQ website):
+            For 'solexa', scores are converted to PHRED qual scores using:
+                $Q = 10 * log(1 + 10 ** (ord($sq) - 64) / 10.0)) / log(10)
+            
 
  Returns : string
  Args    : new value, string
