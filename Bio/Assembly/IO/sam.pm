e4150b6fad5223a1dcab441383ddac0d1e7b91df
maj <maj@bioperl.org>
2009-11-25 22:23:56 +0000

    pre-alpha Bio::Assembly::IO class for bam/sam format
    svn path=/bioperl-live/trunk/; revision=16399

---
+++ Bio/Assembly/IO/sam.pm
@@ -0,0 +1,650 @@
+# $Id$
+#
+# BioPerl module for Bio::Assembly::IO::sam
+#
+# Please direct questions and support issues to <bioperl-l@bioperl.org>
+#
+# Cared for by Mark A. Jensen <maj -at- fortinbras -dot- us>
+#
+# Copyright Mark A. Jensen
+#
+# You may distribute this module under the same terms as perl itself
+
+# POD documentation - main docs before the code
+
+=head1 NAME
+
+Bio::Assembly::IO::sam - An IO module for assemblies in Sam format *Pre-ALPHA!*
+
+=head1 SYNOPSIS
+
+ $aio = Bio::Assembly::IO( -file => "mysam.bam", -refdb => "myrefseqs.fas");
+
+=head1 DESCRIPTION
+
+Requires L<Bio::DB::Sam>.
+
+=head1 FEEDBACK
+
+=head2 Mailing Lists
+
+User feedback is an integral part of the evolution of this and other
+Bioperl modules. Send your comments and suggestions preferably to
+the Bioperl mailing list.  Your participation is much appreciated.
+
+  bioperl-l@bioperl.org                  - General discussion
+http://bioperl.org/wiki/Mailing_lists  - About the mailing lists
+
+=head2 Support
+
+Please direct usage questions or support issues to the mailing list:
+
+L<bioperl-l@bioperl.org>
+
+rather than to the module maintainer directly. Many experienced and
+reponsive experts will be able look at the problem and quickly
+address it. Please include a thorough description of the problem
+with code and data examples if at all possible.
+
+=head2 Reporting Bugs
+
+Report bugs to the Bioperl bug tracking system to help us keep track
+of the bugs and their resolution. Bug reports can be submitted via
+the web:
+
+  http://bugzilla.open-bio.org/
+
+=head1 AUTHOR - Mark A. Jensen
+
+Email maj -at- fortinbras -dot- us
+
+Describe contact details here
+
+=head1 CONTRIBUTORS
+
+Additional contributors names and emails here
+
+=head1 APPENDIX
+
+The rest of the documentation details each of the object methods.
+Internal methods are usually preceded with a _
+
+=cut
+
+# Let the code begin...
+
+
+package Bio::Assembly::IO::sam;
+use strict;
+use warnings;
+
+# Object preamble - inherits from Bio::Root::Root
+
+use Bio::Seq::Quality;
+use Bio::Seq::PrimaryQual;
+use Bio::LocatableSeq;
+use Bio::Assembly::IO;
+use Bio::Assembly::Scaffold;
+use Bio::Assembly::Contig;
+use Bio::Assembly::Singlet;
+use Bio::SeqIO;
+use File::Spec;
+use File::Basename;
+use Carp;
+use Bio::Root::Root;
+use base qw(Bio::Root::Root Bio::Assembly::IO );
+
+BEGIN {
+# check requirements
+    unless ( eval "require Bio::DB::Sam; 1" ) {
+	Bio::Root::Root->throw("__PACKAGE__ requires installation of samtools (libbam) and Bio::DB::Sam (available on CPAN; not part of BioPerl)");
+    }
+}
+
+# or is it bwa?
+my $progname = 'sam';
+
+sub new {
+    my $class = shift;
+    my @args = @_;
+    my $self = $class->SUPER::new(@args);
+    my ($file, $refdb, $format) = $self->_rearrange([qw(FILE REFDB FORMAT)], @args);
+    $self->file($file);
+    $refdb && $self->refdb($refdb);
+    $self->_init_sam() or croak( "Sam initialization failed" );
+    return $self;
+}
+
+
+=head1 Bio::Assembly::IO compliance
+
+=head2 next_assembly()
+
+    Title   : next_assembly
+    Usage   : my $scaffold = $asmio->next_assembly();
+    Function: return the next assembly in the sam-formatted stream
+    Returns : Bio::Assembly::Scaffold object
+    Args    : none
+
+=cut
+
+sub next_assembly {
+    my $self = shift;
+    my @contig_set;
+    # get Bio::DB::Sam object 
+    # could add a refdb or fasfile attribute to contain the reference db name
+    # iterate through seq_ids...
+    my @refseq_ids = $self->sam->seq_ids;
+    my $assembly = Bio::Assembly::Scaffold->new( -progname => $progname );
+
+    foreach my $id (@refseq_ids) {
+    #### this is choice 1: all refseqs into one assy...###
+	$self->_current_refseq_id( $id );
+	# Load contigs and singlets in the scaffold
+	while ( my $obj = $self->next_contig()) {
+	    # Add contig /singlet to assembly
+	    if ($obj->isa('Bio::Assembly::Singlet')) { # a singlet
+		$assembly->add_singlet($obj);
+	    } else { # a contig
+		$assembly->add_contig($obj);
+	    }
+	}
+    }
+$DB::single=1;    
+    return $assembly;
+}
+
+=head2 next_contig()
+
+    Title   : next_contig
+    Usage   : my $contig = $asmio->next_contig();
+    Function: return the next contig or singlet from the sam stream
+    Returns : Bio::Assembly::Contig or Bio::Assembly::Singlet
+    Args    : none
+
+=cut
+
+sub next_contig {
+    my $self = shift;
+    if (!defined $self->_current_contig_seg_idx) {
+	$self->_current_contig_seg_idx(0);
+    }
+    else {
+	$self->_current_contig_seg_idx( 1+$self->_current_contig_seg_idx );
+    }
+    unless ($self->_current_refseq_id) {
+	croak("No current refseq id set");
+    }
+    my $contig_segs = $self->_segset($self->_current_refseq_id);
+    unless ($contig_segs) {
+	croak("No contig segset for current id '".$self->_current_refseq_id."'")
+    }
+    # each segment in @$contig_segs represents a contig within the
+    # current refseq
+    my $contig_seg = $$contig_segs[$self->_current_contig_seg_idx];
+    # each 'feature' in $contig_seg represents a read;
+    # $seqio lets us iterate efficiently over the reads:
+    my $seqio = $contig_seg->features(-iterator => 1);
+    return if (!defined $contig_seg); # iterator finished
+
+    # Contig and read related
+    my $contigobj = $self->_store_contig($contig_seg);
+    my $numseq = 0;
+
+    while ( my $read = $seqio->next_seq ) {
+	$self->_store_read($read, $contigobj);
+	$numseq++;
+    }
+    if ($numseq == 1) { # ooh! a singlet!
+	$contigobj = $self->_store_singlet($contig_seg, $contigobj);
+    }
+    return $contigobj;
+}
+
+=head1 Internal
+
+=head2  _store_contig()
+
+    Title   : _store_contig
+    Usage   : my $contigobj = $self->_store_contig(\%contiginfo);
+    Function: create and load a contig object
+    Returns : Bio::Assembly::Contig object
+    Args    : Bio::DB::Sam::Segment object
+
+=cut 
+
+sub _store_contig {
+    my ($self, $contig_seg) = @_;
+
+    # Create a contig 
+    my $contigobj = Bio::Assembly::Contig->new(
+        -id     => 'sam_assy['.$self->_basename.':'.$self->_current_refseq_id.']/'.$contig_seg->start.'-'.$contig_seg->end,
+        -source => $progname,
+        -strand => 1
+    );
+    my $consobj = $self->_calc_consensus($contig_seg);
+    my $consensus = Bio::LocatableSeq->new(
+        -id    => $contigobj->id,
+        -seq   => $consobj->seq,
+        -start => 1,
+    );
+    $contigobj->set_consensus_sequence($consensus);
+    $contigobj->set_consensus_quality($consobj);
+
+    # Add other misc contig information as subsequence feature
+#     my @other = grep !/asmbl_id|end|qualobj|start/, keys %$contiginfo;
+#     my %other;
+#     @other{@other} = @$contiginfo{@other};
+#     my $contigtags = Bio::SeqFeature::Generic->new(
+#         -primary_tag => "_main_contig_feature:$$contiginfo{'asmbl_id'}",
+#         -start       => 1,
+#         -end         => $contig_seg->length,
+#         -strand      => 1,
+#         # dumping ground:
+#         -tag         => \%other
+#     );
+#     $contigobj->add_features([ $contigtags ], 1);
+
+    return $contigobj;
+}
+
+=head2 _store_read()
+
+    Title   : _store_read
+    Usage   : my $readobj = $self->_store_read($readobj, $contigobj);
+    Function: store information of a read belonging to a contig in a contig object
+    Returns : Bio::LocatableSeq
+    Args    : Bio::DB::Bam::AlignWrapper, Bio::Assembly::Contig
+
+=cut
+
+sub _store_read {
+    my $self = shift;
+    my ($read, $contigobj) = @_;
+
+    my $readseq = Bio::LocatableSeq->new(
+	-display_id => $read->name,
+	-primary_id => $read->name,
+	-seq        => $read->dna,
+	-start      => 1,
+	-strand     => $read->strand,
+	-alphabet   => 'dna'
+	);
+    my $qual = Bio::Seq::PrimaryQual->new(
+	-id         => $read->name."_qual",
+	-qual       => [$read->qscore]
+	);
+
+    my $alncoord = Bio::SeqFeature::Generic->new(
+	-primary_tag => $read->name,
+	-start      => $read->start,
+	-end        => $read->end,
+	-strand     => $read->strand,
+	-qual       => join(' ',$read->qscore),
+	-tag        => { 'contig' => $contigobj->id }
+	
+	);
+
+    $contigobj->set_seq_coord($alncoord, $readseq);
+    $contigobj->set_seq_qual( $readseq, $qual );
+
+#add other misc read info as subsequence feature:
+#    my @other = grep !/aln_(?:end|start)|seq(?:str)?|strand/, keys %$readinfo;
+#    my %other;
+#    @other{@other} = @$readinfo{@other};
+#    my $readtags = Bio::SeqFeature::Generic->new(
+#        -primary_tag => '_main_read_feature:'.$readobj->id,
+#        -start       => $$readinfo{'aln_start'},
+#        -end         => $$readinfo{'aln_end'},
+#        -strand      => $$readinfo{'strand'},
+#        # dumping ground:
+#        -tag         => \%other
+#    );
+#    $alncoord->add_sub_SeqFeature($readtags);
+
+    return $readseq;
+}
+
+=head2  _store_singlet()
+
+    Title   : _store_singlet
+    Usage   : my $singletobj = $self->_store_singlet($contig_seg, $contigobj);
+    Function: store information of a singlet belonging to a scaffold 
+              in a singlet object
+    Returns : Bio::Assembly::Singlet
+    Args    : Bio::DB::Sam::Segment (containing one feature, hopefully),
+              Bio::Assembly::Contig (previously loaded)
+
+=cut
+
+sub _store_singlet {
+    my $self = shift;
+    my ($contig_seg,$contigobj) = @_;
+    my ($read) = $contig_seg->features;
+
+    my $readseq = Bio::LocatableSeq->new(
+	-display_id => $read->name,
+	-primary_id => $read->name,
+	-seq        => $read->dna,
+	-start      => 1
+	-strand     => $read->strand,
+	-alphabet   => 'dna'
+	);
+
+    my $singletobj = Bio::Assembly::Singlet->new( -id => $contigobj->id,
+						  -seqref => $readseq );
+
+    # may want to attach quality, etc. as SeqFeature sometime...
+
+    return $singletobj;
+}
+
+=head2 write_assembly()
+
+ Title   : write_assembly
+ Usage   : 
+ Function: not implemented (module currrently read-only)
+ Returns : 
+ Args    : 
+
+=cut
+
+sub write_assembly {
+    my $self = shift;
+    $self->throw( "This module is currently read-only" );
+}
+
+=head1 REALLY Internal
+
+=head2 _init_sam()
+
+ Title   : _init_sam
+ Usage   : $self->_init_sam($fasfile)
+ Function: obtain a Bio::DB::Sam parsing of the associated sam file
+ Returns : true on success
+ Args    : [optional] name of the fasta reference db (scalar string)
+ Note    : The associated file can be plain text (.sam) or binary (.bam);
+           If the fasta file is not specified, and no file is contained in 
+           the refdb() attribute, a .fas file with the same
+           basename as the sam file will be searched for.
+           
+=cut
+
+sub _init_sam {
+    my $self = shift;
+    my $fasfile = shift;
+    my $file = $self->file;
+    my $sam;
+    $fasfile ||= $self->refdb;
+    $file =~ s/^[<>+]*//; # byebye parasitic mode chars
+    my ($fname, $dir, $suf) = fileparse($file, ".sam", ".bam");
+    $self->_set_from_args({ '_basename' => $fname }, 
+			  -methods => [qw( _basename)],
+			  -create => 1);
+    if (!defined $fasfile) {
+	for (qw( fas fa fasta )) {
+	    $fasfile = File::Spec->catdir($dir, $self->_basename.$_);
+	    last if -e $fasfile;
+	    undef $fasfile;
+	}
+    }
+    unless (-e $fasfile) {
+	croak( "Can't find associated reference fasta db" );
+    }
+    !$self->refdb && $self->refdb($fasfile);
+    $sam = Bio::DB::Sam->new( -bam => $file, 
+			      -fasta => $fasfile );
+    unless (defined $sam) {
+	croak( "Couldn't create the Bio::DB::Sam object" );
+    }
+    $self->{sam} = $sam;
+    # now produce the contig segments for each seq_id...
+    for ($sam->seq_ids) {
+	my $seg = $sam->segment(-seq_id=>$_, -start=>1, -end=>$sam->length($_));
+	${$self->{_segset}}{$_} = [$self->_get_contig_segs_from_coverage($seg)];
+    }
+ 
+    return 1;
+
+}
+
+=head2 _get_contig_segs_from_coverage()
+
+ Title   : _get_contig_segs_from_coverage
+ Usage   : 
+ Function: calculates separate contigs using coverage info 
+           in the segment
+ Returns : array of Bio::DB::Sam::Segment objects, representing
+           each contig
+ Args    : Bio::DB::Sam::Segment object
+
+=cut
+
+sub _get_contig_segs_from_coverage {
+    my $self = shift;
+    my $segment = shift;
+    unless ($self->sam) {
+	croak("Sam object not yet initialized (call _init_sam)");
+    }
+    unless ( ref($segment) =~ /Bio::DB::Sam::Segment/ ) {
+	croak("Requires Bio::DB::Sam::Segment object at arg 1");
+    }
+    my ($cov, @covdata, @rngs, @segs);
+    ($cov) = $segment->features('coverage');
+    unless ($cov) {
+	croak("No coverage data!");
+    }
+    @covdata = $cov->coverage;
+    
+    # calculate contigs: 
+    my $in_contig;
+    my ($lf_end,$rt_end);
+    for (0..$#covdata) {
+	if ($covdata[$_]) {
+	    if ($in_contig) {
+		$rt_end = $_;
+		next;
+	    }
+	    else {
+		$in_contig = 1;
+		# push previous range
+		if (defined $lf_end && defined $rt_end) {
+		    push @rngs, [$lf_end, $rt_end];
+		}
+		$lf_end = $_;
+	    }
+	    
+	}
+	else {
+	    $in_contig = 0;
+	}
+    }
+    # last one
+    push @rngs, [$lf_end, $rt_end] if (defined $lf_end and 
+				       defined $rt_end and
+				       $lf_end <= $rt_end);
+    unless (@rngs) {
+	carp ("No coverage for this segment!");
+	return;
+    }
+    for (@rngs) {
+	push @segs, $self->sam->segment(-seq_id=>'NC_009749', -start=>$$_[0], -end=>$$_[1]);
+    }
+    return @segs;
+}
+
+=head2 _calc_consensus_quality()
+
+ Title   : _calc_consensus_quality
+ Usage   : @qual = $aio->_calc_consensus_quality( $contig_seg );
+ Function: calculate an average or other data-reduced quality
+           over all sites represented by the features contained
+           in a Bio::DB::Sam::Segment
+ Returns : 
+ Args    : a Bio::DB::Sam::Segment object
+
+=cut
+
+sub _calc_consensus_quality {
+    # just an average over sites for now...
+    my $self = shift;
+    my $seg = shift;
+
+    my @quals;
+    my $region = $seg->seq_id.':'.$seg->start.'..'.$seg->end;
+    my $qual_averager = sub {
+	my ($seqid, $pos, $p) = @_;
+        return unless ($seg->start <= $pos and $pos <= $seg->end);
+	my $acc = 0;
+	my $n = 0;
+	for my $pileup (@$p) {
+	    my $b = $pileup->alignment;
+	    $acc += $b->qscore->[$pileup->qpos];
+	    $n++;
+	}
+	push @quals, int($acc/$n);
+    };
+    $self->sam->pileup($region, $qual_averager);
+    return @quals;
+}
+
+=head2 _calc_consensus()
+
+ Title   : _calc_consensus
+ Usage   : @qual = $aio->_calc_consensus( $contig_seg );
+ Function: calculate a simple quality-weighted consensus sequence
+           for the segment
+ Returns : a SeqWithQuality object
+ Args    : a Bio::DB::Sam::Segment object
+
+=cut
+
+sub _calc_consensus {
+    # just an average over sites for now...
+    my $self = shift;
+    my $seg = shift;
+
+    my @quals;
+    my $conseq ='';
+    my $region = $seg->seq_id.':'.$seg->start.'-'.$seg->end;
+
+    my $weighted_consensus = sub {
+	my ($seqid, $pos, $p) = @_;
+        return unless ($seg->start <= $pos and $pos <= $seg->end);
+	my %wt_tbl;
+	my $n =0;
+	my $acc = 0;
+	for my $pileup (@$p) {
+	    my $b = $pileup->alignment;
+	    $acc += $b->qscore->[$pileup->qpos];
+	    $wt_tbl{substr($b->qseq,$pileup->qpos,1)} += 
+		$b->qscore->[$pileup->qpos];
+	    $n++;
+	}
+	# really simple
+	$conseq .= (sort { $wt_tbl{$b}<=>$wt_tbl{$a} } keys %wt_tbl)[0];
+	push @quals, int($acc/$n);
+
+    };
+
+    $self->sam->pileup($region, $weighted_consensus);
+    return Bio::Seq::Quality->new(
+	-qual => join(' ', @quals ),
+	-seq => $conseq,
+	-id => $region
+	);
+}
+
+=head2 refdb()
+
+ Title   : refdb
+ Usage   : $obj->refdb($newval)
+ Function: the name of the reference db fasta file
+ Example : 
+ Returns : value of refdb (a scalar)
+ Args    : on set, new value (a scalar or undef, optional)
+
+=cut
+
+sub refdb {
+    my $self = shift;
+    
+    return $self->{'refdb'} = shift if @_;
+    return $self->{'refdb'};
+}
+
+=head2 _segset()
+
+ Title   : _segset
+ Usage   : $segset_hashref = $self->_segset()
+ Function: hash container for the Bio::DB::Sam::Segment objects that
+           represent each set of contigs for each seq_id
+           { $seq_id => [@contig_segments], ... }
+ Example : 
+ Returns : value of _segset (a hashref) if no arg, 
+           or the arrayref of contig segments, if arg == a seq id
+ Args    : [optional] seq id (scalar string)
+ Note    : readonly; hash elt set in _init_sam()
+
+=cut
+
+sub _segset {
+    my $self = shift;
+    return $self->{'_segset'} unless @_;
+    return ${$self->{'_segset'}}{shift()};
+}
+
+=head2 _current_refseq_id()
+
+ Title   : _current_refseq_id
+ Usage   : $obj->_current_refseq_id($newval)
+ Function: the "current" reference sequence id
+ Example : 
+ Returns : value of _current_refseq (a scalar)
+ Args    : on set, new value (a scalar or undef, optional)
+
+=cut
+
+sub _current_refseq_id {
+    my $self = shift;
+    
+    return $self->{'_current_refseq_id'} = shift if @_;
+    return $self->{'_current_refseq_id'};
+}
+
+=head2 _current_contig_seg_idx()
+
+ Title   : current_contig_seg_idx
+ Usage   : $obj->current_contig_seg_idx($newval)
+ Function: the "current" segment index in the "current" refseq
+ Example : 
+ Returns : value of current_contig_seg_idx (a scalar)
+ Args    : on set, new value (a scalar or undef, optional)
+
+=cut
+
+sub _current_contig_seg_idx {
+    my $self = shift;
+    
+    return $self->{'_current_contig_seg_idx'} = shift if @_;
+    return $self->{'_current_contig_seg_idx'};
+}
+
+=head2 sam()
+
+ Title   : sam
+ Usage   : $obj->sam($newval)
+ Function: store the associated Bio::DB::Sam object
+ Example : 
+ Returns : value of sam (a Bio::DB::Sam object)
+ Args    : on set, new value (a scalar or undef, optional)
+
+=cut
+
+sub sam {
+    my $self = shift;
+    return $self->{'sam'};
+}
+
+
+1;
