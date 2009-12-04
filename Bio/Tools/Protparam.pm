d0e5788baeb3a9a75d766a90e7f51f1846a69b49
maj <maj@bioperl.org>
2009-12-04 23:46:10 +0000

    fixed improper use of throw() / thanks Gang Wu for heads-up
    svn path=/bioperl-live/trunk/; revision=16442

--- Bio/Tools/Protparam.pm
+++ Bio/Tools/Protparam.pm
@@ -126,8 +126,8 @@ sub new {
                           );
 
 	#Check if successful
-	$self->throw("$self->{'url'} error: ", $response->status_line) unless $response->is_success;
-	$self->throw("Bad content type at $self->{'url'} ", $response->content_type) unless $response->content_type eq 'text/html';
+	$self->throw("$url error: ".$response->status_line) unless $response->is_success;
+	$self->throw("Bad content type at $url ".$response->content_type) unless $response->content_type eq 'text/html';
 
 	my $protParamOutput=$response->decoded_content;
 	
