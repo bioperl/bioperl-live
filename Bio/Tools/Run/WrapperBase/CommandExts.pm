6574c13e47a0a04c082b29fc36714d033ece9f6e
maj <maj@bioperl.org>
2010-01-14 15:38:37 +0000

    revert to WrapperBase namespace (bug #2991)
    svn path=/bioperl-live/trunk/; revision=16691

--- Bio/Tools/Run/WrapperBase/CommandExts.pm
+++ Bio/Tools/Run/WrapperBase/CommandExts.pm
@@ -408,7 +408,7 @@ Internal methods are usually preceded with a _
 
 # Let the code begin...
 
-package Bio::Tools::Run::WrapperBase::CommandExts;
+package Bio::Tools::Run::WrapperBase; # need these methods in WrapperBase/maj
 use strict;
 use warnings;
 no warnings qw(redefine);
