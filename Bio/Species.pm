1687dcef058351a6f8020a76690c9018132852f4
Chris Fields <cjfields@bioperl.org>
2010-06-28 13:23:47 -0500

    Bio::Species delegates to Bio::Taxon and Bio::Tree::Tree; this removes the need for weaken() as Bio::Species just acts as a proxy class.  Needs further testing to ensure no mem leaks persist

--- Bio/Species.pm
+++ Bio/Species.pm
@@ -98,11 +98,12 @@ methods. Internal methods are usually preceded with a _
 
 package Bio::Species;
 use strict;
+use warnings;
 
 use Bio::DB::Taxonomy;
 use Bio::Tree::Tree;
-use Scalar::Util qw(weaken isweak);
-use base qw(Bio::Taxon);
+use Bio::Taxon;
+use base qw(Bio::Root::Root Bio::Tree::NodeI);
 
 =head2 new
 
@@ -120,22 +121,26 @@ sub new {
     
     my $self = $class->SUPER::new(@args);
     
+    # Bio::Species is a proxy object that just observes the NodeI interface
+    # methods but delegates them to the proper classes (Bio::Taxon and
+    # Bio::Tree::Tree). This will be surplanted by the much simpler
+    # Bio::Taxon/Bio::DB::Taxonomy modules in the future.
+    
+    # Using a proxy allows proper GC w/o using weaken()
+    
+    $self->taxon(Bio::Taxon->new(@args));
+    
     my ($org, $sp, $var, $classification) =
         $self->_rearrange([qw(ORGANELLE
-					       SUB_SPECIES
-					       VARIANT
-					       CLASSIFICATION)], @args);
+                            SUB_SPECIES
+                            VARIANT
+                            CLASSIFICATION)], @args);
     
     if (defined $classification && ref($classification) eq "ARRAY" && @{$classification}) {
         $self->classification(@$classification);
     }
     else {
-        # store a tree on ourselves so we can use Tree methods
-        $self->{tree} = Bio::Tree::Tree->new();
-        
-        # some things want to freeze/thaw Bio::Species objects, but
-        # _root_cleanup_methods contains a CODE ref, delete it.
-        # delete $self->{tree}->{_root_cleanup_methods};
+        $self->tree(Bio::Tree::Tree->new());
     }
     
     defined $org && $self->organelle($org);
@@ -168,21 +173,22 @@ sub new {
 sub classification {
     my ($self, @vals) = @_;
 
+    my $taxon = $self->taxon;
+
     if (@vals) {
         if (ref($vals[0]) eq 'ARRAY') {
             @vals = @{$vals[0]};
         }
-        
         # make sure the lineage contains us as first or second element
         # (lineage may have subspecies, species, genus ...)
-        my $name = $self->node_name;
+        my $name = $taxon->node_name;
         my ($genus, $species) = (quotemeta($vals[1]), quotemeta($vals[0]));
         if ($name && ($name !~ m{$species}i && $name !~ m{$genus}i) && $name !~ m{$vals[1] $vals[0]}i) {
             if ($name =~ /^$vals[1] $vals[0]\s*(.+)/) {
                 # just assume the problem is someone tried to make a Bio::Species starting at subspecies
                 #*** no idea if this is appropriate! just a possible fix related to bug 2092
                 $self->sub_species($1);
-                $name = $self->node_name("$vals[1] $vals[0]");
+                $name = $taxon->node_name("$vals[1] $vals[0]");
             }
             else {
                 $self->warn("The supplied lineage does not start near '$name' (I was supplied '".join(" | ", @vals)."')");
@@ -191,31 +197,30 @@ sub classification {
         
         # create a lineage for ourselves
         my $db = Bio::DB::Taxonomy->new(-source => 'list', -names => [reverse @vals]);
-        unless ($self->scientific_name) {
+        unless ($taxon->scientific_name) {
             # assume we're supposed to be the leaf of the supplied lineage
-            $self->scientific_name($vals[0]);
+            $self->taxon->scientific_name($vals[0]);
         }
-        unless ($self->rank) {
+        unless ($taxon->rank) {
             # and that we are rank species
-            $self->rank('species');
+            $taxon->rank('species');
         }
         
-        $self->db_handle($db);
-
-        $self->{tree} = Bio::Tree::Tree->new(-node => $self);
+        $taxon->db_handle($db);
+        
+        $self->tree(Bio::Tree::Tree->new(-node => $taxon));
         # some things want to freeze/thaw Bio::Species objects, but tree's
         # _root_cleanup_methods contains a CODE ref, delete it.
         #*** even if we don't delete the cleanup methods, we still get memory
         #    leak-like symtoms, and the actual cleanup causes a mass of
         #    warnings... needs investigation!
-        delete $self->{tree}->{_root_cleanup_methods};
+        #delete $self->{tree}->{_root_cleanup_methods};
     }
     
     @vals = ();
-    foreach my $node ($self->{tree}->get_lineage_nodes($self), $self) {
+    foreach my $node ($self->tree->get_lineage_nodes($taxon), $taxon) {
         unshift(@vals, $node->scientific_name || next);
     }
-    weaken($self->{tree}->{'_rootnode'}) unless isweak($self->{tree}->{'_rootnode'});
     return @vals;
 }
 
@@ -268,96 +273,102 @@ sub classification {
 sub species {
     my ($self, $species) = @_;
     
-	if ($species) {
-		$self->{_species} = $species;
-	}
-	
-	unless (defined $self->{_species}) {
-		# work it out from our nodes
-		my $species_taxon = $self->{tree}->find_node(-rank => 'species');
-		unless ($species_taxon) {
-			# just assume we are rank species
-			$species_taxon = $self;
-		}
-		
-		$species = $species_taxon->scientific_name;
+    if ($species) {
+            $self->{_species} = $species;
+    }
+
+    if (!defined $self->{_species}) {
+        # work it out from our nodes
+        #my $species_taxon = $self->tree->find_node(-rank => 'species');
+        #unless ($species_taxon) {
+        #        # just assume we are rank species
+        #        $species_taxon = $self->taxon;
+        #}
+        #my $species_taxon = $self->taxon;
+
+        # this should always be the species node (we are in Bio::Species, not
+        # Bio::Genus).  If we are getting something else here, we are probably
+        # missing a key error handling step early on.
         
-		#
-		# munge it like the Bio::SeqIO modules used to do
-		# (more or less copy/pasted from old Bio::SeqIO::genbank, hence comments
-		#  referring to 'ORGANISM' etc.)
-		#
-		
-		my $root = $self->{tree}->get_root_node;
-		unless ($root) {
-            $self->{tree} = Bio::Tree::Tree->new(-node => $species_taxon);
-            delete $self->{tree}->{_root_cleanup_methods};
-            $root = $self->{tree}->get_root_node;
-            weaken($self->{tree}->{'_rootnode'}) unless isweak($self->{tree}->{'_rootnode'});
+        $species = $self->taxon->scientific_name;
+        
+        #
+        # munge it like the Bio::SeqIO modules used to do
+        # (more or less copy/pasted from old Bio::SeqIO::genbank, hence comments
+        #  referring to 'ORGANISM' etc.)
+        #
+    
+        my $root = $self->tree->get_root_node;
+        unless ($root) {
+            $self->throw("No root node defined for taxon species node:$species");
+            #$self->{tree} = Bio::Tree::Tree->new(-node => $species_taxon);
+            #delete $self->{tree}->{_root_cleanup_methods};
+            #$root = $self->{tree}->get_root_node;
+            #weaken($self->{tree}->{'_rootnode'}) unless isweak($self->{tree}->{'_rootnode'});
         }
         
-		my @spflds = split(' ', $species);
-		if (@spflds > 1 && $root->node_name ne 'Viruses') {
-			$species = undef;
-			
-			# does the next term start with uppercase?
-			# yes: valid genus; no then unconventional
-			# e.g. leaf litter basidiomycete sp. Collb2-39
-			my $genus;
-			if ($spflds[0] =~ m/^[A-Z]/) {
-				$genus = shift(@spflds);
-			}
-			else {
-				undef $genus;
-			}
+        my @spflds = split(' ', $species);
+        if (@spflds > 1 && $root->node_name ne 'Viruses') {
+            $species = undef;
             
-			my $sub_species;
-			if (@spflds) {
-				while (my $fld = shift @spflds) {
-					$species .= "$fld ";
-					# does it have subspecies or varieties?
-					last if ($fld =~ m/(sp\.|var\.)/);
-				}
-				chop $species;	# last space
-				$sub_species = join ' ',@spflds if(@spflds);
-			}
-			else {
-				$species = 'sp.';
-			}
-			
-			# does ORGANISM start with any words which make its genus undefined?
-			# these are in @unkn_genus	
-			# this in case species starts with uppercase so isn't caught above. 
-			# alter common name if required
-			my $unconv = 0; # is it unconventional species name?
-			my @unkn_genus = ('unknown','unclassified','uncultured','unidentified');
-			foreach (@unkn_genus) {
-				if ($genus && $genus =~ m/$_/i)	{
-					$species = $genus . " " . $species;
-					undef $genus;
-					$unconv = 1;
-					last;
-				}
-				elsif ($species =~ m/$_/i)	{
-					$unconv = 1;
-					last;
-				}
-			}
-			if (!$unconv && !$sub_species && $species =~ s/^(\w+)\s(\w+)$/$1/)	{
-				# need to extract subspecies from conventional ORGANISM format.  
-				# Will the 'word' in a two element species name
-				# e.g. $species = 'thummi thummi' => $species='thummi' & 
-				# $sub_species='thummi'
-				$sub_species = $2;
-			}
-			
-			$self->genus($genus) if $genus;
-			$self->sub_species($sub_species) if $sub_species;
-		}
-		
-		$self->{_species} = $species;
+            # does the next term start with uppercase?
+            # yes: valid genus; no then unconventional
+            # e.g. leaf litter basidiomycete sp. Collb2-39
+            my $genus;
+            if ($spflds[0] =~ m/^[A-Z]/) {
+                $genus = shift(@spflds);
+            }
+            else {
+                undef $genus;
+            }
+    
+            my $sub_species;
+            if (@spflds) {
+                while (my $fld = shift @spflds) {
+                    $species .= "$fld ";
+                    # does it have subspecies or varieties?
+                    last if ($fld =~ m/(sp\.|var\.)/);
+                }
+                chop $species;  # last space
+                $sub_species = join ' ',@spflds if(@spflds);
+            }
+            else {
+                $species = 'sp.';
+            }
+    
+            # does ORGANISM start with any words which make its genus undefined?
+            # these are in @unkn_genus      
+            # this in case species starts with uppercase so isn't caught above. 
+            # alter common name if required
+            my $unconv = 0; # is it unconventional species name?
+            my @unkn_genus = ('unknown','unclassified','uncultured','unidentified');
+            foreach (@unkn_genus) {
+                if ($genus && $genus =~ m/$_/i) {
+                    $species = $genus . " " . $species;
+                    undef $genus;
+                    $unconv = 1;
+                    last;
+                }
+                elsif ($species =~ m/$_/i)      {
+                    $unconv = 1;
+                    last;
+                }
+            }
+            if (!$unconv && !$sub_species && $species =~ s/^(\w+)\s(\w+)$/$1/)      {
+                # need to extract subspecies from conventional ORGANISM format.  
+                # Will the 'word' in a two element species name
+                # e.g. $species = 'thummi thummi' => $species='thummi' & 
+                # $sub_species='thummi'
+                $sub_species = $2;
+            }
+        
+            $self->genus($genus) if $genus;
+            $self->sub_species($sub_species) if $sub_species;
+        }
+    
+        $self->{_species} = $species;
     }
-	return $self->{_species};
+    return $self->{_species};
 }
 
 =head2 genus
@@ -374,22 +385,20 @@ sub species {
 
 sub genus {
     my ($self, $genus) = @_;
-    
-	if ($genus) {
+        if ($genus) {
         $self->{_genus} = $genus;
     }
-	
-	unless (defined $self->{_genus}) {
-		my $genus_taxon = $self->{tree}->find_node(-rank => 'genus');
-		unless ($genus_taxon) {
-			# just assume our ancestor is rank genus
-			$genus_taxon = $self->ancestor;
-		}
-		
-		$self->{_genus} = $genus_taxon->scientific_name if $genus_taxon;
-	}
-	
-	return $self->{_genus};
+    unless (defined $self->{_genus}) {
+        my $genus_taxon = $self->tree->find_node(-rank => 'genus');
+        unless ($genus_taxon) {
+            # just assume our ancestor is rank genus
+            $genus_taxon = $self->taxon->ancestor;
+        }
+        
+        $self->{_genus} = $genus_taxon->scientific_name if $genus_taxon;
+    }
+    
+    return $self->{_genus};
 }
 
 =head2 sub_species
@@ -405,8 +414,8 @@ sub genus {
 sub sub_species {
     my ($self, $sub) = @_;
     
-    unless (defined $self->{'_sub_species'}) {
-        my $ss_taxon = $self->{tree}->find_node(-rank => 'subspecies');
+    if (!defined $self->{'_sub_species'}) {
+        my $ss_taxon = $self->tree->find_node(-rank => 'subspecies');
         if ($ss_taxon) {
             if ($sub) {
                 $ss_taxon->scientific_name($sub);
@@ -421,6 +430,9 @@ sub sub_species {
                 $self->{'_sub_species'} = $sub;
             }
             return $ss_taxon->scientific_name;
+        }
+        else {
+            # should we create a node here to be added to the tree?
         }
     }
     
@@ -444,13 +456,16 @@ sub sub_species {
 sub variant{
     my ($self, $var) = @_;
     
-    unless (defined $self->{'_variant'}) {
-        my $var_taxon = $self->{tree}->find_node(-rank => 'variant');
+    if (!defined $self->{'_variant'}) {
+        my $var_taxon = $self->tree->find_node(-rank => 'variant');
         if ($var_taxon) {
             if ($var) {
                 $var_taxon->scientific_name($var);
             }
             return $var_taxon->scientific_name;
+        }
+        else {
+            # should we create a node here to be added to the tree?
         }
     }
     
@@ -468,12 +483,13 @@ sub variant{
            if the first argument is 'FULL' (and the species has a subspecies).
  Args    : Optionally the string 'FULL' to get the full name including
            the subspecies.
+ Note    : This is just munged from the taxon() name
 
 =cut
 
 sub binomial {
     my ($self, $full) = @_;
-    my $rank = $self->rank || 'no rank';
+    my $rank = $self->taxon->rank || 'no rank';
     
     my ($species, $genus) = ($self->species, $self->genus);
     unless (defined $species) {
@@ -502,21 +518,20 @@ sub binomial {
  Usage   : $result = $self->validate_species_name($string);
  Function: Validate the species portion of the binomial
  Args    : string
- Notes   : The string following the "genus name" in the NCBI binomial
-           is so variable that it's not clear that this is a useful
-           function. Consider the binomials 
-           "Simian 11 rotavirus (serotype 3 / strain SA11-Patton)",
-           or "St. Thomas 3 rotavirus", straight from GenBank.
-           This is particularly problematic in microbes and viruses.
-           As such, this isn't actually used automatically by any Bio::Species
-           method.
+ Notes   : The string following the "genus name" in the NCBI binomial is so
+           variable that it's not clear that this is a useful function. Consider
+           the binomials "Simian 11 rotavirus (serotype 3 / strain
+           SA11-Patton)", or "St. Thomas 3 rotavirus", straight from GenBank.
+           This is particularly problematic in microbes and viruses. As such,
+           this isn't actually used automatically by any Bio::Species method.
+
 =cut
 
 sub validate_species_name {
     my( $self, $string ) = @_;
 
     return 1 if $string eq "sp.";
-	return 1 if $string =~ /strain/;
+    return 1 if $string =~ /strain/;
     return 1 if $string =~ /^[a-z][\w\s-]+$/i;
     $self->throw("Invalid species name '$string'");
 }
@@ -534,6 +549,9 @@ sub validate_name {
  Example : $self->organelle('Chloroplast')
  Returns : The organelle name in a string
  Args    : String, which is the organelle name
+ Note    : TODO: We currently do not know where the organelle definition will
+           eventually go.  This is stored in the source seqfeature, though,
+           so the information isn't lost.
 
 =cut
 
@@ -543,11 +561,73 @@ sub organelle {
     return $self->{'_organelle'};
 }
 
-sub dont_DESTROY {
+=head2 Delegation
+
+The following methods delegate to the internal Bio::Taxon instance. This is
+mainly to allow older code to migrate to using Bio::Taxon and related methods in
+the future when this classs is deprecated.
+
+=cut 
+
+sub node_name {shift->taxon->node_name(@_)}
+sub scientific_name {shift->taxon->node_name(@_)}
+
+sub id {shift->taxon->id(@_)}
+sub object_id {shift->taxon->id(@_)}
+sub ncbi_taxid {shift->taxon->ncbi_taxid(@_)}
+sub rank {shift->taxon->rank(@_)}
+sub division {shift->taxon->division(@_)}
+
+sub common_names {shift->taxon->common_names(@_)}
+sub common_name {shift->taxon->common_names(@_)}
+
+sub genetic_code {shift->taxon->genetic_code(@_)}
+sub mitochondrial_genetic_code {shift->taxon->mitochondrial_genetic_code(@_)}
+
+sub create_date { shift->taxon->create_date(@_)}
+sub pub_date { shift->taxon->pub_date(@_)}
+sub update_date { shift->taxon->update_date(@_)}
+
+sub db_handle { shift->taxon->db_handle(@_)}
+
+sub parent_id { shift->taxon->parent_id(@_)}
+sub parent_taxon_id { shift->taxon->parent_id(@_)}
+
+sub version { shift->taxon->version(@_)}
+sub authority { shift->taxon->authority(@_)}
+sub namespace { shift->taxon->namespace(@_)}
+
+sub ancestor { shift->taxon->ancestor(@_)}
+sub get_Parent_Node { shift->taxon->get_Parent_Node(@_)}
+sub each_Descendent { shift->taxon->each_Descendent(@_)}
+sub get_Children_Nodes { shift->taxon->get_Children_Nodes(@_)}
+sub remove_Descendant { shift->taxon->remove_Descendant(@_)}
+
+sub name { shift->taxon->name(@_)}
+
+sub taxon {
+    my ($self, $taxon) = @_;
+    if (!$self->{taxon} || $taxon) {
+        $taxon ||= Bio::Taxon->new();
+        $self->{taxon} = $taxon;
+    }
+    $self->{taxon};
+}
+
+sub tree {
+    my ($self, $tree) = @_;
+    if (!$self->{tree} || $tree) {
+        $tree ||= Bio::Tree::Tree->new();
+        $self->{tree} = $tree;
+    }
+    $self->{tree};
+}
+
+sub DESTROY {
     my $self = shift;
     $self->{tree}->cleanup_tree if $self->{tree};
     delete $self->{tree};
-    $self->node_cleanup;
+    $self->taxon->node_cleanup;
 }
 
 1;
