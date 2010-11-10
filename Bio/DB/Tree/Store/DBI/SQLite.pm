22c7d0fb0b9b33135a520df9d234fc5a3464dac0
hyphaltip <jason@bioperl.org>
2010-11-10 12:28:18 -0500

    more get and fetch retrieval

--- Bio/DB/Tree/Store/DBI/SQLite.pm
+++ Bio/DB/Tree/Store/DBI/SQLite.pm
@@ -7,33 +7,80 @@ use Cwd 'abs_path';
 use File::Spec;
 use base qw(Bio::DB::Tree::Store);
 
+
+###
+# object initialization
+#
+sub init {
+  my $self          = shift;
+  my ($dsn,
+      $is_temporary,
+      $autoindex,
+      $dump_dir,
+      $user,
+      $pass,
+      $dbi_options,
+      $writeable,
+      $create,
+     ) = $self->_rearrange([qw(DSN TEMPORARY AUTOINDEX DUMPDIR
+			       USER PASS OPTIONS WRITE
+			       CREATE)
+			   ],@_);
+  $dbi_options  ||= {};
+  $writeable    = 1 if $is_temporary or $dump_dir;
+  $dsn or $self->throw("Usage: ".__PACKAGE__."->init(-dsn => \$dbh || \$dsn)");
+
+  my $dbh;
+  if (ref $dsn) {
+    $dbh = $dsn;
+  } else {
+    $dsn = "dbi:SQLite:$dsn" unless $dsn =~ /^dbi:/;
+    $dbh = DBI->connect($dsn,$user,$pass,$dbi_options) or $self->throw($DBI::errstr);
+    $dbh->do("PRAGMA synchronous = OFF;"); # makes writes much faster
+    $dbh->do("PRAGMA temp_store = MEMORY;"); # less disk I/O; some speedup
+    $dbh->do("PRAGMA cache_size = 20000;"); # less disk I/O; some speedup
+  }
+  $self->{dbh}       = $dbh;
+  $self->{is_temp}   = $is_temporary;
+  $self->{writeable} = $writeable;
+
+#  $self->default_settings;
+#  $self->autoindex($autoindex)                   if defined $autoindex;
+#  $self->dumpdir($dump_dir)                      if $dump_dir;
+  if ($self->is_temp) {
+    $self->init_tmp_database();
+  } elsif ($create) {
+    $self->init_database('erase');
+  }
+}
+
 sub table_definitions {
   my $self = shift;
-  my $defs = 
-      {
-	  tree => <<END,
+  my $defs =
+    {
+     tree => <<END,
 (
-       tree_id integer primary autoincrement,
+       tree_id integer primary key autoincrement,
        label text not null,
        is_rooted integer default 1,
        root_id integer not null,
        annotations text
-) ;
-
+);
 create unique index ui_tree_id on tree(tree_id);
-create index i_tree_node_id ON tree(node_id);
+create index i_root_node_id ON tree(root_id);
 create index i_treelabel on tree(label);
+create index i_rooted on tree(is_rooted);
 END
 
-       node => <<END,
+     node => <<END,
 (
-       node_id integer primary autoincrement,
+       node_id integer primary key autoincrement,
        parent_id integer,
        label text,
        distance_to_parent real,
        annotations text,
        left_idx integer,
-       right_idx integer,
+       right_idx integer
 );
 create index ui_nodeid ON node (node_id);
 create index i_nodelabel ON node (label);
@@ -41,28 +88,43 @@ create index i_leftidx ON node (left_idx);
 create index i_rightidx ON node (right_idx);
 
 END
-};
+    };
   return $defs;
 }
+
+=head2 Node methods
+
+=cut
 
 sub insert_node {
   my $self = shift;
   my $object = shift;
-  my $index_flag = shift || 0;
   my $annotations = join(";",map { sprintf("%s=%s",$_,
 					   join(",",$object->get_tag_values($_))) } 
 			 $object->get_all_tags);
   my $sth = $self->_prepare(<<END);
-INSERT INTO node (node_id,parent_id,label,distance_to_parent,annotations) VALUES (?,?,?)
+INSERT INTO node (node_id,parent_id,label,distance_to_parent,annotations) VALUES (?,?,?,?,?)
 END
   $sth->execute(undef,$object->parent_id,$object->id, $annotations);
   $sth->finish;
+  $object->node_id($self->dbh->func('last_insert_rowid'));
 }
 
+sub _create_node {
+  my $self = shift;
+  my ($parent,$label,$branchlen,$annotations) = @_;
+  my $sth = $self->_prepare(<<END);
+INSERT INTO node (node_id,parent_id,label,distance_to_parent,annotations) VALUES (?,?,?,?,?)
+END
+  $sth->execute(undef,$parent,$label, $branchlen,$annotations);
+  $sth->finish;
+  return Bio::DB::Tree::Node->new(-node_id => $self->dbh->func('last_insert_rowid'),
+				  -store   => $self);
+
+}
 sub insert_tree {
   my $self = shift;
   my $object = shift;
-  my $index_flag = shift || 0;
   my $annotations = join(";",map { sprintf("%s=%s",$_,
 					   join(",",$object->get_tag_values($_))) } 
 			 $object->get_all_tags);
@@ -91,17 +153,24 @@ sub _fetch_node {
   my $self = shift;
   my $id   = shift;
 
-  my $sth = $self->_prepare(<<END);
-SELECT (node_id, parent_id, label, distance_to_parent,annotations) FROM node where node_id = ?
-END
-  $sth->execute($id);
-  my ($nid,$pid,$lbl,$distance,$annot) = @{$sth->fetchrow_arrayref};
+#  my $sth = $self->_prepare(<<END);
+#SELECT (node_id, parent_id, label, distance_to_parent,annotations) FROM node where node_id = ?
+#END
 
-  return Bio::DB::Tree::Node->new(-internal_id => $id,
-				  -parent_id   => $pid,
-				  -id          => $lbl,
-				  -branch_lengths => $distance,
-				  -annotations => $annot);
+  my $sth = $self->_prepare(<<END);
+SELECT node_id FROM node where node_id = ?
+END
+
+  $sth->execute($id);
+#  my ($nid,$pid,$lbl,$distance,$annot) = @{$sth->fetchrow_arrayref};
+  my ($nid) = @{$sth->fetchrow_arrayref};
+  return Bio::DB::Tree::Node->new(-node_id => $nid,
+				  -store   => $self);
+#				  -parent_id   => $pid,#
+#				  -id          => $lbl#,
+#				  -branch_lengths => $distance,
+#				  -annotations => $annot,
+#				  -store        => $self);
 }
 
 sub _fetch_node_children {
@@ -137,19 +206,19 @@ END
   return @nodes;
 }
 
-
-sub _get_branch_length {
+sub _fetch_node_branch_length {
   my $self = shift;
   my $id   = shift;
   my $sth = $self->_prepare(<<END);
 SELECT distance_to_parent FROM node WHERE node_id = ?
 END
-  my $d = @{$sth->fetchrow_array};
+  $sth->execute($id);
+  my ($d) = @{$sth->fetchrow_arrayref || []};
   $sth->finish;
   return $d;
 }
 
-sub _get_label {
+sub _fetch_node_label {
   my $self = shift;
   my $id   = shift;
   my $sth = $self->_prepare(<<END);
@@ -160,6 +229,76 @@ END
   return $l;
 }
 
+sub _fetch_node_parent_id {
+  my $self = shift;
+  my $id   = shift;
+  # being lazy - just get the parent_id and then fetch_node from that ID
+  my $sth = $self->_prepare(<<END);
+SELECT parent_id FROM node WHERE node_id = ?
+END
+  $sth->execute($id);
+  my ($pid) = @{$sth->fetchrow_arrayref};
+  $sth->finish;
+  $pid;
+}
+sub _fetch_node_parent {
+  my $self = shift;
+  $self->_fetch_node($self->_fetch_node_parent_id(shift));
+}
+
+
+sub _unset_node_parent {
+  my $self = shift;
+  my $id   = shift;
+  my $sth = $self->_prepare(<<END);
+UPDATE node set parent_id = NULL where node_id = ?
+END
+  $sth->execute($id);
+  $sth->finish;
+}
+
+sub _delete_node {
+  my $self = shift;
+  my $id   = shift;
+
+  my $sth = $self->_prepare(<<END);
+DELETE FROM node self, node n WHERE n.left_idx >= s.left_idx AND n.right_idx <= s.right_idx AND n.node_id = ?
+END
+  $sth->execute($id);
+  $sth->finish;
+#  $self->dbh->commit;
+}
+
+
+sub _is_Leaf {
+  my $self = shift;
+  my $id = shift;
+  my $sth = $self->_prepare(<<END);
+SELECT COUNT(*) FROM node WHERE node_id = ? AND left_idx == right_idx
+END
+  my ($isleaf) = @{$sth->fetchrow_arrayref};
+  return $isleaf;
+}
+
+=head2 Tree methods
+
+=cut
+
+sub _fetch_tree_root_node {
+  my $self = shift;
+  my $treeid = shift;
+
+  my $sth = $self->_prepare(<<END);
+SELECT root_id FROM tree WHERE tree_id = ?
+END
+  $sth->execute($treeid);
+  my ($root_node) = @{$self->fetchrow_arrayref};
+  $self->_fetch_node($root_node);
+}
+
+=head2 DB management methods
+
+=cut
 
 sub max_id {
   my $self = shift;
@@ -179,33 +318,30 @@ sub max_id {
   $id;
 }
 
-sub _is_Leaf {
-  my $self = shift;
-  my $id = shift;
-  my $sth = $self->_prepare(<<END);
-SELECT COUNT(*) FROM node WHERE node_id = ? AND left_idx == right_idx
-END
-  my ($isleaf) = @{$sth->fetchrow_arrayref};
-  return $isleaf;
-}
-
 sub _init_database {
   my $self  = shift;
   my $erase = shift;
 
   my $dbh    = $self->dbh;
   my $tables = $self->table_definitions;
-
   for my $t (keys %$tables) {
-    next if $t eq 'meta';      # don't get rid of meta data!
-    my $table = $self->_qualify($t);
-    $dbh->do("DROP table IF EXISTS $table") if $erase;
-    my $query = "CREATE TABLE IF NOT EXISTS $table $tables->{$t}";
+    $dbh->do("DROP table IF EXISTS $t") if $erase;
+    my $query = "CREATE TABLE IF NOT EXISTS $t $tables->{$t}";
     $self->_create_table($dbh,$query);
   }
   1;
 }
 
+
+sub _create_table {
+    my $self         = shift;
+    my ($dbh,$query) = @_;
+    for my $q (split ';',$query) {
+	chomp($q);
+	next unless $q =~ /\S/;
+	$dbh->do("$q;\n") or $self->throw("$q -- ".$dbh->errstr);
+    }
+}
 
 sub init_tmp_database {
   my $self = shift;
@@ -220,6 +356,15 @@ sub init_tmp_database {
     $self->_create_table($dbh,$query);
   }
   1;
+}
+
+sub _prepare {
+  my $self = shift;
+  $self->{dbh}->prepare(@_);
+}
+
+sub dbh {
+  shift->{dbh}
 }
 
 sub optimize {
@@ -277,3 +422,5 @@ more details.
 
 
 =cut
+
+1;
