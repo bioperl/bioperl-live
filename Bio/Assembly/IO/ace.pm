66b071d89bd0b6a315018da5b8fdb7497b28fddb
Florent Angly <florent.angly@gmail.com>
2011-09-05 10:50:50 +1000

    Paving the way... 3

--- Bio/Assembly/IO/ace.pm
+++ Bio/Assembly/IO/ace.pm
@@ -828,7 +828,13 @@ sub variant {
     my ($self, $variant) = @_;
     if (defined $variant) {
         $variant = lc $variant;
-        if (not exists $variant{$variant}) {
+        my $var_name = '%'.ref($self).'::variant';
+        my %ok_variants = eval $var_name; # e.g. %Bio::Assembly::IO::ace::variant
+        if (scalar keys %ok_variants == 0) {
+            $self->throw('Cannot check for validity of variant because global '.
+                "variant $var_name is not set or is empty\n");
+        }
+        if (not exists $ok_variants{$variant}) {
             $self->throw($variant.' is not a valid variant of the '.$self->format.
                 ' format');
         }
