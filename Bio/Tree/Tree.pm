bioperl/bioperl-live

commit 3d1882a78c1c039461173b5b224d67d41e307f31

handle breadth-first search properly now, can do it with a loop no need to make recursive function

Bio/Tree/Tree.pm
@@ -191,29 +191,37 @@ Function: Return list of Bio::Tree::NodeI objects
 Returns : array of Bio::Tree::NodeI objects
 Args    : (named values) hash with one value 
            order => 'b|breadth' first order or 'd|depth' first order
+	   sortby => $sortby [optional] "height", "creation", "alpha", "revalpha",
+           or coderef to be used to sort the order of children nodes. See L<Bio::Tree::Node> for details
 
 =cut
 
 sub get_nodes{
    my ($self, @args) = @_;
-   
+  warn("args are @args\n"); 
    my ($order, $sortby) = $self->_rearrange([qw(ORDER SORTBY)],@args);
    $order ||= 'depth';
    $sortby ||= 'none';
-   my $node = $self->get_root_node || return;
-   if ($order =~ m/^b|(breadth)$/oi) {
-       my @children = ($node);
-       for (@children) {
-        push @children, $_->each_Descendent($sortby);
+   my @children;
+   my $node = $self->get_root_node || return undef;
+   
+   if ($order =~ m/^b$/oi) {
+       @children = ($node);
+       my @to_process = ($node);
+       while( @to_process ) { 
+	my $n = shift @to_process;
+        my @c  = $n->each_Descendent($sortby);
+	push @children, @c;
+	push @to_process, @c;
        }
-       return @children;
-   }
-
-   if ($order =~ m/^d|(depth)$/oi) {
+   } elsif ($order =~ m/^d$/oi) {
        # this is depth-first search I believe
-       my @children = ($node,$node->get_all_Descendents($sortby));
-       return @children;
+       @children = ($node,$node->get_all_Descendents($sortby));
+   } else {
+	$self->verbose(1);
+	$self->warn("specified an order '$order' which I don't understan\n");
    }
+   @children;
 }
 
 =head2 get_root_node

commit 8463d4679d0f2a82be86e7694556c8550e3c3887

Fixed bug introduced by commit 3d1882a78c1c039461173b5b224d67d41e307f31: instead of returning [undef] when it finds no nodes, get_node now returns [] as it should. + cosmetic changes

Bio/Tree/Tree.pm
@@ -11,9 +11,10 @@
 
 # POD documentation - main docs before the code
 
+
 =head1 NAME
 
-Bio::Tree::Tree - An Implementation of TreeI interface.
+Bio::Tree::Tree - An implementation of the TreeI interface.
 
 =head1 SYNOPSIS
 
@@ -46,7 +47,7 @@ Example of issue:
   # tree is not assigned to a variable, so passes from memory after
   # root node is passed
   my $root = Bio::TreeIO->new(-format => 'newick', -file => 'foo.txt')->next_tree
-		 ->get_root_node;
+                 ->get_root_node;
 
   # gets nothing, as all Node links are broken when Tree is garbage-collected above
   my @descendents = $root->get_all_Descendents;
@@ -130,38 +131,38 @@ Args    : -root     => L<Bio::Tree::NodeI> object which is the root
 =cut
 
 sub new {
-  my($class,@args) = @_;
+    my ($class, @args) = @_;
   
-  my $self = $class->SUPER::new(@args);
-  $self->{'_rootnode'} = undef;
-  $self->{'_maxbranchlen'} = 0;
-  $self->_register_for_cleanup(\&cleanup_tree);
-  my ($root,$node,$nodel,$id,$score)= $self->_rearrange([qw(ROOT NODE NODELETE 
-                              ID SCORE)], @args);
+    my $self = $class->SUPER::new(@args);
+    $self->{'_rootnode'} = undef;
+    $self->{'_maxbranchlen'} = 0;
+    $self->_register_for_cleanup(\&cleanup_tree);
+    my ($root, $node, $nodel, $id, $score) =
+        $self->_rearrange([qw(ROOT NODE NODELETE ID SCORE)], @args);
   
-  if ($node && ! $root) {
-    $self->throw("Must supply a Bio::Tree::NodeI") unless ref($node) && $node->isa('Bio::Tree::NodeI');
-    my @lineage = $self->get_lineage_nodes($node);
-    $root = shift(@lineage) || $node;
+    if ($node && ! $root) {
+        $self->throw("Must supply a Bio::Tree::NodeI") unless ref($node) && $node->isa('Bio::Tree::NodeI');
+        my @lineage = $self->get_lineage_nodes($node);
+        $root = shift(@lineage) || $node;
     
-    # to stop us pulling in entire database of a Bio::Taxon when we later do
-    # get_nodes() or similar, specifically set ancestor() for each node
-    if ($node->isa('Bio::Taxon')) {
-      push(@lineage, $node) unless $node eq $root;
-      my $ancestor = $root;
-      foreach my $lineage_node (@lineage) {
-        $lineage_node->ancestor($ancestor);
-      } continue { $ancestor = $lineage_node; }
+        # to stop us pulling in entire database of a Bio::Taxon when we later do
+        # get_nodes() or similar, specifically set ancestor() for each node
+        if ($node->isa('Bio::Taxon')) {
+            push(@lineage, $node) unless $node eq $root;
+            my $ancestor = $root;
+            foreach my $lineage_node (@lineage) {
+                $lineage_node->ancestor($ancestor);
+            } continue { $ancestor = $lineage_node; }
+        }
     }
-  }
-  if ($root) {
-    $self->set_root_node($root);
-  }
+    if ($root) {
+        $self->set_root_node($root);
+    }
   
-  $self->nodelete($nodel || 0);
-  $self->id($id)       if defined $id;
-  $self->score($score) if defined $score;
-  return $self;
+    $self->nodelete($nodel || 0);
+    $self->id($id)       if defined $id;
+    $self->score($score) if defined $score;
+    return $self;
 }
 
 
@@ -177,11 +178,12 @@ Args    : on set, new boolean value
 
 =cut
 
-sub nodelete{
+sub nodelete {
     my $self = shift;
     return $self->{'nodelete'} = shift if @_;
     return $self->{'nodelete'};
 }
+
 
 =head2 get_nodes
 
@@ -191,37 +193,40 @@ Function: Return list of Bio::Tree::NodeI objects
 Returns : array of Bio::Tree::NodeI objects
 Args    : (named values) hash with one value 
            order => 'b|breadth' first order or 'd|depth' first order
-	   sortby => $sortby [optional] "height", "creation", "alpha", "revalpha",
+           sortby => [optional] "height", "creation", "alpha", "revalpha",
            or coderef to be used to sort the order of children nodes. See L<Bio::Tree::Node> for details
 
 =cut
 
-sub get_nodes{
-   my ($self, @args) = @_;
-   my ($order, $sortby) = $self->_rearrange([qw(ORDER SORTBY)],@args);
-   $order ||= 'depth';
-   $sortby ||= 'none';
-   my @children;
-   my $node = $self->get_root_node || return undef;
-   
-   if ($order =~ m/^b/oi) { # breadth-first
-       @children = ($node);
-       my @to_process = ($node);
-       while( @to_process ) { 
-	my $n = shift @to_process;
-        my @c  = $n->each_Descendent($sortby);
-	push @children, @c;
-	push @to_process, @c;
-       }
-   } elsif ($order =~ m/^d/oi) { # depth-first
-       # this is depth-first search I believe
-       @children = ($node,$node->get_all_Descendents($sortby));
-   } else {
-	$self->verbose(1);
-	$self->warn("specified an order '$order' which I don't understan\n");
-   }
-   @children;
+sub get_nodes {
+    my ($self, @args) = @_;
+    my ($order, $sortby) = $self->_rearrange([qw(ORDER SORTBY)], @args);
+    $order  ||= 'depth';
+    $sortby ||= 'none';
+
+    my @children;
+    my $node = $self->get_root_node;
+    if ($node) {
+        if ($order =~ m/^b/oi) { # breadth-first
+            @children = ($node);
+            my @to_process = ($node);
+            while( @to_process ) { 
+                my $n = shift @to_process;
+                my @c  = $n->each_Descendent($sortby);
+                push @children, @c;
+                push @to_process, @c;
+            }
+        } elsif ($order =~ m/^d/oi) { # depth-first
+            @children = ($node, $node->get_all_Descendents($sortby));
+        } else {
+            $self->verbose(1);
+            $self->warn("specified an order '$order' which I don't understan\n");
+        }
+    }
+
+    return @children;
 }
+
 
 =head2 get_root_node
 
@@ -234,11 +239,11 @@ Args    : none
 
 =cut
 
-
-sub get_root_node{
-   my ($self) = @_;
-   return $self->{'_rootnode'};
+sub get_root_node {
+    my ($self) = @_;
+    return $self->{'_rootnode'};
 }
+
 
 =head2 set_root_node
 
@@ -250,19 +255,19 @@ Args    : Bio::Tree::NodeI
 
 =cut
 
-sub set_root_node{
-   my $self = shift;
-   if( @_ ) { 
-       my $value = shift;
-       if( defined $value && 
-       ! $value->isa('Bio::Tree::NodeI') ) { 
-       $self->warn("Trying to set the root node to $value which is not a Bio::Tree::NodeI");
-       return $self->get_root_node;
-       }
-       $self->{'_rootnode'} = $value;
-   } 
-   return $self->get_root_node;
+sub set_root_node {
+    my $self = shift;
+    if ( @_ ) { 
+        my $value = shift;
+        if ( defined $value && ! $value->isa('Bio::Tree::NodeI') ) { 
+            $self->warn("Trying to set the root node to $value which is not a Bio::Tree::NodeI");
+            return $self->get_root_node;
+        }
+        $self->{'_rootnode'} = $value;
+    } 
+    return $self->get_root_node;
 }
+
 
 =head2 total_branch_length
 
@@ -275,6 +280,7 @@ Args    : none
 =cut
 
 sub total_branch_length { shift->subtree_length }
+
 
 =head2 subtree_length
 
@@ -294,7 +300,7 @@ sub subtree_length {
     return unless $node;
     my $sum = 0;
     for ( $node->get_all_Descendents ) {
-    $sum += $_->branch_length || 0;
+        $sum += $_->branch_length || 0;
     }
     return $sum;
 }
@@ -310,13 +316,14 @@ Args    : [optional] new value to set
 
 =cut
 
-sub id{
-   my ($self,$val) = @_;
-   if( defined $val ) { 
+sub id {
+   my ($self, $val) = @_;
+   if ( defined $val ) { 
        $self->{'_treeid'} = $val;
    }
    return $self->{'_treeid'};
 }
+
 
 =head2 score
 
@@ -330,9 +337,9 @@ Args    : newvalue (optional)
 
 =cut
 
-sub score{
-   my ($self,$val) = @_;
-   if( defined $val ) { 
+sub score {
+   my ($self, $val) = @_;
+   if ( defined $val ) { 
        $self->{'_score'} = $val;
    }
    return $self->{'_score'};
@@ -387,7 +394,7 @@ sub as_text {
 
     my $string = '';
     open(my $fh,">",\$string) or die ("Couldn't open $string as file: $!\n");
-    my $test = $iomod->new(-format=>$format,-fh=>$fh);
+    my $test = $iomod->new( -format => $format, -fh => $fh );
 
     # Get the default params for the given IO module.
     $test->set_params($params_input);
@@ -396,6 +403,7 @@ sub as_text {
     close($fh);
     return $string;
 }
+
 
 =head2 Methods for associating Tag/Values with a Tree
 
@@ -413,15 +421,16 @@ Args    : $tag   - tag name
 
 =cut
 
-sub set_tag_value{
-    my ($self,$tag,@values) = @_;
-    if( ! defined $tag || ! scalar @values ) {
-    $self->warn("cannot call set_tag_value with an undefined value");
+sub set_tag_value {
+    my ($self, $tag, @values) = @_;
+    if ( ! defined $tag || ! scalar @values ) {
+        $self->warn("cannot call set_tag_value with an undefined value");
     }
     $self->remove_tag ($tag);
     map { push @{$self->{'_tags'}->{$tag}}, $_ } @values;
     return scalar @{$self->{'_tags'}->{$tag}};
 }
+
 
 =head2 add_tag_value
 
@@ -434,14 +443,15 @@ Args    : $tag   - tag name
 
 =cut
 
-sub add_tag_value{
-    my ($self,$tag,$value) = @_;
-    if( ! defined $tag || ! defined $value ) {
-    $self->warn("cannot call add_tag_value with an undefined value");
+sub add_tag_value {
+    my ($self, $tag, $value) = @_;
+    if ( ! defined $tag || ! defined $value ) {
+        $self->warn("cannot call add_tag_value with an undefined value");
     }
     push @{$self->{'_tags'}->{$tag}}, $value;
     return scalar @{$self->{'_tags'}->{$tag}};
 }
+
 
 =head2 remove_tag
 
@@ -454,14 +464,15 @@ Args    : $tag - tagname to remove
 =cut
 
 sub remove_tag {
-   my ($self,$tag) = @_;
-   if( exists $self->{'_tags'}->{$tag} ) {
-       $self->{'_tags'}->{$tag} = undef;
-       delete $self->{'_tags'}->{$tag};
-       return 1;
-   }
-   return 0;
+    my ($self, $tag) = @_;
+    if ( exists $self->{'_tags'}->{$tag} ) {
+        $self->{'_tags'}->{$tag} = undef;
+        delete $self->{'_tags'}->{$tag};
+        return 1;
+    }
+    return 0;
 }
+
 
 =head2 remove_all_tags
 
@@ -473,11 +484,12 @@ Args    : None
 
 =cut
 
-sub remove_all_tags{
-   my ($self) = @_;
-   $self->{'_tags'} = {};
-   return;
+sub remove_all_tags {
+    my ($self) = @_;
+    $self->{'_tags'} = {};
+    return;
 }
+
 
 =head2 get_all_tags
 
@@ -489,11 +501,12 @@ Args    : None
 
 =cut
 
-sub get_all_tags{
-   my ($self) = @_;
-   my @tags = sort keys %{$self->{'_tags'} || {}};
-   return @tags;
+sub get_all_tags {
+    my ($self) = @_;
+    my @tags = sort keys %{$self->{'_tags'} || {}};
+    return @tags;
 }
+
 
 =head2 get_tag_values
 
@@ -505,11 +518,12 @@ Args    : $tag - tag name
 
 =cut
 
-sub get_tag_values{
-   my ($self,$tag) = @_;
-   return wantarray ? @{$self->{'_tags'}->{$tag} || []} :
-                     (@{$self->{'_tags'}->{$tag} || []})[0];
+sub get_tag_values {
+    my ($self, $tag) = @_;
+    return wantarray ? @{$self->{'_tags'}->{$tag} || []} :
+                      (@{$self->{'_tags'}->{$tag} || []})[0];
 }
+
 
 =head2 has_tag
 
@@ -522,9 +536,10 @@ Args    : $tag - tagname
 =cut
 
 sub has_tag {
-   my ($self,$tag) = @_;
-   return exists $self->{'_tags'}->{$tag};
+    my ($self, $tag) = @_;
+    return exists $self->{'_tags'}->{$tag};
 }
+
 
 # safe tree clone that doesn't seg fault
 
@@ -555,6 +570,7 @@ sub clone {
     my $tree = $self->new(-root => $parent_clone);
     return $tree;
 }
+
 
 # -- private internal methods --
 

t/Tree/Tree.t
@@ -6,17 +6,15 @@ use strict;
 BEGIN { 
     use lib '.';
     use Bio::Root::Test;
-    
-#/maj    test_begin(-tests => 60);
-    test_begin(-tests => 62);	
+    test_begin(-tests => 64);
     use_ok('Bio::TreeIO');
 }
 
 my $verbose = test_debug();
 
 my $treeio = Bio::TreeIO->new(-verbose => $verbose,
-			     -format => 'nhx',
-			     -file   => test_input_file('test.nhx'));
+                              -format => 'nhx',
+                              -file   => test_input_file('test.nhx'));
 my $tree = $treeio->next_tree;
 
 # tests for tags
@@ -40,16 +38,16 @@ is(@nodes, 2,'Number of nodes that have ADH2 as name');
 
 if( $verbose ) {
     $treeio = Bio::TreeIO->new(-verbose => $verbose,
-			      -format => 'nhx',
-			      );
+                               -format => 'nhx',
+                              );
     $treeio->write_tree($tree);
     print "nodes are: \n",
     join(", ", map {  $_->id . ":". (defined $_->branch_length ? 
-				     $_->branch_length : '' ) } @nodes), "\n";
+                                     $_->branch_length : '' ) } @nodes), "\n";
 }
 
 $treeio = Bio::TreeIO->new(-format => 'newick',
-			  -file   => test_input_file('test.nh'));
+                           -file   => test_input_file('test.nh'));
 $tree = $treeio->next_tree;
 
 
@@ -60,16 +58,16 @@ if( $verbose ) {
 }
 
 my @hADH = ( $tree->find_node('hADH1'),
-	     $tree->find_node('hADH2') );
+             $tree->find_node('hADH2') );
 my ($n4) = $tree->find_node('yADH4');
 
 is($tree->is_monophyletic(-nodes    => \@hADH,
-			  -outgroup => $n4),1,'Test Monophyly');
+                          -outgroup => $n4),1,'Test Monophyly');
 
 my @mixgroup = ( $tree->find_node('hADH1'),
-		 $tree->find_node('yADH2'),
-		 $tree->find_node('yADH3'),
-		 );
+                 $tree->find_node('yADH2'),
+                 $tree->find_node('yADH3'),
+                 );
 
 my ($iADHX) = $tree->find_node('iADHX');
 
@@ -77,7 +75,7 @@ my ($iADHX) = $tree->find_node('iADHX');
 is($iADHX->height, 0,'Height');
 is($iADHX->depth,0.22,'Depth');
 isnt( $tree->is_monophyletic(-nodes   => \@mixgroup,
-			    -outgroup=> $iADHX),1, 'non-monophyletic group');
+                             -outgroup=> $iADHX),1, 'non-monophyletic group');
 
 # binary tree?
 is $tree->is_binary, 0, 'not a binary tree';
@@ -88,48 +86,48 @@ is $tree->is_binary, 1, 'after force_binary() it is';
 is scalar $tree->get_nodes, 17, 'and there are more nodes (17)';
 
 my $in = Bio::TreeIO->new(-format => 'newick',
-			 -fh     => \*DATA);
+                          -fh     => \*DATA);
 $tree = $in->next_tree;
 my ($a,$b,$c,$d) = ( $tree->find_node('A'),
-		     $tree->find_node('B'),
-		     $tree->find_node('C'),
-		     $tree->find_node('D'));
+                     $tree->find_node('B'),
+                     $tree->find_node('C'),
+                     $tree->find_node('D'));
 
 is($tree->is_monophyletic(-nodes => [$b,$c],
-			  -outgroup => $d),1, 'B,C are Monophyletic');
+                          -outgroup => $d),1, 'B,C are Monophyletic');
 
 is($tree->is_monophyletic(-nodes => [$b,$a],
-			  -outgroup => $d),1,'A,B are Monophyletic');
+                          -outgroup => $d),1,'A,B are Monophyletic');
 
 $tree = $in->next_tree;
 my ($e,$f,$i);
 ($a,$b,$c,$d,$e,$f,$i) = ( $tree->find_node('A'),
-			   $tree->find_node('B'),
-			   $tree->find_node('C'),
-			   $tree->find_node('D'),
-			   $tree->find_node('E'),
-			   $tree->find_node('F'),
-			   $tree->find_node('I'),
-			   );
+                           $tree->find_node('B'),
+                           $tree->find_node('C'),
+                           $tree->find_node('D'),
+                           $tree->find_node('E'),
+                           $tree->find_node('F'),
+                           $tree->find_node('I'),
+                           );
 isnt( $tree->is_monophyletic(-nodes => [$b,$f],
-			    -outgroup => $d),1,'B,F are not Monophyletic' );
+                             -outgroup => $d),1,'B,F are not Monophyletic' );
 
 is($tree->is_monophyletic(-nodes => [$b,$a],
-			  -outgroup => $f),1, 'A,B are Monophyletic');
+                          -outgroup => $f),1, 'A,B are Monophyletic');
 
 # test for paraphyly
 
 isnt(  $tree->is_paraphyletic(-nodes => [$a,$b,$c],
-			   -outgroup => $d), 1,'A,B,C are not Monophyletic w D as outgroup');
+                              -outgroup => $d), 1,'A,B,C are not Monophyletic w D as outgroup');
 
 is(  $tree->is_paraphyletic(-nodes => [$a,$f,$e],
-			   -outgroup => $i), 1, 'A,F,E are monophyletic with I as outgroup');
+                            -outgroup => $i), 1, 'A,F,E are monophyletic with I as outgroup');
 
 
 # test for rerooting the tree
 my $out = Bio::TreeIO->new(-format => 'newick', 
-			   -fh => \*STDERR, 
-			   -noclose => 1);
+                           -fh => \*STDERR, 
+                           -noclose => 1);
 $tree = $in->next_tree;
 $tree->verbose( -1 ) unless $verbose;
 my $node_cnt_orig = scalar($tree->get_nodes);
@@ -147,7 +145,7 @@ is($tree->reroot($a),1, 'Can re-root with A as outgroup');
 $out->write_tree($tree) if $verbose;
 is($node_cnt_orig, scalar($tree->get_nodes), 'Count the number of nodes');
 my $total_length_new = $tree->total_branch_length;
-my $eps = 0.001 * $total_length_new;	# tolerance for checking length
+my $eps = 0.001 * $total_length_new; # tolerance for checking length
 warn("orig total len ", $total_length_orig, "\n") if $verbose;
 warn("new  total len ", $tree->total_branch_length,"\n") if $verbose;
 # according to retree in phylip these branch lengths actually get larger
@@ -212,8 +210,8 @@ is($tree->get_root_node, $a,'Root is really the ancestor we asked for'); #mod /m
 
 # BFS and DFS search testing
 $treeio = Bio::TreeIO->new(-verbose => $verbose,
-			     -format => 'newick',
-			     -file   => test_input_file('test.nh'));
+                           -format  => 'newick',
+                           -file    => test_input_file('test.nh'));
 $tree = $treeio->next_tree;
 my ($ct,$n) = (0);
 my $let = ord('A');
@@ -255,7 +253,7 @@ is($DFSorder, '0,1,2,A,B,C,D', 'DFS after removing all but 0,1,2,A,B,C,D');
 
 # try out the id to bootstrap copy method
 $treeio = Bio::TreeIO->new(-format => 'newick',
-			   -file   => test_input_file('bootstrap.tre'));
+                           -file   => test_input_file('bootstrap.tre'));
 $tree = $treeio->next_tree;
 my ($test_node) = $tree->find_node(-id => 'A');
 is($test_node->ancestor->id, 90,'Testing bootstrap copy');
@@ -268,8 +266,8 @@ is($test_node->ancestor->ancestor->bootstrap, '25', 'Testing bootstrap copy');
 
 # change TreeIO to parse 
 $treeio = Bio::TreeIO->new(-format => 'newick',
-			   -file   => test_input_file('bootstrap.tre'),
-			   -internal_node_id => 'bootstrap');
+                           -file   => test_input_file('bootstrap.tre'),
+                           -internal_node_id => 'bootstrap');
 $tree = $treeio->next_tree;
 ($test_node) = $tree->find_node(-id => 'A');
 is($test_node->ancestor->id, '','Testing auto-boostrap copy during parse');
@@ -280,6 +278,10 @@ is($test_node->ancestor->bootstrap, '90',
 is($test_node->ancestor->ancestor->bootstrap, '25', 
    'Testing auto-boostrap copy during parse');
 
+# return an empty array when no nodes are found
+ok $tree = Bio::Tree::Tree->new();
+@nodes = $tree->get_nodes;
+is scalar @nodes, 0;
 
 
 __DATA__