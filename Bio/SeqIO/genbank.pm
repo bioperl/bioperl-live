4fabd27897e559a29d42586cd5312bd8a37111ea
Adam Sjøgren <adsj@novozymes.com>
2012-10-09 12:46:04 +0200

    Quote qualifiers that are so long that they are line wrapped in genbank format.

--- Bio/SeqIO/genbank.pm
+++ Bio/SeqIO/genbank.pm
@@ -190,6 +190,8 @@ use Bio::Annotation::DBLink;
 
 use base qw(Bio::SeqIO);
 
+# Note that a qualifier that exceeds one line (i.e. a long label) will
+# automatically be quoted regardless:
 our %FTQUAL_NO_QUOTE = map {$_ => 1} qw(
     anticodon           citation
     codon               codon_start
@@ -1119,7 +1121,9 @@ sub _print_GenBank_FTHelper {
 	    }
 	    # there are almost 3x more quoted qualifier values and they
 	    # are more common too so we take quoted ones first
-	    elsif (!$FTQUAL_NO_QUOTE{$tag}) {
+            #
+            # Long qualifiers, that will be line wrapped, are always quoted
+	    elsif (!$FTQUAL_NO_QUOTE{$tag} or length("/$tag=$value")>=60) {
 		my ($pat) = ($value =~ /\s/ ? '\s|$' : '.|$');
 		$self->_write_line_GenBank_regex(" "x21,
 						 " "x21,
