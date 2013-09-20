63113e5baea8b8a32290ab176d601d5c5639c57e
Felipe da Veiga Leprevost <felipe@leprevost.com.br>
2013-09-20 18:38:00 -0300

    creating this first methods

--- Bio/DB/NextProt.pm
+++ Bio/DB/NextProt.pm
@@ -2,16 +2,117 @@ package Bio::DB::NextProt;
 
 use strict;
 use warnings;
-
+use JSON;
+use REST::Client;
 
 sub new {
 	my ($class, @args) = @_;
 	#my $self = $class->SUPER::new(@args);
 	my $self = {};
-	$self->{_fileInput}	= undef,
+	$self->{_client}	= REST::Client->new({host=> "http://www.nextprot.org", timeout => 10,});
+	$self->{_query}		= undef;
+	$self->{_filter}	= undef;
+	$self->{_format}	= "json";
 	bless($self, $class);
 	return $self;
 }
 
+sub search_protein() {
+	my $self  = shift;
+    my %param = @_;
+
+	my $path = "/rest/protein/list";
+	my $string = '';
+
+	if (defined $param{'-query'} && defined $param{'-filter'}) {
+		
+		$self->{_client}->GET($path."?query=".$param{'-query'}."&filter=".$param{'-filter'}."&format=".$self->{_format});
+
+	} elsif (defined $param{'-query'}) {
+		
+		$self->{_client}->GET($path."?query=".$param{'-query'}."&format=".$self->{_format});
+
+	} elsif (defined $param{'-filter'}) {
+		
+		$self->{_client}->GET($path."?filter=".$param{'-filter'}."&format=".$self->{_format});
+	}
+
+	&reset_params();
+
+	return $self->{_client}->responseContent();
+
+}
+
+sub search_cv() {
+	my $self  = shift;
+	my %param = @_;
+	
+	my $path   = "/rest/cv/list";
+	my $string = '';
+
+    if (defined $param{'-query'} && defined $param{'-filter'}) {
+
+        $self->{_client}->GET($path."?query=".$param{'-query'}."&filter=".$param{'-filter'}."&format=".$self->{_format});
+
+    } elsif (defined $param{'-query'}) {
+
+        $self->{_client}->GET($path."?query=".$param{'-query'}."&format=".$self->{_format});
+
+    } elsif (defined $param{'-filter'}) {
+
+        $self->{_client}->GET($path."?filter=".$param{'-filter'}."&format=".$self->{_format});
+    }
+
+	&reset_params();
+
+	return $self->{_client}->responseContent();
+
+}
+
+sub get_protein_info() {
+	my $self  = shift;
+	my %param = @_;
+
+	my $path   = "/rest/entry/";
+	my $string = '';
+
+}
+
+sub reset_params() {
+	my $self = shift;
+
+	$self->{_query}  = undef;
+	$self->{_filter} = undef;
+}
+
 
 1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
