554c3df8d1560b53e457231a2c19d3df9ef68ad4
Francisco J. Ossandon <fco.j.ossandon@gmail.com>
2013-12-24 14:03:10 -0300

    Some cleaning of trailing spaces, added missing Changes and bumped version to current Bio::Root::Version

--- Bio/Structure/StructureI.pm
+++ Bio/Structure/StructureI.pm
@@ -1,7 +1,7 @@
 #
 # BioPerl module for Bio::Structure::StructureI
 #
-# Please direct questions and support issues to <bioperl-l@bioperl.org> 
+# Please direct questions and support issues to <bioperl-l@bioperl.org>
 #
 # Cared for by Kris Boulez <kris.boulez@algonomics.com>
 #
@@ -34,15 +34,15 @@ the Bioperl mailing list.  Your participation is much appreciated.
   bioperl-l@bioperl.org                  - General discussion
   http://bioperl.org/wiki/Mailing_lists  - About the mailing lists
 
-=head2 Support 
+=head2 Support
 
 Please direct usage questions or support issues to the mailing list:
 
 I<bioperl-l@bioperl.org>
 
-rather than to the module maintainer directly. Many experienced and 
-reponsive experts will be able look at the problem and quickly 
-address it. Please include a thorough description of the problem 
+rather than to the module maintainer directly. Many experienced and
+reponsive experts will be able look at the problem and quickly
+address it. Please include a thorough description of the problem
 with code and data examples if at all possible.
 
 =head2 Reporting Bugs
