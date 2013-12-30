03782cef020768af397f8203fde39ab5ae973629
Francisco J. Ossandon <fco.j.ossandon@gmail.com>
2013-12-30 17:59:57 -0300

    StandAloneBlast & CommandExts: Apply double-quotes on executable path if it contains whitespaces before trying to run it

--- Bio/Tools/Run/WrapperBase/CommandExts.pm
+++ Bio/Tools/Run/WrapperBase/CommandExts.pm
@@ -972,6 +972,11 @@ sub _run {
     # Get program executable
     my $exe = $self->executable;
     $self->throw("Can't find executable for '".($self->is_pseudo ? $self->command : $self->program_name)."'; can't continue") unless $exe;
+    # Use double quotes if executable path have empty spaces
+    if ($exe =~ m/ /) {
+        $exe = "\"$exe\"";
+    }
+
     # Get command-line options
     my $options = $self->_translate_params();
     # Get file specs sans redirects in correct order
