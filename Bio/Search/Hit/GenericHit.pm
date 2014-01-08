35067868ce25dfbbefcb614a5c8289a7f45faa9a
Francisco J. Ossandon <fco.j.ossandon@gmail.com>
2014-01-08 17:08:20 -0300

    GenericHSP: Added '.' to gap symbols in "_pre_gaps", since is now used by HMMER3 format in alignments
    GenericHit: Changed "frac_aligned_query" and "frac_aligned_hit"
    to return undef if the query/hit length is unknown (like in
    some HMMER outputs), to avoid division by 0 crashes.
    Added this new behaviour to documentation and will include some
    tests for hmmer in next commit.

--- Bio/Search/Hit/GenericHit.pm
+++ Bio/Search/Hit/GenericHit.pm
@@ -1259,7 +1259,8 @@ sub frac_conserved {
            : across all HSPs (not including intervals between non-overlapping
            : HSPs).
  Example   : $frac_alnq = $hit_object->frac_aligned_query();
- Returns   : Float (2-decimal precision, e.g., 0.75).
+ Returns   : Float (2-decimal precision, e.g., 0.75),
+           : or undef if query length is unknown to avoid division by 0.
  Argument  : n/a
  Throws    : n/a
  Comments  : If you need data for each HSP, use hsps() and then interate
@@ -1282,8 +1283,9 @@ sub frac_aligned_query {
 
     Bio::Search::SearchUtils::tile_hsps($self) unless $self->tiled_hsps;
 
-    sprintf( "%.2f", $self->length_aln('query') /
-             $self->logical_length('query'));
+    my $qry_len = $self->logical_length('query');
+    return undef if $qry_len == 0; # Avoid division by 0 crash
+    sprintf( "%.2f", $self->length_aln('query') / $qry_len);
 }
 
 
@@ -1295,7 +1297,8 @@ sub frac_aligned_query {
            : across all HSPs (not including intervals between non-overlapping
            : HSPs).
  Example   : $frac_alnq = $hit_object->frac_aligned_hit();
- Returns   : Float (2-decimal precision, e.g., 0.75).
+ Returns   : Float (2-decimal precision, e.g., 0.75),
+           : or undef if hit length is unknown to avoid division by 0.
  Argument  : n/a
  Throws    : n/a
  Comments  : If you need data for each HSP, use hsps() and then interate
@@ -1318,7 +1321,9 @@ sub frac_aligned_hit {
 
     Bio::Search::SearchUtils::tile_hsps($self) unless $self->tiled_hsps;
 
-    sprintf( "%.2f", $self->length_aln('sbjct') / $self->logical_length('sbjct'));
+    my $sbjct_len = $self->logical_length('sbjct');
+    return undef if $sbjct_len == 0; # Avoid division by 0 crash
+    sprintf( "%.2f", $self->length_aln('sbjct') / $sbjct_len);
 }
 
 
