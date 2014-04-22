1b876edadd2b13c9afcd13703105196059df37e2
Francisco J. Ossandon <fco.j.ossandon@gmail.com>
2014-04-22 15:53:00 -0300

    Standardized URL to "https://github.com/bioperl/bioperl-live/issues" instead of "https://github.com/bioperl/bioperl-live/issues/" (since the first is the dominant form in the other hundreds files)

--- ide/bioperl-mode/site-lisp/bioperl-skel.el
+++ ide/bioperl-mode/site-lisp/bioperl-skel.el
@@ -201,7 +201,7 @@
     "of the bugs and their resolution. Bug reports can be submitted via" \N
     "the web:" \N
     \N
-    "  https://github.com/bioperl/bioperl-live/issues/" \N 
+    "  https://github.com/bioperl/bioperl-live/issues" \N 
     \N
     "=head1 AUTHOR - " v1 \N
     \N
