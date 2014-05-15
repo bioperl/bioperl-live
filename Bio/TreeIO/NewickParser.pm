dee22d2be5ea6266ec3205348b0d4cd15d61e8c6
Chris Fields <cjfields@bioperl.org>
2014-05-14 22:08:02 -0500

    Add a potential fix for AMPHORA2 newick support; this may be reverted as the format isn't quite valid newick, but it does work w/o regres`sion in tests.  Test case also added.
    Test case and fix from the blog post of Bill Nelson:
    http://wcnjournalclub.blogspot.com/2014/02/getting-amphora2-to-work-with-bioperl.html

--- Bio/TreeIO/NewickParser.pm
+++ Bio/TreeIO/NewickParser.pm
@@ -154,7 +154,14 @@ sub parse_newick {
             $self->_end('nhx_tag');
 
             $token = next_token(\$newick, ",);"); #move to , or )
-        }
+        } elsif ($token =~ /\[/) {
+            # This is a hack to make AMPHORA2 work
+            if ($token =~ /\[(\S+)\]/) {
+                $self->_start('bootstrap');
+                $self->_chars($1);
+                $self->_end('bootstrap');
+             }
+            $token = next_token(\$newick, ",);");        }
         $state = 5;
       } elsif ($state == 5) { # end node
         if($token eq ')') {
